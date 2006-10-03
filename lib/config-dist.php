a6eae91a6247f0aff7eedd18dfd8eb910c49a21c
Penny Leach <penny@mickey.wgtn.cat-it.co.nz>
2006-10-03 18:11:15 +1300

    beginnings of config-dist

---
+++ lib/config-dist.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *  This file is part of maraha.
+ *
+ *   maraha is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+
+ *   maraha is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with maraha; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+unset($cfg);
+$cfg = new StdClass;
+
+
+// database connection details
+$cfg->dbtype = 'postgres8';
+$cfg->dbhost = 'localhost';
+$cfg->dbport = 5432;
+$cfg->dbname = 'mahara';
+$cfg->dbuser = 'mahara';
+$cfg->dbpass = 'mahara';
+
+// wwwroot - include trailing slash
+$cfg->wwwroot = 'http://myhost.com/mahara/';
+
+// dirroot - uploaded files - include trailing slash.
+// must be writable by the webserver and outside document root.
+$cfg->dataroot = '/path/to/uploaddir';
+?>
