7541f3f19cb7b490138ca64d77d5e47fde17f800
Nigel McNie <nigel@catalyst.net.nz>
2006-10-03 12:59:58 +0000

    Removed worthless unset of $cfg. Added a TODO about programatically generating the wwwroot - $_SERVER has more than enough information to do this.

--- lib/config-dist.php
+++ lib/config-dist.php
@@ -17,7 +17,6 @@
  *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-unset($cfg);
 $cfg = new StdClass;
 
 
@@ -30,6 +29,7 @@ $cfg->dbuser = 'mahara';
 $cfg->dbpass = 'mahara';
 
 // wwwroot - include trailing slash
+// @todo <nigel>: Generate programatically
 $cfg->wwwroot = 'http://myhost.com/mahara/';
 
 // dirroot - uploaded files - include trailing slash.
