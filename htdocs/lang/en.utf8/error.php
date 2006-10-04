5217fdeb0ddfccaf5d826b45c3e4fda6959ec258
Penny Leach <penny@mickey.wgtn.cat-it.co.nz>
2006-10-05 10:19:02 +1300

    added missing strings to error

--- htdocs/lang/en.utf8/error.php
+++ htdocs/lang/en.utf8/error.php
@@ -1,3 +1,5 @@
 <?php
 $string['dbconnfailed'] = 'Failed to connect to database, error message was %s';
+$string['registerglobals'] = 'You have dangerous PHP settings, register_globals is on. Mahara is trying to work around this, but you should really fix it';
+$string['magicquotesgpc'] = 'You have dangerous PHP settings, magic_quotes_gpc is on. Mahara is trying to work around this, but you should really fix it';
 ?>
