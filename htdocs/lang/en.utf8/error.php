7b20ebadc61efefe73b4f4279797e3fc025ffbaa
Nigel McNie <nigel@catalyst.net.nz>
2006-10-06 00:18:52 +1300

    Added language strings for if magic_quotes_runtime is on (and if it cannot be turned off).

--- htdocs/lang/en.utf8/error.php
+++ htdocs/lang/en.utf8/error.php
@@ -2,4 +2,7 @@
 $string['dbconnfailed'] = 'Failed to connect to database, error message was %s';
 $string['registerglobals'] = 'You have dangerous PHP settings, register_globals is on. Mahara is trying to work around this, but you should really fix it';
 $string['magicquotesgpc'] = 'You have dangerous PHP settings, magic_quotes_gpc is on. Mahara is trying to work around this, but you should really fix it';
+$string['magicquotesruntimeerror'] = 'You have the PHP setting magic_quotes_runtime on, and Mahara cannot turn it off. Mahara requires this to be off to function properly. Please turn it off either in your php.ini or in your .htaccess file';
+$string['magicquotesruntimeworkaround'] = 'You have dangerous PHP settings, magic_quotes_runtime is on. Mahara is trying to work around this, but you should really fix it';
+
 ?>
