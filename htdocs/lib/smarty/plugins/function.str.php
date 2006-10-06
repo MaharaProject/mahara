343939621e9ca55e30646466e3a7c0ec23898b87
Penny Leach <penny@mickey.wgtn.cat-it.co.nz>
2006-10-06 15:25:57 +1300

    getstring like function for smarty

---
+++ htdocs/lib/smarty/plugins/function.str.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+
+/**
+ * Smarty {str} function plugin
+ *
+ * Type:     function<br>
+ * Name:     str<br>
+ * Date:     June 22, 2006<br>
+ * Purpose:  Fetch internationalized strings
+ * @author   Martyn Smith <martyn@catalyst.net.nz>
+ * @version  1.0
+ * @param array
+ * @param Smarty
+ * @return Internationalized string
+ */
+function smarty_function_str($params, &$smarty) {
+    global $CFG;
+    static $dictionary;
+    
+    if (!isset($params['section'])) {
+        $params['section'] = 'mahara';
+    }
+
+    $args = array($params['tag'],$params['section']);
+    if (isset($params['args']) && is_array($params['args'])) {
+        $args = array_merge($args,$params['args']);
+    }
+
+    $ret = call_user_func_array('get_string',$args);
+
+    // If there is an 'assign' parameter, place it into that instead.
+    if (!empty($params['assign'])) {
+        $smarty->assign($params['assign'], $ret);
+        return;
+    }
+
+    return $ret;
+}
