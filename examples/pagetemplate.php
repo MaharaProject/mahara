a23cde981273a8aca7adb803b6f5c01a41b97649
Nigel McNie <nigel@catalyst.net.nz>
2006-10-09 15:32:16 +1300

    Corrected capitalisation, now we know the name of the project.

--- examples/pagetemplate.php
+++ examples/pagetemplate.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This program is part of mahara
+ * This program is part of Mahara
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
