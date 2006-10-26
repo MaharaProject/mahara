2f674ba9aafbb5c66a83dbd7c7276b70562b3020
Nigel McNie <nigel@catalyst.net.nz>
2006-10-26 16:23:48 +1300

    Fixed minor bug in select if 'options' is not set. Added checkbox and radio element types.

---
+++ htdocs/lib/form/elements/radio.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This program is part of Mahara
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * @package    mahara
+ * @subpackage form
+ * @author     Nigel McNie <nigel@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006,2007 Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+defined('INTERNAL') || die();
+
+/**
+ * Renders a set of radio buttons for a form
+ *
+ * @param array $element The element to render
+ * @param Form $form     The form the element is being rendered on 
+ */
+function form_render_radio($element, Form $form) {
+    if (!isset($element['options']) || !is_array($element['options']) || count($element['options']) < 1) {
+        log_warn('Radio elements should have at least one option');
+    }
+    
+    $result = '';
+    $form_value = $form->get_value($element);
+    $id = $element['id'];
+    foreach ($element['options'] as $value => $text) {
+        $uid = $id . substr(md5(microtime()), 0, 4);
+        $element['id'] = $uid;
+        $result .= '<input type="radio"'
+            . Form::element_attributes($element)
+            . ' value="' . hsc($value) . '"'
+            . (($form_value == $value) ? ' checked="checked"' : '')
+            . "> <label for=\"$uid\">" . hsc($text) . "</label>\n";
+    }
+    
+    return $result;
+}
+
+?>
