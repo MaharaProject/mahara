90a484deb769b4d8262070cc72e33a18b0dca0b9
Richard Mansfield <richard@betlehem.wgtn.cat-it.co.nz>
2006-11-03 11:34:51 +1300

    Admin site page editor

---
+++ htdocs/admin/editchangepage.json.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This program is part of mahara
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * @package    mahara
+ * @subpackage admin
+ * @author     Penny Leach <penny@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006,2007 Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+define('INTERNAL', 1);
+require(dirname(dirname(__FILE__)) . '/init.php');
+
+$pagename = clean_requestdata('pagename', PARAM_ALPHAEXT, REQUEST_EITHER);
+
+$data['pagename'] = $pagename;
+try {
+    $page = get_record('site_content','name',$pagename);
+    $data['content'] = $page->content;
+    $data['success'] = 1;
+}
+catch (Exception $e) {
+    $data['success'] = 0;
+    $data['errormessage'] = $e->getMessage();
+}
+
+echo json_encode($data);  
+?>
