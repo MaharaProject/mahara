25b4c9c2c036968e68b70cf2ed6f1552a8e34913
Richard Mansfield <richard@betlehem.wgtn.cat-it.co.nz>
2006-11-06 13:16:19 +1300

    Check for changes to text content before loading a new page into the editor

--- htdocs/admin/editsitepage.php
+++ htdocs/admin/editsitepage.php
@@ -38,7 +38,7 @@ asort($pageoptions);
 $f = array(
     'name' => 'editsitepage',
     'method' => 'post',
-    'onsubmit' => 'return submitForm(\'editsitepage\',\'savesitepage.json.php\');',
+    'onsubmit' => 'return submitForm(\'editsitepage\',\'savesitepage.json.php\',contentSaved);',
     'action' => '',
     'elements' => array(
         'pagename' => array(
@@ -50,11 +50,11 @@ $f = array(
         'pagetext' => array(
             'name' => 'pagetext',
             'type' => 'wysiwyg',
-            'rows' => 25,
-            'cols' => 85,
+            'rows' => 20,
+            'cols' => 80,
             'title' => get_string('pagecontent'),
             'description' => get_string('textdesc'),
-            'value' => 'blah',
+            'value' => '',
             'rules' => array(
                 'required' => true
             )
@@ -73,19 +73,30 @@ if (use_html_editor()) {
 }
 
 $ijs = <<< EOJS
-setEditorContent = function(content) { $('pagetext').innerHTML = content; };
-function onLoad() {
-    requestPageText();
-    connect('pagename', 'onchange', requestPageText);
-    if (typeof(tinyMCE) != 'undefined') {
-        setEditorContent = tinyMCE.setContent;
-    }
-}
+
+// global stuff, set in onLoad().
+setEditorContent = function () {};
+getEditorContent = function () {};
+var oldpagename = '';
+var originalcontent = '';
+
 function requestPageText() {
+    // Allow the user to abort change if changes have been made in the editor.
+    if (getEditorContent() != originalcontent) {
+        var answer = confirm(get_string('discardchanges'));
+        if (!answer) {
+            $('pagename').value = oldpagename;
+            return;
+        }
+    }
+    displayMessage({'message':get_string('loadingpagecontent', $('pagename').value),'type':'info'});
     var d = loadJSONDoc('editchangepage.json.php',{'pagename':$('pagename').value});
     d.addCallback(function(data) {
         if (data.success) {
+            displayMessage({'message':get_string('loadedsuccessfully', $('pagename').value),'type':'info'});
             setEditorContent(data.content);
+            originalcontent = getEditorContent();
+            oldpagename = $('pagename').value;
         }
         else {
             displayMessage({'message':get_string('failedloadingpagecontent', $('pagename').value),
@@ -93,6 +104,33 @@ function requestPageText() {
         }
     });
 }
+
+// Called from submitForm on successful page save.
+function contentSaved () {  
+    originalcontent = getEditorContent();
+    requestPageText();
+}
+
+function onLoad() {
+    originalcontent = getEditorContent();
+    requestPageText();
+    connect('pagename', 'onchange', requestPageText);
+    if (typeof(tinyMCE) != 'undefined') {
+        setEditorContent = function (c) {
+            tinyMCE.setContent(c);
+            tinyMCE.execCommand('mceFocus',false,'mce_editor_0');
+        }
+        getEditorContent = tinyMCE.getContent;
+    }
+    else {
+        setEditorContent = function (c) {
+            $('pagetext').innerHTML = c;
+            $('pagetext').focus();
+        };
+        getEditorContent = function () { return $('pagetext').innerHTML };
+    }
+}
+
 addLoadEvent(onLoad);
 EOJS;
 
