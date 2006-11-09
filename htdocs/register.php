373c93a5e192b4c0fe4d94fac5630c58f6af2db2
Nigel McNie <nigel@catalyst.net.nz>
2006-11-09 23:05:12 +1300

    Implemented validation and submission of the first page, and also implemented the second page. Last page requires some profile stuff yet.

--- htdocs/register.php
+++ htdocs/register.php
@@ -28,6 +28,50 @@ define('INTERNAL', 1);
 define('PUBLIC', 1);
 require('init.php');
 
+/*
+ * This page handles three different tasks:
+ *   1) Showing a visitor the registration form
+ *   2) Telling the visitor to check their e-mail for a message
+ *   3) Given a key, display profile information to edit
+ *
+ * It uses the session to store some state
+ */
+if (!session_id()) {
+    session_start();
+}
+
+// @todo stuff to do for registration:
+//   - captcha image for step 1
+//   - show mandatory profile fields as configured sitewide. Relies on penny
+//     doing profile database work (internal artefact)
+//   - allow uploading of optional profile icon. requires profile stuffs
+
+// Step two of registration (first as it's the easiest): the user has
+// registered, show them a screen telling them this.
+if (!empty($_SESSION['registered'])) {
+    unset($_SESSION['registered']);
+    die_info(get_string('registeredok'));
+}
+
+if (isset($_GET['key'])) {
+    if (!$registration = get_record('usr_registration', 'regkey', $_GET['key'])) {
+        die_info(get_string('registrationnosuchkey'));
+    }
+
+    // This should show mandatory profile fields, and the optional profile icon thing.
+    // But until the database is ready, just do the stuff required to register the user.
+
+    // Move the user record to the usr table from the registration table
+    $registrationid = $registration->id;
+    unset($registration->id);
+    insert_record('usr', $registration);
+    delete_records('usr_registration', 'id', $registrationid);
+
+    // Log the user in and send them to the homepage
+    $SESSION->login($registration);
+    redirect(get_config('wwwroot'));
+}
+
 $elements = array(
     'username' => array(
         'type' => 'text',
@@ -80,7 +124,7 @@ $elements = array(
     )
 );
 
-$institutions = get_records('institution', 'registerallowed', true);
+$institutions = get_records_select('institution', "registerallowed = 1 AND authplugin = 'internal'");
 if (count($institutions) > 1) {
     $options = array();
     foreach ($institutions as $institution) {
@@ -107,12 +151,17 @@ $elements['tandc'] = array(
     'options' => array(
         'yes' => get_string('yes'),
         'no'  => get_string('no')
-    )
+    ),
+    'defaultvalue' => 'no',
+    'rules' => array(
+        'required' => true
+    ),
+    'separator' => '<br>'
 );
 
 $elements['submit'] = array(
-    'type' => 'submit',
-    'value' => get_string('register')
+    'type' => 'submitcancel',
+    'value' => array(get_string('register'), get_string('cancel'))
 );
 
 $form = array(
@@ -122,24 +171,90 @@ $form = array(
     'elements' => $elements
 );
 
+/**
+ * @todo add note: because the form select thing will eventually enforce
+ * that the result for $values['institution'] was in the original lot,
+ * and because that only allows authmethods that use 'internal' auth, we
+ * can guarantee that the auth method is internal
+ */
 function register_validate(Form $form, $values) {
-    global $SESSION;
-    $SESSION->add_info_msg('validating...');
-    
     $institution = $values['institution'];
-    $authtype    = auth_get_authtype_for_institution($institution);
-    $authclass   = 'Auth' . ucfirst(strtolower($authtype));
-    log_debug($institution . ' ' . $authclass . ' ' . $authtype);
-    safe_require('auth', $authtype, 'lib.php', 'require_once');
+    safe_require('auth', 'internal', 'lib.php', 'require_once');
 
-    if (!$form->get_error('username') && !call_static_method($authclass, 'is_username_valid', $values['username'])) {
-        $form->set_error('username', get_string('usernameinvalidform', 'auth.' . auth_get_authtype_for_institution($institution)));
+    if (!$form->get_error('username') && !AuthInternal::is_username_valid($values['username'])) {
+        $form->set_error('username', get_string('usernameinvalidform'));
+    }
+
+    if (!$form->get_error('username') && record_exists('usr', 'username', $values['username'])) {
+        $form->set_error('username', get_string('usernamealreadytaken'));
+    }
+
+    if (!$form->get_error('password1') && !AuthInternal::is_password_valid($values['password1'])) {
+        $form->set_error('password1', get_string('passwordinvalidform'));
+    }
+
+    if (!$form->get_error('password1') && $values['password1'] != $values['password2']) {
+        $form->set_error('password2', get_string('passwordsdonotmatch'));
+    }
+
+    // First name and last name must contain at least one non whitespace
+    // character, so that there's something to read
+    if (!$form->get_error('firstname') && !preg_match('/\S/', $values['firstname'])) {
+        $form->set_error('firstname', get_string('thisfieldisrequired'));
+    }
+
+    if (!$form->get_error('lastname') && !preg_match('/\S/', $values['lastname'])) {
+        $form->set_error('lastname', get_string('thisfieldisrequired'));
+    }
+
+    // The e-mail address cannot already be in the system
+    if (!$form->get_error('email')
+        && (record_exists('usr', 'email', $values['email'])
+        || record_exists('usr_registration', 'email', $values['email']))) {
+        $form->set_error('email', get_string('emailalreadytaken'));
+    }
+    
+    // If the user hasn't agreed to the terms and conditions, don't bother
+    if ($values['tandc'] != 'yes') {
+        $form->set_error('tandc', get_string('youmustagreetotheterms'));
     }
 }
 
 function register_submit($values) {
     global $SESSION;
-    $SESSION->add_ok_msg('w00t');
+
+    // store password encrypted
+    // don't die_info, since reloading the page shows the login form.
+    // instead, redirect to some other page that says this
+    safe_require('auth', 'internal', 'lib.php', 'require_once');
+    $values['salt']     = substr(md5(rand(1000000, 9999999)), 2, 8);
+    $values['password'] = AuthInternal::encrypt_password($values['password1'], $values['salt']);
+    $values['regkey']   = get_random_key();
+    $values['timeregistered'] = db_format_timestamp(time());
+    try {
+        insert_record('usr_registration', $values);
+
+        $user =(object) $values;
+        email_user($user, null,
+            get_string('registeredemailsubject'),
+            get_string('registeredemailmessagetext', 'mahara', $values['regkey']),
+            get_string('registeredemailmessagehtml', 'mahara', $values['regkey'], $values['regkey']));
+    }
+    catch (EmailException $e) {
+        die_info(get_string('registrationunsuccessful'));
+    }
+    catch (SQLException $e) {
+        die_info(get_string('registrationunsuccessful'));
+    }
+
+    // Add a marker in the session to say that the user has registered
+    $_SESSION['registered'] = true;
+
+    redirect(get_config('wwwroot') . 'register.php');
+}
+
+function register_cancel_submit() {
+    redirect(get_config('wwwroot'));
 }
 
 $smarty = smarty();
