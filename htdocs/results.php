5ddd2303a365b79d1a4ea48d96af73923c9db239
Richard Mansfield <richard@betlehem.wgtn.cat-it.co.nz>
2006-11-17 14:50:57 +1300

    Search results page added

---
+++ htdocs/results.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This program is part of Mahara
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * @package    mahara
+ * @subpackage core
+ * @author     Richard Mansfield <richard.mansfield@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006,2007 Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+define('INTERNAL', 1);
+require('init.php');
+
+// strings for firstname etc from  artefact/internal/lang/...
+$query = urlencode(param_variable('query',''));
+
+$javascript = <<<JAVASCRIPT
+var results = new TableRenderer(
+    'searchresults',
+    'results.json.php?query={$query}',
+    [
+        'firstname',
+        'lastname',
+        'preferredname',
+        'email',
+    ]
+);
+
+results.updateOnLoad();
+
+JAVASCRIPT;
+
+$smarty = smarty(array('tablerenderer'));
+$smarty->assign('INLINEJAVASCRIPT', $javascript);
+$smarty->display('results.tpl');
+
+?>
