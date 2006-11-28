81c2b50d1be6e0744f8e27bbc0f0b40afe64cf9c
Richard Mansfield <richard@betlehem.wgtn.cat-it.co.nz>
2006-11-28 17:30:35 +1300

    Moved basic file plugin stuff into its own directories

---
+++ htdocs/artefact/file/lib.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * This program is part of Mahara
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * @package    mahara
+ * @subpackage artefact-internal
+ * @author     Penny Leach <penny@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006,2007 Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+defined('INTERNAL') || die();
+
+class ArtefactTypeFolder extends ArtefactType {
+    public function commit() {
+        $this->commit_basic();
+    }
+    
+    public function delete() {
+        $this->delete_basic();
+    }
+
+    public function render($format, $options) {
+
+    }
+
+    public function get_icon() {
+
+    }
+
+    public static function get_render_list() {
+
+    }
+    
+    public static function can_render_to($format) {
+
+    }    
+
+    public static function collapse_config() {
+        return 'file';
+    }
+}
+
+class ArtefactTypeFile extends ArtefactType {
+
+    public function commit() {
+        $this->commit_basic();
+    }
+    
+    public function delete() {
+        $this->delete_basic();
+    }
+
+    public function render($format, $options) {
+
+    }
+
+    public function get_icon() {
+
+    }
+
+    public static function get_render_list() {
+
+    }
+    
+    public static function can_render_to($format) {
+
+    }
+
+    public static function has_config() {
+        return true;
+    }
+
+    public static function get_config_options() {
+        return array(); // @todo  
+    }
+}
+class ArtefactTypeImage extends ArtefactTypeFile {
+    
+    public static function collapse_config() {
+        return 'file';
+    }
+}
+
+?>
