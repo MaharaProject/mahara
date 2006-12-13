0cfeb53f9218833e8d6fe171b010b758979f6923
Richard Mansfield <richard@betlehem.wgtn.cat-it.co.nz>
2006-12-14 09:11:09 +1300

    Create folder script

---
+++ htdocs/artefact/file/createfolder.json.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * This program is part of Mahara
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * @package    mahara
+ * @subpackage artefact-file
+ * @author     Richard Mansfield <richard.mansfield@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006,2007 Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+define('INTERNAL', 1);
+require(dirname(dirname(dirname(__FILE__))) . '/init.php');
+safe_require('artefact', 'file');
+global $USER;
+
+$parentfolder   = param_variable('parentfolder', null); // id of parent artefact
+$title          = param_variable('title');
+$description    = param_variable('description', null);
+$collideaction  = param_variable('collideaction', 'fail');
+
+$data = new StdClass;
+if ($parentfolder) {
+    $data->parent = (int) $parentfolder;
+}
+$data->title = $title;
+$data->description = $description;
+$data->owner = $USER->get('id');
+$data->container = 1;
+$data->locked = 0;
+
+$f = new ArtefactTypeFolder(0, $data);
+if ($oldid = ArtefactTypeFileBase::exists_in_db($data->title, $data->owner, $parentfolder)) {
+    if ($collideaction == 'replace') {
+        require_once('artefact.php');
+        $obj = artefact_instance_from_id($oldid);
+        $obj->delete();
+        json_reply(false, get_string('foldercreated'));
+    }
+    else {
+        json_reply('local', get_string('fileexists'));
+    }
+}
+if (!isset($result->error)) {
+    if ($f->save_uploaded_file('userfile')) {
+        json_reply(false, get_string('foldercreated'));
+    }
+    else {
+        json_reply('local', get_string('errorcreatingfolder'));
+    }
+}
+
+?>
