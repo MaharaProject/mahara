13eca0679108dbf06126eb58d09ef235bb134259
Penny Leach <penny@mickey.wgtn.cat-it.co.nz>
2006-12-15 12:34:57 +1300

    tagged and moving on... taken away disable logins and set minupgradefrom

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -27,8 +27,9 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2006121501;
-$config->release = '0.1 (build tag BUILD_20061215)';
-$config->disablelogin = true;
+$config->version = 2006121502;
+$config->release = '0.2';
+$config->minupgradefrom = 2006121501;
+$config->minupgraderelease = '0.1 (build tag BUILD_20061215)';
 
 ?>
