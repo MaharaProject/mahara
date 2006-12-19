df076ef0d4bbea46d868f98c16edc5fcc1f50efd
Nigel McNie <nigel@catalyst.net.nz>
2006-12-20 11:05:01 +1300

    First go at edit view access screen

---
+++ htdocs/view/editaccess.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * This program is part of Mahara
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * @package    mahara
+ * @subpackage core
+ * @author     Nigel McNie <nigel@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006,2007 Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+define('INTERNAL', 1);
+define('MENUITEM', 'view');
+require(dirname(dirname(__FILE__)) . '/init.php');
+require_once('pieforms/pieform.php');
+require_once('pieforms/pieform/elements/calendar.php');
+$smarty = smarty(array(), pieform_get_headdata_calendar(pieform_configure_calendar(array())));
+
+$viewid = param_integer('viewid');
+$prefix = get_config('dbprefix');
+
+if (!$data = get_records_sql_array('SELECT va.accesstype AS type, va.startdate, va.stopdate
+    FROM ' . $prefix . 'view_access va
+    LEFT JOIN ' . $prefix . 'view v ON (va.view = v.id)
+    WHERE va.view = ?
+    AND v.owner = ?
+    ORDER BY va.accesstype', array($viewid, $USER->get('id')))) {
+    $SESSION->add_error_msg(get_string('canteditdontown', 'view'));
+    redirect(get_config('wwwroot') . 'view/');
+}
+
+foreach ($data as &$item) {
+    $item = (array)$item;
+}
+log_debug($data);
+
+// Get access for users, groups and communities
+$extradata = get_records_sql_array("
+    SELECT 'user' AS type, usr AS id, 0 AS tutoronly, startdate, stopdate
+        FROM {$prefix}view_access_usr
+        WHERE view = ?
+UNION
+    SELECT 'group', grp, 0, startdate, stopdate
+        FROM {$prefix}view_access_group
+        WHERE view = ?
+UNION
+    SELECT 'community', community, tutoronly, startdate, stopdate
+        FROM {$prefix}view_access_community
+        WHERE view = ?", array($viewid, $viewid, $viewid));
+if ($extradata) {
+    foreach ($extradata as &$extraitem) {
+        $extraitem = (array)$extraitem;
+    }
+    $data = array_merge($data, $extradata);
+}
+
+
+$form = array(
+    'name' => 'editviewaccess',
+    'elements' => array(
+        'accesslist' => array(
+            'type'         => 'viewacl',
+            'defaultvalue' => $data
+        ),
+        'viewid' => array(
+            'type' => 'hidden',
+            'value' => $viewid
+        ),
+        'submit' => array(
+            'type' => 'submitcancel',
+            'value' => array(get_string('saveaccess'), get_string('cancel'))
+        )
+    )
+);
+
+function editviewaccess_cancel_submit() {
+    redirect(get_config('wwwroot') . 'view/');
+}
+
+function editviewaccess_submit($values) {
+    global $SESSION, $USER, $viewid;
+
+    db_begin();
+    delete_records('view_access', 'view', $viewid);
+    delete_records('view_access_usr', 'view', $viewid);
+    delete_records('view_access_group', 'view', $viewid);
+    delete_records('view_access_community', 'view', $viewid);
+    $time = db_format_timestamp(time());
+
+    // View access
+    foreach ($values['accesslist'] as $item) {
+        $accessrecord = new StdClass;
+        $accessrecord->view = $viewid;
+        $accessrecord->startdate = db_format_timestamp($item['startdate']);
+        $accessrecord->stopdate  = db_format_timestamp($item['stopdate']);
+        switch ($item['type']) {
+            case 'public':
+            case 'loggedin':
+            case 'friends':
+                $accessrecord->accesstype = $item['type'];
+                insert_record('view_access', $accessrecord);
+                break;
+            case 'user':
+                $accessrecord->usr = $item['id'];
+                insert_record('view_access_usr', $accessrecord);
+                break;
+            case 'group':
+                $accessrecord->grp = $item['id'];
+                insert_record('view_access_group', $accessrecord);
+                break;
+            case 'community':
+                $accessrecord->community = $item['id'];
+                insert_record('view_access_community', $accessrecord);
+                break;
+        }
+    }
+    db_commit();
+    $SESSION->add_ok_msg(get_string('viewaccesseditedsuccessfully'));
+    redirect(get_config('wwwroot') . 'view/');
+}
+
+$smarty->assign('titlestr', get_string('editviewaccess', 'view'));
+$smarty->assign('form', pieform($form));
+$smarty->display('view/create4.tpl');
+
+?>
