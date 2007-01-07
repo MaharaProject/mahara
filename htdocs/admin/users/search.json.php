c6b70ddf4412d870bda58f5498c79bc9a56c513c
Martyn Smith <martyn@catalyst.net.nz>
2007-01-08 11:56:51 +1300

    Admin search

---
+++ htdocs/admin/users/search.json.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * This program is part of Mahara
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * @package    mahara
+ * @subpackage core
+ * @author     Martyn Smith <martyn@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006,2007 Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+define('INTERNAL', 1);
+
+require(dirname(dirname(dirname(__FILE__))) . '/init.php');
+
+json_headers();
+json_check_sesskey();
+
+$action = param_variable('action');
+$dbprefix = get_config('dbprefix');
+
+if ($action == 'suspend') {
+    $id = param_integer('id');
+    $reason = param_variable('reason');
+
+    try {
+        suspend_user($id, $reason);
+    }
+    catch (MaharaException $e) {
+        json_reply('suspendfailed', $e->getMessage());
+    }
+
+    echo json_encode(array('error' => null));
+    exit;
+}
+
+if ($action == 'search') {
+    require('searchlib.php');
+    try {
+        $query = param_variable('query');
+    }
+    catch (ParameterException $e) {
+        json_reply('missingparameter','Missing parameter \'query\'');
+    }
+    $limit = param_integer('limit', 20);
+    $offset = param_integer('offset', 0);
+
+    $data = search_user($query, $limit, $offset);
+
+    if ($data['data']) {
+        foreach ($data['data'] as &$result) {
+            $result->name = display_name($result);
+        }
+    }
+
+    json_headers();
+    $data['error'] = false;
+    $data['message'] = '';
+    echo json_encode($data);
+    exit;
+}
+
+?>
+
