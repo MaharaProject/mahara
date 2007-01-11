9a4646cd05b518590d98d449a208122748d0a0dc
Nigel McNie <nigel@catalyst.net.nz>
2007-01-12 10:09:51 +1300

    Use classes instead of styles.

--- htdocs/account/activity/index.php
+++ htdocs/account/activity/index.php
@@ -68,10 +68,10 @@ var activitylist = new TableRenderer(
         'date',
         function (r, d) {
             if (r.read == 1) {
-                return TD({'style': 'text-align: center;'},IMG({'src' : d.star, 'alt' : d.unread}));
+                return TD({'class': 'center'},IMG({'src' : d.star, 'alt' : d.unread}));
             }
             else {
-                return TD({'style': 'text-align: center;'}, INPUT({'type' : 'checkbox', 'class' : 'tocheck', 'name' : 'unread-' + r.id}));
+                return TD({'class': 'center'}, INPUT({'type' : 'checkbox', 'class' : 'tocheck', 'name' : 'unread-' + r.id}));
             }
         }
     ]
