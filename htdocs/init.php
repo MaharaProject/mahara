29b7a6126af2357f9d3a3d9a142d8f37a7390f1e
Martyn Smith <martyn@catalyst.net.nz>
2007-01-15 11:09:38 +1300

    Unbreak the dataroot path "fixing"

--- htdocs/init.php
+++ htdocs/init.php
@@ -53,7 +53,7 @@ $CFG = (object)array_merge((array)$cfg, (array)$CFG);
 
 // Fix up paths in $CFG
 foreach (array('docroot', 'dataroot') as $path) {
-    $CFG->{$path} = (substr($CFG->{$path}, -1) != DIRECTORY_SEPARATOR) ? $CFG->{$path} . DIRECTORY_SEPARATOR : $CFG->{$path} . "/";
+    $CFG->{$path} = (substr($CFG->{$path}, -1) != DIRECTORY_SEPARATOR) ? $CFG->{$path} . DIRECTORY_SEPARATOR : $CFG->{$path};
 }
 
 // xmldb stuff
