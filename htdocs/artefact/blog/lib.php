54fc6474c57cedbcb22aa0f59efb2d681cd4bf31
Richard Mansfield <richard@betlehem.wgtn.cat-it.co.nz>
2007-01-18 12:15:24 +1300

    Show blogpost author in renderfull mode

--- htdocs/artefact/blog/lib.php
+++ htdocs/artefact/blog/lib.php
@@ -468,7 +468,9 @@ class ArtefactTypeBlogPost extends ArtefactType {
     protected function render_full($options) {
         $smarty = smarty();
         $smarty->assign('artefact', $this);
-        $smarty->assign('creationtime', format_date($this->ctime));
+        $smarty->assign('postedbyon', get_string('postedbyon', 'artefact.blog',
+                                                 display_name($this->owner),
+                                                 format_date($this->ctime)));
         return $smarty->fetch('artefact:blog:render/blogpost_renderfull.tpl');
     }
 
