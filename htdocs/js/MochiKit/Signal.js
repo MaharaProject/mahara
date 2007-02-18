455a200a504e57ff44736af133b2f3e080856a07
Martyn Smith <martyn@catalyst.net.nz>
2007-02-19 10:55:22 +1300

    Upstream sync

--- htdocs/js/MochiKit/Signal.js
+++ htdocs/js/MochiKit/Signal.js
@@ -539,8 +539,6 @@ MochiKit.Base.update(MochiKit.Signal, {
                 self._disconnect(observers[i]);
             }
         }
-        
-        delete self._observers;
     },
 
     _listener: function (src, sig, func, obj, isDOM) {
