f952f8431d14345bb408f55293c1f71cfef0bc49
Donal McMullan <donal@catalyst.net.nz>
2007-04-04 18:04:16 +1200

    Adding a policy for what the despatcher will execute.

--- htdocs/api/xmlrpc/dispatcher.php
+++ htdocs/api/xmlrpc/dispatcher.php
@@ -1,67 +1,74 @@
 <?php
 
-class Request {
+class Dispatcher {
 
-    private $error     = array();
-    private $request   = array();
+    private $params    = array();
     private $callstack = array();
     private $payload   = '';
     private $method    = '';
+    private $response  = '';
 
     function __construct($payload) {
         global $CFG;
         $this->payload = $payload;
-        $this->method  = '';
 
         // xmlrpc_decode_request is defined such that the '$method' string is
         // passed in by reference.
-        $this->request = xmlrpc_decode_request($this->payload, $this->method);
+        $this->params  = xmlrpc_decode_request($this->payload, $this->method, 'UTF-8');
 
-        $this->Dispatcher = new Dispatcher($this->request);
-/*
+        // The method name is not allowed to have a dot, except for a single dot
+        // which preceeds the php extension. It can have slashes but it cannot
+        // begin with a slash. We specifically don't want .. to be possible.
         if (0 == preg_match("@^[A-Za-z0-9]+/[A-Za-z0-9/_-]+(\.php/)?[A-Za-z0-9_-]+$@",$this->method)) {
-            $this->error = array(0 => 'The function does not exist');
             throw new XmlrpcServerException('The function does not exist', 6010);
-            return false;
         }
 
+        // Security: I'm thinking that we should not return separate errors for
+        //           the file not existing, the file not being readable, etc. as
+        //           it might provide an opportunity for outsiders to scan the
+        //           server for random files. So just a single message/code for
+        //           all failures here.
         if(strpos($this->method, '/') !== false) {
             $this->callstack  = explode('/', $this->method);
-        } elseif(strpos($this->method, '.') !== false) {
-            $this->callstack  = explode('.', $this->method);
         } else {
             throw new XmlrpcServerException('The function does not exist', 6011);
-            return false;
         }
 
         $functionname = array_pop($this->callstack);
-        $filename     = implode('/', $this->callstack);
+        $filename     = $CFG->docroot . implode('/', $this->callstack);
 
-        //if(!file_exists($filename.'.php')
-*/
-        return true;
+        if(!file_exists($filename)) {
+           throw new XmlrpcServerException('The function does not exist', 6011);
+        }
+
+        if(!is_readable($filename)) {
+            throw new XmlrpcServerException('The function does not exist', 6011);
+        }
+
+        // Make sure that the fully resolved path really is under docroot
+        $realpath = realpath($filename);
+        if(0 == preg_match("@^{$CFG->docroot}@", $realpath)) {
+            throw new XmlrpcServerException('The function does not exist '.$realpath.' '.$CFG->docroot, 6011);
+        }
+
+        // Make sure that the file we are including is called api.php
+        if(0 == preg_match("@api.php$@", $realpath)) {
+            throw new XmlrpcServerException('The function does not exist '.$realpath.' '.$CFG->docroot, 6011);
+        }
+
+        $temp = '';
+        $xmlrpcserver = xmlrpc_server_create();
+
+        include_once($filename);
+
+        $this->response = xmlrpc_server_call_method($xmlrpcserver, $payload, $temp, array("encoding" => "utf-8"));
+        return $this->response;
     }
 
     function __get($name) {
-        if(isset($this->{$name})) return $this->{$name};
+        if ($name == 'response') return $this->response;
         return null;
     }
 }
 
-class Dispatcher {
-    function __construct($payload) {
-        $zed = 'anything';
-        $xmlrpcserver = xmlrpc_server_create();
-        xmlrpc_server_register_method($xmlrpcserver, 'examples.getTime', 'time');
-        xmlrpc_server_register_method($xmlrpcserver, 'examples.getTime', 'netdate');
-        $response = xmlrpc_server_call_method($xmlrpcserver, $payload, $zed, array("encoding" => "utf-8"));
-        //$response = mnet_server_prepare_response($response);
-        var_dump($response);
-    }
-}
-
-function netdate($method, $params, $hostinfo) {
-    echo $method.', '. $params .', '. $hostinfo;
-    return date($params[0]);
-}
 ?>
