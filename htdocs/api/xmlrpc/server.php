5cd36d6621e6862ab092d76ec1b4ad2b88336424
Donal McMullan <donal@catalyst.net.nz>
2007-04-04 18:06:49 +1200

    Remove bootstrap code and move utilities in to functions in lib.php

--- htdocs/api/xmlrpc/server.php
+++ htdocs/api/xmlrpc/server.php
@@ -42,48 +42,28 @@ define('INTERNAL', 1);
 define('PUBLIC', 1);
 require(dirname(__FILE__).'/lib.php');
 
-
 // Catch anything that goes wrong in init.php
 ob_start();
     require(dirname(dirname(dirname(__FILE__))).'/init.php');
-    require_once($CFG->docroot .'/api/xmlrpc/request.php');
+    require_once($CFG->docroot .'/api/xmlrpc/dispatcher.php');
     $errors = trim(ob_get_contents());
 ob_end_clean();
 
 // Content type for output is never html:
-//header('Content-type: text/xml; charset=utf-8');
-header('Content-type: text/plain; charset=utf-8');
+header('Content-type: text/xml; charset=utf-8');
+//header('Content-type: text/plain; charset=utf-8');
 
 if(!empty($errors)) throw new XmlrpcServerException('Initialization failed. Non-recoverable error.', 6000);
 
 set_exception_handler('xmlrpc_exception');
 ini_set('display_errors',0);
+
 error_reporting(E_ALL);
 ini_set('display_errors', true);
-/*
-$f = new Peer();
-$f->init($CFG->wwwroot);
-$f->bootstrap($CFG->wwwroot, 'mahara');
-$f->commit();
-exit;
-*/
-//$payload = $HTTP_RAW_POST_DATA;
-$payload =  '';
-
-/**/
 $openssl = OpenSslRepo::singleton();
-$payload = '<?xml version="1.0"?>
-<methodCall>
-   <methodName>examples.getTime</methodName>
-   <params>
-      <param>
-         <value><string>d/m/Y H:i:s</string></value>
-         </param>
-      </params>
-   </methodCall>';
-$payload = xmldsig_envelope($payload);
-$payload = xmlenc_envelope($payload, $openssl->certificate);
-/**/
+$payload           = $HTTP_RAW_POST_DATA;
+$payload_encrypted = false;
+$payload_signed    = false;
 
 try {
     $xml = new SimpleXMLElement($payload);
@@ -91,58 +71,16 @@ try {
     throw new XmlrpcServerException('Payload is not a valid XML document', 6001);
 }
 
-// Cascading switch
+// Cascading switch. Kinda.
 if($xml->getName() == 'encryptedMessage') {
-    $data = base64_decode($xml->EncryptedData->CipherData->CipherValue);
-    $key  = base64_decode($xml->EncryptedKey->CipherData->CipherValue);
-    $payload = '';    // Initialize payload var
-    $payload = $openssl->openssl_open($data, $key);
-    try {
-        $xml = new SimpleXMLElement($payload);
-    } catch (Exception $e) {
-        throw new XmlrpcServerException('Encrypted payload is not a valid XML document', 6002);
-    }
+    $payload_encrypted = true;
+    $wwwroot           = (string)$xml->wwwroot;
+    $payload           = xmlenc_envelope_strip($xml);
 }
 
-
-
 if($xml->getName() == 'signedMessage') {
-
-    $signature = base64_decode($xml->Signature->SignatureValue);
-    $payload   = base64_decode($xml->object);
-    $wwwroot   = $xml->wwwroot;
-    $timestamp = $xml->timestamp;
-
-    $Peer = new Peer();
-    if(!$Peer->init($wwwroot)) {
-        // Bootstrap unknown hosts?
-        throw new XmlrpcServerException('We don\'t have a record for your webserver in our database', 6003);
-    }
-
-    // Does the signature match the data and the public cert?
-    $signature_verified = openssl_verify($payload, $signature, $Peer->public_key->certificate);
-
-    if ($signature_verified == 1) {
-        // Parse the XML
-        try {
-            $xml = new SimpleXMLElement($payload);
-        } catch (Exception $e) {
-            throw new XmlrpcServerException('Signed payload is not a valid XML document', 6007);
-        }
-    } elseif ($signature_verified == 0) {
-        $currkey = get_public_key($MNET_REMOTE_CLIENT->wwwroot, $MNET_REMOTE_CLIENT->application->xmlrpc_server_url);
-        if($currkey != $certificate) {
-            // Has the server updated its certificate since our last 
-            // handshake?
-            if(!$MNET_REMOTE_CLIENT->refresh_key()) {
-                throw new XmlrpcServerException('The signature on your message was not valid', 6006);
-            }
-        } else {
-            throw new XmlrpcServerException('The signature on your message was not valid', 6005);
-        }
-    } else {
-        throw new XmlrpcServerException('An error occurred while trying to verify your message signature', 6004);
-    }
+    $payload_signed = true;
+    $payload        = xmldsig_envelope_strip($xml);
 }
 
 if($xml->getName() == 'methodCall') {
@@ -150,11 +88,24 @@ if($xml->getName() == 'methodCall') {
     if(empty($xml->methodName)) {
         throw new XmlrpcServerException('Payload is not an XML-RPC document', 6008);
     }
-    //$Request = new Request($payload);
+
     $Dispatcher = new Dispatcher($payload);
-    //var_dump($Request); 
+
+    if ($payload_signed) {
+        $response = xmldsig_envelope($Dispatcher->response);
+    } else {
+        $response = $Dispatcher->response;
+    }
+
+    if ($payload_encrypted) {
+        $peer     = get_peer($wwwroot);
+        $response = xmlenc_envelope($response, $peer->certificate);
+    }
+
+    echo $response;
+
 } else {
-    throw new XmlrpcServerException('Unrecognized XML document form', 6009);
+    throw new XmlrpcServerException('Unrecognized XML document form: ' . var_export($xml,1), 6009);
 }
 
 ?>
