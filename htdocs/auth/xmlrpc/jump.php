207ca83278f8be269657bd7fb513557debddbd00
Donal McMullan <donal@opteron22.(none)>
2007-06-05 03:29:07 +1200

    Launchpad for opening sso sessions on remote hosts

---
+++ htdocs/auth/xmlrpc/jump.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @author Martin Dougiamas
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package moodle multiauth
+ *
+ * Authentication Plugin: Moodle Network Authentication
+ *
+ * Multiple host authentication support for Moodle Network.
+ *
+ * 2006-11-01  File created.
+ */
+define('INTERNAL', 1);
+
+require(dirname(dirname(dirname(__FILE__))).'/init.php');
+require_once($CFG->docroot .'/api/xmlrpc/client.php');
+require_once($CFG->docroot .'/auth/xmlrpc/lib.php');
+require_once($CFG->docroot .'/include/eLearning/institution.php');
+
+$remotewwwroot = param_variable('wr');
+$instanceid    = param_variable('ins');
+$wantsurl      = '';
+
+$peer = new Peer();
+$peer->findByWwwroot($remotewwwroot);
+$url = $remotewwwroot.$peer->application->ssolandurl;
+
+$providers = get_service_providers($USER->authinstance);
+$approved  = false;
+
+$url = start_jump_session($peer, $instanceid);
+
+if (empty($url)) {
+    throw new Exception('DEBUG: Jump session was not started correctly or blank URL returned.'); // TODO: errors
+}
+redirect($url);
+
+?>
