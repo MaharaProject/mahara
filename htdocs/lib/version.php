6e725653885cd255cb61ed54fb2bec3f8034e372
Donal McMullan <donal@opteron22.(none)>
2007-06-05 03:28:07 +1200

    Bump version for db update

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -27,7 +27,7 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2007051500;
+$config->version = 2007060203;
 $config->release = '0.8.0.dev';
 $config->minupgradefrom = 2007011600;
 $config->minupgraderelease = '0.4.0 (build tag BUILD_20070122)';
