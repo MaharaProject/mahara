67cdde56ee85cd899af9282b5b44ec2684892450
Donal McMullan <donal@catalyst.net.nz>
2007-06-22 12:05:40 +1200

    Removing old demo files

--- htdocs/auth/xmlrpc/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-xmlrpc_server_register_method($xmlrpcserver, 'auth/xmlrpc/api.php/getTime', 'getTime');
-
-function getTime($method, $params, $hostinfo) {
-    // echo $method.', '. $params .', '. $hostinfo;
-    return date($params[0]);
-}
-?>
