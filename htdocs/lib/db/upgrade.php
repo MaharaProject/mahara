c0460b7578eff7ea0e693a3f545320af90ae5180
Donal McMullan <donal@catalyst.net.nz>
2007-07-16 21:26:57 +1200

    fixing 691: inconsistent foreign keys in usr

--- htdocs/lib/db/upgrade.php
+++ htdocs/lib/db/upgrade.php
@@ -386,6 +386,10 @@ function xmldb_core_upgrade($oldversion=0) {
         $key->setAttributes(XMLDB_KEY_FOREIGN, array('lastauthinstance'), 'auth_instance', array('id'));
         add_key($table, $key);
 
+        $key   = new XMLDBKey("authinstancefk");
+        $key->setAttributes(XMLDB_KEY_FOREIGN, array('authinstance'), 'auth_instance', array('id'));
+        add_key($table, $key);
+
         $record = new stdClass();
 
         $record->name            = 'mahara';
