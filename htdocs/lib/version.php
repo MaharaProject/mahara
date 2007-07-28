eb9a0e2fb162e2972d98548d8aa4950c5f2ec3e5
Nigel McNie <nigel@catalyst.net.nz>
2007-07-28 21:50:40 +1200

    Version bump for new release

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -27,8 +27,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2007062900;
-$config->release = '0.8.0.dev';
+$config->version = 20070728;
+$config->release = '0.8.0';
 $config->minupgradefrom = 2007011600;
 $config->minupgraderelease = '0.4.0 (build tag BUILD_20070122)';
 
