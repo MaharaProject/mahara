19ed8ab2a24a6348967b43b64e2870500b7885ee
Nigel McNie <nigel@catalyst.net.nz>
2007-08-02 09:44:53 +1200

    Version bump for community -> group table renaming

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -27,7 +27,7 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2007081700;
+$config->version = 2007081701;
 $config->release = '0.9.0dev';
 $config->minupgradefrom = 2007080700;
 $config->minupgraderelease = '0.8.0 (release tag 0.8.0_RELEASE)';
