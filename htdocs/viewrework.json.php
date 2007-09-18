876a68d4e993e15b0e93903c8d9732e2d58bf9aa
Nigel McNie <nigel@catalyst.net.nz>
2007-09-18 15:14:37 +1200

    Removed old TODO comment

--- htdocs/viewrework.json.php
+++ htdocs/viewrework.json.php
@@ -50,26 +50,6 @@ if ($change) {
 switch ($action) {
 case 'blocktype_list':
     $category = param_alpha('category');
-    // TODO:
-    //
-    // Where I can get to:
-    // Static version: Clicking buttons = response, but no action.
-    // Dynamic version: Actions = response, but no action performed.
-    //
-    // What can be done:
-    // [done] Add column: js should make room for it, put in the raw html (note: need to generate column raw html in a function then!)
-    // [done] Remove column: js should destroy the existing column, hopefully moving the blocks to other columns (could be implemented as move move delete?)
-    // Moving block instances: js should drag+drop, do ajax stub request. static should send response
-    // Add block: js should be done by drag and drop. static should respond
-    //
-    // Where I'm up to:
-    // Drag+Drop move blockinstances
-    // ...
-    // Fix up all the stuff that works with javascript to work in the non javascript version
-    // Clean up view_process_changes() and related things
-    // ...
-    // Blocktype adding to view (but only after the change can be reflected in the database!)
-    //
     $data = view_build_blocktype_list($category, true);
     json_reply(false, array('message' => false, 'data' => $data));
     break;
