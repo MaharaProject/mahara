c3966482bad9026763cd1cfb216a9f17b0489c2d
Nigel McNie <nigel@catalyst.net.nz>
2007-10-23 19:18:12 +1300

    Second cut at pagination javascript.
    Now documented and cleaned up somewhat. Also takes an 'extradata' parameter of stuff to send back to the json script.

--- htdocs/js/paginator.js
+++ htdocs/js/paginator.js
@@ -1,14 +1,21 @@
 /**
  * Hooks into pagination built with the smarty function 'mahara_pagination',
  * and rewrites it to be javascript aware
+ *
+ * @param id The ID of the div that contains the pagination
+ * @param datatable The ID of the table containing the paginated data
+ * @param script    The URL (from wwwroot) of the script to hit to get new
+ *                  pagination data
+ * @param limit     Extra data to pass back in the ajax requests to the script
  */
-var Paginator = function(id, datatable, script) {
+var Paginator = function(id, datatable, script, extradata) {
     var self = this;
 
-    this.init = function(id, datatable, script) {
+    this.init = function(id, datatable, script, extradata) {
         self.id = id;
         self.datatable = $(datatable);
         self.jsonScript = config['wwwroot'] + script;
+        self.extraData = extradata;
 
         self.rewritePaginatorLinks();
     }
@@ -25,25 +32,28 @@ var Paginator = function(id, datatable, script) {
                     var loc = a.href.indexOf('?');
                     var queryData = [];
                     if (loc != -1) {
-                        queryData = parseQueryString(a.href.substring(loc + 1, a.href.length)); // ie danger
-                        log(queryData);
+                        queryData = parseQueryString(a.href.substring(loc + 1, a.href.length));
+                        queryData.extradata = serializeJSON(self.extraData);
                     }
 
                     sendjsonrequest(self.jsonScript, queryData, 'GET', function(data) {
                         getFirstElementByTagAndClassName('tbody', null, self.datatable).innerHTML = data['data']['tablerows'];
-                        log('id', self.id, 'self', self);
-                        $(self.id).innerHTML = data['data']['pagination'];
+
+                        // Update the pagination
+                        var tmp = DIV();
+                        tmp.innerHTML = data['data']['pagination'];
+                        swapDOM(self.id, tmp.firstChild);
+
+                        // Run the pagination js to make it live
                         eval(data['data']['pagination_js']);
-                        //self.changePage();
+
+                        // Update the result count
+                        getFirstElementByTagAndClassName('div', 'results', self.id).innerHTML = data['data']['count'] + ' results'; // TODO i18n
                     });
                 });
             }
         });
     }
 
-    this.changePage = function() {
-        
-    }
-
-    this.init(id, datatable, script);
+    this.init(id, datatable, script, extradata);
 }
