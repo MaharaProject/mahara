b03a01432c04609b455a802c4615f769270d5e61
Nigel McNie <nigel@catalyst.net.nz>
2007-10-26 15:00:04 +1300

    Added pieform rules for ensuring that a value is at least or most a certain size.

---
+++ htdocs/lib/pieforms/pieform/rules/minvalue.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This program is part of Pieforms
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * @package    pieform
+ * @subpackage rule
+ * @author     Nigel McNie <nigel@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006 Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+/**
+ * Checks whether the given value is at least a certain size.
+ *
+ * @param Pieform $form      The form the rule is being applied to
+ * @param string  $value     The value to check
+ * @param array   $element   The element to check
+ * @param int     $maxlength The value to check for
+ * @return string            The error message, if the value is invalid.
+ */
+function pieform_rule_minvalue(Pieform $form, $value, $element, $minvalue) {
+    if ($value != '' && intval($value) < $minvalue) {
+        return sprintf($form->i18n('rule', 'minvalue', 'minvalue', $element), $minvalue);
+    }
+}
+
+function pieform_rule_minvalue_i18n() {
+    return array(
+        'en.utf8' => array(
+            'minvalue' => 'This value can not be smaller than %d'
+        ),
+    );
+}
+
+?>
