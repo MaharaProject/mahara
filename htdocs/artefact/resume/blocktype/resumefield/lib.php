90aeecaefb566eec12306b9fd3f22fb781c2ad2b
Nigel McNie <nigel@catalyst.net.nz>
2007-11-07 14:34:07 +1300

    Added the first go at the resumefield blocktype.

---
+++ htdocs/artefact/resume/blocktype/resumefield/lib.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * This program is part of Mahara
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * @package    mahara
+ * @subpackage blocktype-resumefield
+ * @author     Nigel McNie <nigel@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006,2007 Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+defined('INTERNAL') || die();
+
+class PluginBlocktypeResumefield extends PluginBlocktype {
+
+    public static function get_title() {
+        return get_string('title', 'blocktype.resume/resumefield');
+    }
+
+    public static function get_description() {
+        return get_string('description', 'blocktype.resume/resumefield');
+    }
+
+    public static function get_categories() {
+        return array('resume');
+    }
+
+    public static function render_instance(BlockInstance $instance) {
+        require_once(get_config('docroot') . 'lib/artefact.php');
+        $smarty = smarty_core();
+        $configdata = $instance->get('configdata');
+
+        // Get data about the resume field in this blockinstance
+        if (!empty($configdata['artefactid'])) {
+            $resumefield = artefact_instance_from_id($configdata['artefactid']);
+            $rendered = $resumefield->render_self(false);
+            $result = $rendered['html'];
+            if (!empty($rendered['javascript'])) {
+                $result .= '<script type="text/javascript">' . $rendered['javascript'] . '</script>';
+            }
+            return $result;
+        }
+        return '';
+    }
+
+    public static function get_artefacts(BlockInstance $instance) {
+        $configdata = $instance->get('configdata');
+        if (isset($configdata['artefactid'])) {
+            return array($configdata['artefactid']);
+        }
+
+        return false;
+    }
+
+    public static function has_instance_config() {
+        return true;
+    }
+
+    public static function instance_config_form($instance) {
+        $configdata = $instance->get('configdata');
+
+        $form = array();
+
+        // Which resume field does the user want
+        $form[] = self::artefactchooser_element((isset($configdata['artefactid'])) ? $configdata['artefactid'] : null);
+        $form[] = array(
+            'type' => 'html',
+            'value' => '<a href="' . get_config('wwwroot') . 'artefact/resume/">Fill out your resume</a> in order to add more fields!'
+        );
+
+        return $form;
+    }
+
+    // TODO: make decision on whether this should be abstract or not
+    public static function artefactchooser_element($default=null) {
+        safe_require('artefact', 'resume');
+        return array(
+            'name'  => 'artefactid',
+            'type'  => 'artefactchooser',
+            'title' => get_string('fieldtoshow', 'blocktype.resume/resumefield'),
+            'defaultvalue' => $default,
+            'blocktype' => 'resumefield',
+            'limit'     => 655360, // 640K profile fields is enough for anyone!
+            'selectone' => true,
+            'artefacttypes' => PluginArtefactResume::get_artefact_types(),
+            'template'  => 'artefact:resume:artefactchooser-element.tpl',
+        );
+    }
+
+    /**
+     * Deliberately enforce _no_ sort order. The database will return them in 
+     * the order they were inserted, which means roughly the order that they 
+     * are listed in the profile screen
+     */
+    public static function artefactchooser_get_sort_order() {
+        return '';
+    }
+}
+
+?>
---
+++ htdocs/artefact/resume/theme/default/artefactchooser-element.tpl
@@ -0,0 +1,11 @@
+    <tr>
+        <td style="width: 20px;" rowspan="2">
+            {if $selectone}<input type="radio" class="radio"id="{$elementname}_{$artefact->id}" name="{$elementname}" value="{$artefact->id|escape}"{if $value == $artefact->id} checked="checked"{/if}>
+            {else}<input type="checkbox" id="{$elementname}_{$artefact->id}" name="{$elementname}[{$artefact->id}]"{if $value && in_array($artefact->id, $value)} checked="checked"{/if}>
+            <input type="hidden" name="{$elementname}_onpage[]" value="{$artefact->id}">
+            {/if}</td>
+        <th><label for="{$elementname}_{$artefact->id}" title="{$artefact->title|strip_tags|substr:0:60|escape}">{str tag=$artefact->artefacttype section=artefact.resume}</label></th>
+    </tr>
+    <tr>
+        <td>{if $artefact->description}{$artefact->description}{/if}</td>
+    </tr>
