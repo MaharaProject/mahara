d36c11ac8958683618eb1095762882f653bd789e
Francois Marier <francois@catalyst.net.nz>
2007-11-20 18:21:38 +1300

    Move common code into a mahara common package, making the -apache packages optional There are now 3 binary packages being built: - mahara: the bulk of Mahara, can be used with any web server - mahara-apache: configures Mahara for Apache 1.x - mahara-apache2: configures Mahara for Apache 2.x
    Another big change was moving the Mahara code from /var/www/mahara into /usr/share/mahara.  This is to follow the Debian policy more closely and to mirror what the mediawiki does for example.
    
    Other changes:
    - Set myself as maintainer until the package has been accepted in Debian.
    - Cleaned-up the package dependencies and build dependencies.
    - Removed the database checks in preinst since they were postgres specific
    - Removed files (like README, LICENSE) we shouldn't install
    - Fix for rotations of empty log files
    - Fix the path in the purge target of mahara-apache.postrm
    - Renamed the XS-Vcs-* fields to Vcs-*
    - Check to make sure that the cron job exists before running it

---
+++ debian/mahara.cron.d
@@ -0,0 +1,5 @@
+#
+# Mahara implements its own cron to cater for people who need to run it in
+# situations where normal unixy cron isn't available.
+#
+* * * * *   www-data     if [ -r /usr/share/mahara/lib/cron.php ] ; then /usr/bin/php5 /usr/share/mahara/lib/cron.php >>/var/log/mahara-cron.log 2>&1 ; fi
