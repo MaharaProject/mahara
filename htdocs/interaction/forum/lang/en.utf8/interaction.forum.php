918c2cb55d50a2b1c8c26c0e71aebd6f1d57057f
Clare Lenihan <clare@catalyst.net.nz>
2007-11-22 12:05:52 +1300

    added language strings

--- htdocs/interaction/forum/lang/en.utf8/interaction.forum.php
+++ htdocs/interaction/forum/lang/en.utf8/interaction.forum.php
@@ -1,8 +1,29 @@
 <?php
 
+$string['addtitle'] = 'Add forum';
+$string['body'] = 'Body';
+$string['cancel'] = 'Cancel';
+$string['count'] = 'Count';
+$string['delete'] = 'Delete';
+$string['deleteforum'] = 'Delete forum';
+$string['deletepostsure'] = 'Are you sure you want to do this? It cannot be undone.';
+$string['edit'] = 'Edit';
+$string['edittitle'] = 'Edit forum';
 $string['name'] = 'Forum';
 $string['nameplural'] = 'Forums';
-$string['addtitle'] = 'Add forum';
-$string['edittitle'] = 'Edit forum';
+$string['newforum'] = 'New forum';
+$string['newtopic'] = 'New topic';
+$string['noforums'] = 'There are no forums in this group';
+$string['post'] = 'Post';
+$string['poster'] = 'Poster';
+$string['posts'] = 'Posts';
+$string['regulartopics'] = 'Regular topics';
+$string['reply'] = 'Reply';
+$string['sticky'] = 'Sticky';
+$string['stickytopics'] = 'Sticky topics';
+$string['subject'] = 'Subject';
+$string['subjecttoolong'] = 'The subject must be less than 255 characters';
+$string['subscribe'] = 'Subscribe';
+$string['unsubscribe'] = 'Unsubscribe';
 
 ?>
