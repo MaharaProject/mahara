e795749c7942123163a4e008094e31d975f59488
Clare Lenihan <clare@catalyst.net.nz>
2007-11-26 11:03:05 +1300

    updated language file

--- htdocs/interaction/forum/lang/en.utf8/interaction.forum.php
+++ htdocs/interaction/forum/lang/en.utf8/interaction.forum.php
@@ -3,12 +3,19 @@
 $string['addtitle'] = 'Add forum';
 $string['body'] = 'Body';
 $string['cancel'] = 'Cancel';
+$string['closed'] = 'Closed';
 $string['count'] = 'Count';
 $string['delete'] = 'Delete';
 $string['deleteforum'] = 'Delete forum';
+$string['deletepost'] = 'Delete post';
 $string['deletepostsure'] = 'Are you sure you want to do this? It cannot be undone.';
+$string['deletetopic'] = 'Delete topic';
+$string['deletetopicsure'] = 'Are you sure you want to do this? It cannot be undone.';
 $string['edit'] = 'Edit';
+$string['edited'] = 'Edited';
+$string['editpost'] = 'Edit post';
 $string['edittitle'] = 'Edit forum';
+$string['edittopic'] = 'Edit topic';
 $string['name'] = 'Forum';
 $string['nameplural'] = 'Forums';
 $string['newforum'] = 'New forum';
@@ -17,13 +24,14 @@ $string['noforums'] = 'There are no forums in this group';
 $string['post'] = 'Post';
 $string['poster'] = 'Poster';
 $string['posts'] = 'Posts';
+$string['re'] ='Re: ';
 $string['regulartopics'] = 'Regular topics';
 $string['reply'] = 'Reply';
 $string['sticky'] = 'Sticky';
 $string['stickytopics'] = 'Sticky topics';
 $string['subject'] = 'Subject';
-$string['subjecttoolong'] = 'The subject must be less than 255 characters';
 $string['subscribe'] = 'Subscribe';
-$string['unsubscribe'] = 'Unsubscribe';
+$string['topic'] = 'Topic';
+$string['unsubscribe'] = 'Unsubscribe';6
 
 ?>
