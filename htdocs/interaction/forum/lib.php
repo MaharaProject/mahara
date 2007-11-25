9d96f9e6088a6f9e795a2278e4e234f11df2c7e6
Clare Lenihan <clare@catalyst.net.nz>
2007-11-26 11:07:03 +1300

    added is_forum_moderator method

--- htdocs/interaction/forum/lib.php
+++ htdocs/interaction/forum/lib.php
@@ -15,5 +15,35 @@ class InteractionForumInstance extends InteractionInstance {
 
 }
 
+/**
+ * Is a user a moderator of a given forum
+ * 
+ * @param int $forumid id of forum
+ * @param int $userid optional id of user, defaults to logged in user
+ *
+ * @returns boolean
+ */
+function is_forum_moderator($forumid, $userid=null) {
+    if (empty($userid)) {
+        global $USER;
+        $userid = $USER->get('id');
+    }
+    else if (!is_int($userid)) {
+        throw new InvalidArgumentException("non integer user id given to is_forum_moderator: $userid");
+    }
+    
+    if (!is_int($forumid)) {
+        throw new InvalidArgumentException("non integer forum id given to is_forum_moderator: $forumid");
+    }
+    
+    return record_exists_sql(
+        'SELECT "user"
+        FROM {interaction_forum_moderator}
+        WHERE "user" = ?
+        AND forum = ?',
+        array($userid, $forumid)
+    );
+}
+
 
 ?>
