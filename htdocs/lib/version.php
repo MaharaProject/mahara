98bf79c1e58c338d66b285da0665cbb23e0d1201
Nigel McNie <nigel@catalyst.net.nz>
2007-11-30 15:24:04 +1300

    Version bump for the 0.9.0beta2 release.

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -27,8 +27,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2007112300;
-$config->release = '0.9.0beta2dev';
+$config->version = 2007113000;
+$config->release = '0.9.0beta2';
 $config->minupgradefrom = 2007080700;
 $config->minupgraderelease = '0.8.0 (release tag 0.8.0_RELEASE)';
 $config->disablelogin = false;
