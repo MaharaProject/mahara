097c01fdca984fce79058b9396f7209fa3de2a78
Nigel McNie <nigel@catalyst.net.nz>
2007-12-04 15:46:57 +1300

    German translation for 'before' pieform rule. Thanks to Heinz.

--- htdocs/lib/pieforms/pieform/rules/before.php
+++ htdocs/lib/pieforms/pieform/rules/before.php
@@ -48,6 +48,9 @@ function pieform_rule_before_i18n() {
         'en.utf8' => array(
             'before' => 'This can not be after the field "%s"'
         ),
+        'de.utf8' => array(
+            'before' => 'Dies kann nicht nach dem Feld "%s" sein'
+        ),
     );
 }
 
