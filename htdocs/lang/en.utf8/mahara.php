ecd38a203c6d006a0232a8121a8ec76d3318bec3
Nigel McNie <nigel@catalyst.net.nz>
2007-12-09 11:09:10 +1300

    Translate 'captchaimage' string, as picked up by file content check.

--- htdocs/lang/en.utf8/mahara.php
+++ htdocs/lang/en.utf8/mahara.php
@@ -167,6 +167,7 @@ $string['cannotremovedefaultemail'] = 'You cannot remove your primary email addr
 $string['emailtoolong'] = 'E-mail addresses cannot be longer that 255 characters';
 $string['mustspecifyoldpassword'] = 'You must specify your current password';
 $string['captchatitle'] = 'CAPTCHA Image';
+$string['captchaimage'] = 'CAPTCHA Image';
 $string['captchadescription'] = 'Enter the characters you see in the picture to the right. Letters are not case sensitive';
 $string['captchaincorrect'] = 'Enter the letters as they are shown in the image';
 
