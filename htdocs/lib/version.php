0c0967b7465417a08abeafea3f50e085ceb13833
Richard Mansfield <richard.mansfield@catalyst.net.nz>
2008-01-24 18:48:08 +1300

    Bump version to match latest upgrade

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -27,7 +27,7 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2008012400;
+$config->version = 2008012401;
 $config->release = '1.0.0alpha2dev';
 $config->minupgradefrom = 2007080700;
 $config->minupgraderelease = '0.8.0 (release tag 0.8.0_RELEASE)';
