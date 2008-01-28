b015e38fa4b6440484f939aee1bc95938629079e
Clare Lenihan <clare@catalyst.net.nz>
2008-01-28 14:15:20 +1300

    removed check of whether layout is editable

--- htdocs/view/blocks.php
+++ htdocs/view/blocks.php
@@ -96,9 +96,6 @@ $smarty->assign('formurl', get_config('wwwroot') . 'view/blocks.php');
 $smarty->assign('category', $category);
 $smarty->assign('new', $new);
 $smarty->assign('view', $view->get('id'));
-
-$smarty->assign('can_change_layout', ($view->get('numcolumns') > 1 && $view->get('numcolumns') < 5));
-
 $smarty->display('view/blocks.tpl');
 
 ?>
