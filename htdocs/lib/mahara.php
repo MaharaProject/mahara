f2204a636d0b176db33359eda091666abbfa568f
Nigel McNie <nigel@catalyst.net.nz>
2008-01-29 14:30:41 +1300

    Add a configuration parameter for where the javascript files for Pieforms are located (new in pieforms 0.3)

--- htdocs/lib/mahara.php
+++ htdocs/lib/mahara.php
@@ -1093,6 +1093,7 @@ function pieform_configure() {
         'renderer'  => 'maharatable',
         'requiredmarker' => true,
         'elementclasses' => true,
+        'jsdirectory'    => get_config('wwwroot') . 'lib/pieforms/static/core/',
         'jserrorcallback'       => 'formError',
         'globaljserrorcallback' => 'formGlobalError',
         'jssuccesscallback'     => 'formSuccess',
