9d1ecf986bf9257662a1e4aaf91f606476e261ef
Richard Mansfield <richard.mansfield@catalyst.net.nz>
2008-01-30 11:56:32 +1300

    Add restore from loggedinas link to profile sideblock

--- htdocs/lib/mahara.php
+++ htdocs/lib/mahara.php
@@ -1687,6 +1687,10 @@ function profile_sideblock() {
     $data = array(
         'id' => $USER->get('id'),
     );
+    $saveduser = $USER->get('parentuser');
+    if (!empty($saveduser) && $saveduser->name) {
+        $data['realuser'] = $saveduser->name;
+    }
     $data['unreadnotifications'] = call_static_method(generate_class_name('notification', 'internal'), 'unread_count', $USER->get('id'));
     $data['unreadnotificationsmessage'] = $data['unreadnotifications'] == 1 ? get_string('unreadmessage') : get_string('unreadmessages');
     $invitedgroups = get_invited_groups();
