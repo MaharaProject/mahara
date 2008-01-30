cf6087e4a9b254717279770c4e145750c0fee036
Nigel McNie <nigel@catalyst.net.nz>
2008-01-30 15:35:31 +1300

    Whole pile of language string and help file updates for consistency and clarity.

--- htdocs/lang/en.utf8/admin.php
+++ htdocs/lang/en.utf8/admin.php
@@ -65,12 +65,12 @@ $string['manageinstitutions'] = 'Manage Institutions';
 // Admin homepage strings
 $string['siteoptions']    = 'Site options';
 $string['siteoptionsdescription'] = 'Configure basic site options such as the name, language and theme';
-$string['sitepages']     = 'Site pages';
-$string['sitepagesdescription'] = 'Edit the core site content';
-$string['sitemenu'] = 'Site links and resources';
-$string['sitemenudescription'] = 'Manage the links and files within Useful Links and Resources';
+$string['editsitepages']     = 'Edit site pages';
+$string['editsitepagesdescription'] = 'Edit the content of various pages around the site';
+$string['linksandresourcesmenu'] = 'Links and Resources Menu';
+$string['linksandresourcesmenudescription'] = 'Manage the links and files within the Links and Resources Menu';
 $string['adminfiles']          = 'Admin Files';
-$string['adminfilesdescription'] = 'Upload and administer files that can be put in Useful Links and Resources';
+$string['adminfilesdescription'] = 'Upload and administer files that can be put in the Links and Resources Menu';
 $string['networking']          = 'Networking';
 $string['networkingdescription'] = 'Configure networking for Mahara';
 
@@ -89,93 +89,54 @@ $string['uploadcsvdescription'] = 'Upload a CSV file containing new users';
 $string['usersearch'] = 'User Search';
 $string['usersearchdescription'] = 'Search all users and perform administrative actions on them';
 $string['usersearchinstructions'] = 'You can search for users by clicking on the initials of their first and last names, or by entering a name in the search box.  You can also enter an email address in the search box if you would like to search email addresses.';
+
 $string['institutionmembersdescription'] = 'Associate users with institutions';
 $string['institutionstaffdescription'] = 'Assign users Staff permissions';
 $string['institutionadminsdescription'] = 'Assign Institution Administrator access rights';
+
 $string['pluginadmin'] = 'Plugin Administration';
 $string['pluginadmindescription'] = 'Install and configure plugins';
 
 // Site options
 $string['allowpublicviews'] = 'Allow public views';
 $string['allowpublicviewsdescription'] = 'If set to yes, users will be able to create Views that are accessable to the public rather than only to logged in users';
-$string['artefactviewinactivitytime'] = 'Artefact view inactivity time';
-$string['artefactviewinactivitytimedescription'] = 'The time after which an inactive view or artefact will be moved to the InactiveContent area';
+$string['defaultaccountinactiveexpire'] = 'Default account inactivity time';
+$string['defaultaccountinactiveexpiredescription'] = 'How long a user account will remain active without the user logging in';
+$string['defaultaccountinactivewarn'] = 'Warning time for inactivity/expiry';
+$string['defaultaccountinactivewarndescription'] = 'The time before user accounts are to expire or become inactive at which a warning message will be sent to them';
+$string['defaultaccountlifetime'] = 'Default account lifetime';
+$string['defaultaccountlifetimedescription'] = 'If set, user accounts will expire after this period of time from when they have been created';
 $string['language'] = 'Language';
 $string['pathtoclam'] = 'Path to clam';
 $string['pathtoclamdescription'] = 'The filesystem path to clamscan or clamdscan';
 $string['pathtofile'] = 'Path to file';
 $string['pathtofiledescription'] = 'Filesystem path to the \'file\' program';
+$string['searchplugin'] = 'Search plugin';
+$string['searchplugindescription'] = 'Search plugin to use';
 $string['sessionlifetime'] = 'Session lifetime';
 $string['sessionlifetimedescription'] = 'Time in minutes after which an inactive logged in user will be automatically logged out';
 $string['setsiteoptionsfailed'] = 'Failed setting the %s option';
 $string['sitedefault'] = 'Site Default';
 $string['sitelanguagedescription'] = 'The default language for the site';
 $string['sitename'] = 'Site name';
-$string['sitenamedescription'] = ' ';
-$string['siteoptions'] = 'Site options';
+$string['sitenamedescription'] = 'The site name appears in certain places around the site and in e-mails sent from the site';
+$string['siteoptionspagedescription'] = 'Here you can set some global options that will apply by default throughout the entire site.';
 $string['siteoptionsset'] = 'Site options have been updated';
-$string['sitethemedescription'] = ' ';
+$string['sitethemedescription'] = 'The default theme for the site';
 $string['theme'] = 'Theme';
 $string['updatesiteoptions'] = 'Update site options';
 $string['usersallowedmultipleinstitutions'] = 'Users allowed multiple institutions';
 $string['usersallowedmultipleinstitutionsdescription'] = 'If checked, users can be members of several institutions at the same time';
 $string['viruschecking'] = 'Virus checking';
 $string['viruscheckingdescription'] = 'If checked, virus checking will be enabled for all uploaded files using ClamAV';
-$string['searchplugin'] = 'Search plugin';
-$string['searchplugindescription'] = 'Search plugin to use';
-
-// Networking options
-$string['networkingextensionsmissing'] = 'Sorry, you cannot configure Mahara networking because your PHP installation is missing one or more required extensions:';
-$string['publickey'] = 'Public key';
-$string['publickeydescription'] = 'This public key is automatically generated, and rotated every 28 days.';
-$string['publickeyexpires'] = 'Public key expires';
-$string['enablenetworkingdescription'] = 'Allow your Mahara server to communicate with servers running Moodle and other applications.';
-$string['enablenetworking'] = 'Enable networking';
-$string['networkingenabled'] = 'Networking has been enabled. ';
-$string['networkingdisabled'] = 'Networking has been disabled. ';
-$string['networkingunchanged'] = 'Network settings were not changed';
-$string['promiscuousmode'] = 'Auto-register all hosts';
-$string['promiscuousmodedisabled'] = 'Auto-register has been disabled. ';
-$string['promiscuousmodeenabled'] = 'Auto-register has been enabled. ';
-$string['promiscuousmodedescription'] = 'Create an institution record for any host that connects to you, and allow its users to log on to Mahara';
-$string['wwwroot'] = 'WWW Root';
-$string['whatisnetworking'] = 'What is Networking?';
-$string['whatnetworkingis'] = 'Mahara\'s networking features allow it to communicate with Mahara or Moodle sites running on the same or another machine. If networking is enabled, you can use it to configure single-sign-on for users who log in at either Moodle or Mahara.';
-
-// Admin links and resources editor
-//$string['menueditor']    = 'Menu editor';
-$string['adminfile']           = 'Admin file';
-$string['adminpublicdirname']  = 'public';  // Name of the directory in which to store public admin files
-$string['adminpublicdirdescription'] = 'Files accessible by logged out users';
-$string['badmenuitemtype']     = 'Unknown item type';
-$string['confirmdeletemenuitem'] = 'Do you really want to delete this item?';
-$string['externallink']        = 'External link';
-$string['type']                = 'Type';
-$string['name']                = 'Name';
-$string['noadminfiles']        = 'No admin files available';
-$string['linkedto']            = 'Linked to';
-$string['editmenus']           = 'Edit links and resources';
-$string['menuitemsaved']       = 'Item saved';
-$string['savingmenuitem']      = 'Saving item';
-$string['menuitemsloaded']     = 'Items loaded';
-$string['deletingmenuitem']    = 'Deleting item';
-$string['deletefailed']        = 'Failed deleting item';
-$string['menuitemdeleted']     = 'Item deleted';
-$string['loadingmenuitems']    = 'Loading items';
-$string['loadmenuitemsfailed'] = 'Failed to load items';
-$string['loggedinmenu']        = 'Logged in links and resources';
-$string['loggedoutmenu']       = 'Public links and resources';
-$string['public']              = 'public';
 
 // Site content
 $string['about']               = 'About';
 $string['discardpageedits']    = 'Discard your changes to this page?';
-$string['editsitecontent']     = 'Edit site content';
+$string['editsitepagespagedescription'] = 'Here you can edit the content of some pages around the site, such as the homepage (for logged in and out users separately), and the pages linked to in the footer.';
 $string['home']                = 'Home';
-$string['loadingpagecontent']  = 'Loading site page content';
 $string['loadsitepagefailed']  = 'Failed to load site page';
 $string['loggedouthome']       = 'Logged out home';
-$string['pagecontents']        = ' ';
 $string['pagename']            = 'Page name';
 $string['pagesaved']           = 'Page saved';
 $string['pagetext']            = 'Page text';
@@ -185,6 +146,52 @@ $string['savefailed']          = 'Save failed';
 $string['sitepageloaded']      = 'Site page loaded';
 $string['termsandconditions']  = 'Terms and Conditions';
 $string['uploadcopyright']     = 'Upload Copyright Statement';
+
+// Links and resources menu editor
+$string['adminfile']           = 'Admin file';
+$string['adminpublicdirname']  = 'public';  // Name of the directory in which to store public admin files
+$string['adminpublicdirdescription'] = 'Files accessible by logged out users';
+$string['badmenuitemtype']     = 'Unknown item type';
+$string['confirmdeletemenuitem'] = 'Do you really want to delete this item?';
+$string['deletingmenuitem']    = 'Deleting item';
+$string['deletefailed']        = 'Failed deleting item';
+$string['externallink']        = 'External link';
+$string['editmenus']           = 'Edit links and resources';
+$string['linkedto']            = 'Linked to';
+$string['linksandresourcesmenupagedescription'] = 'The Links and Resources Menu appears to all users on most pages. You can add links to other websites and to files uploaded to the %sAdmin Files%s section.';
+$string['loadingmenuitems']    = 'Loading items';
+$string['loadmenuitemsfailed'] = 'Failed to load items';
+$string['loggedinmenu']        = 'Logged in links and resources';
+$string['loggedoutmenu']       = 'Public links and resources';
+$string['menuitemdeleted']     = 'Item deleted';
+$string['menuitemsaved']       = 'Item saved';
+$string['menuitemsloaded']     = 'Items loaded';
+$string['name']                = 'Name';
+$string['noadminfiles']        = 'No admin files available';
+$string['public']              = 'public';
+$string['savingmenuitem']      = 'Saving item';
+$string['type']                = 'Type';
+
+// Admin Files
+$string['adminfilespagedescription'] = 'Here you can upload files that can be included in the %sLinks and Resources Menu%s. Files in the home directory will be able to be added to the logged in menu, while files in the public directory will be able to be added to the public menu.';
+
+// Networking options
+$string['networkingextensionsmissing'] = 'Sorry, you cannot configure Mahara networking because your PHP installation is missing one or more required extensions:';
+$string['publickey'] = 'Public key';
+$string['publickeydescription'] = 'This public key is automatically generated, and rotated every 28 days';
+$string['publickeyexpires'] = 'Public key expires';
+$string['enablenetworkingdescription'] = 'Allow your Mahara server to communicate with servers running Moodle and other applications';
+$string['enablenetworking'] = 'Enable networking';
+$string['networkingenabled'] = 'Networking has been enabled. ';
+$string['networkingdisabled'] = 'Networking has been disabled. ';
+$string['networkingpagedescription'] = 'Mahara\'s networking features allow it to communicate with Mahara or Moodle sites running on the same or another machine. If networking is enabled, you can use it to configure single-sign-on for users who log in at either Moodle or Mahara.';
+$string['networkingunchanged'] = 'Network settings were not changed';
+$string['promiscuousmode'] = 'Auto-register all hosts';
+$string['promiscuousmodedisabled'] = 'Auto-register has been disabled. ';
+$string['promiscuousmodeenabled'] = 'Auto-register has been enabled. ';
+$string['promiscuousmodedescription'] = 'Create an institution record for any host that connects to you, and allow its users to log on to Mahara';
+$string['wwwroot'] = 'WWW Root';
+$string['wwwrootdescription'] = 'This is the URL at which your users access this Mahara installation, and the URL the SSL keys are generated for';
 
 // Upload CSV
 $string['csvfile'] = 'CSV File';
@@ -292,12 +299,6 @@ $string['admininstitutions'] = 'Admininster Institutions';
 $string['adminauthorities'] = 'Admininster Authorities';
 $string['addinstitution'] = 'Add Institution';
 $string['authplugin'] = 'Authentication plugin';
-$string['defaultaccountinactiveexpire'] = 'Default account inactivity time';
-$string['defaultaccountinactiveexpiredescription'] = 'How long a user account will remain active without the user logging in';
-$string['defaultaccountinactivewarn'] = 'Warning time for inactivity/expiry';
-$string['defaultaccountinactivewarndescription'] = 'The time before user accounts are to expire or become inactive at which a warning message will be sent to them';
-$string['defaultaccountlifetime'] = 'Default account lifetime';
-$string['defaultaccountlifetimedescription'] = 'How long newly created user accounts will be usable for before they expire';
 $string['deleteinstitution'] = 'Delete Institution';
 $string['deleteinstitutionconfirm'] = 'Are you really sure you wish to delete this institution?';
 $string['institutionaddedsuccessfully'] = 'Institution added successfully. Please configure an authentication plugin for this institution.';
