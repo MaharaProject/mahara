a1e35522a66cf14a49298f5ee5b754c5ff7d8f2f
Clare Lenihan <clare@catalyst.net.nz>
2008-02-05 15:46:47 +1300

    fixing order

--- htdocs/user/myfriends.php
+++ htdocs/user/myfriends.php
@@ -73,7 +73,7 @@ else {
             UNION SELECT usr1 AS id, \'2\' AS status FROM {usr_friend} WHERE usr2 = ?
         ) f
         JOIN {usr} u ON f.id = u.id
-        ORDER BY status DESC, firstname, lastname, u.id
+        ORDER BY status, firstname, lastname, u.id
         LIMIT ?
         OFFSET ?', array($userid, $userid, $userid, $limit, $offset)
     );
