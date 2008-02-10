f8f4d47c6a0edcd20dc2214d83a4dbc1b62f5724
Clare Lenihan <clare@catalyst.net.nz>
2008-02-11 09:46:31 +1300

    changing access start/stop to below other access elements, and adding descriptions of what they do

--- htdocs/lang/en.utf8/view.php
+++ htdocs/lang/en.utf8/view.php
@@ -132,7 +132,10 @@ $string['deleteviewconfirm'] = 'Do you really want to delete this view? It canno
 
 $string['editaccesspagedescription'] = '<p>You can control who can see your view, and when they can do so. By default, only you can see your views.</p>
     <p>You can grant access to your view to your friends, all logged in users, or only to the certain users and groups you choose.</p>
-    <p>All dates are optional. If you wish, you can use them to restrict the time in which people can see your view. Access start and end further restrict when peope can see your view.</p>';
+    <p>All dates are optional. If you wish, you can use them to restrict the time in which people can see your view.</p>';
+$string['startdatedescription'] = 'If set, no one can access your view before this time, even if you give them access.';
+$string['stopdatedescription'] = 'If set, no one can access your view after this time, even if you give them access.';
+
 $string['emptylabel'] = 'Click here to enter text for this label';
 $string['empty_block'] = 'Select an artefact from the tree on the left to place here';
 
