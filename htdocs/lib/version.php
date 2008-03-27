0b3e7b933bad44dc8848563c5d4b1bc6462a5749
Nigel McNie <nigel@catalyst.net.nz>
2008-03-28 09:48:46 +1300

    Version bump for 1.0.0rc2dev

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,7 +28,7 @@ defined('INTERNAL') || die();
 
 $config = new StdClass;
 $config->version = 2008032800;
-$config->release = '1.0.0rc1';
+$config->release = '1.0.0rc2dev';
 $config->minupgradefrom = 2007080700;
 $config->minupgraderelease = '0.8.0 (release tag 0.8.0_RELEASE)';
 $config->disablelogin = true;
