8f9aa61117f9bfe6e0b3fc4667eb888af2a39d85
Nigel McNie <nigel@catalyst.net.nz>
2008-04-22 14:24:07 +1200

    A capitalisation fix. Added a sentence to step three to say how to continue.

--- htdocs/lang/en.utf8/view.php
+++ htdocs/lang/en.utf8/view.php
@@ -132,7 +132,8 @@ $string['cantdeleteview'] = 'You cannot delete this View';
 $string['deletespecifiedview'] = 'Delete View "%s"';
 $string['deleteviewconfirm'] = 'Do you really want to delete this View? It cannot be undone.';
 
-$string['editaccesspagedescription2'] = '<p>By default, only you can see your View. Here you can choose who else you would like to be able to see the information in your View. Click Add to grant access to Public, Logged in Users or Friends. Use the search box to add individual Users or Groups. All those added will appear in the right hand pane, under Added.</p>';
+$string['editaccesspagedescription2'] = '<p>By default, only you can see your View. Here you can choose who else you would like to be able to see the information in your View. Click Add to grant access to Public, Logged in Users or Friends. Use the search box to add individual Users or Groups. All those added will appear in the right hand pane, under Added.</p>
+<p>Once you are done, scroll down and click Save to continue.</p>';
 
 $string['overridingstartstopdate'] = 'Overriding Start/Stop Dates';
 $string['overridingstartstopdatesdescription'] = 'If you want, you can set an overriding start and/or stop date. Other people will not be able to see your View before the start date and after the end date, regardless of any other access you have granted.';
@@ -158,7 +159,7 @@ $string['confirmcancelcreatingview'] = 'This View has not been completed. Do you
 
 // view control stuff
 
-$string['editblockspagedescription'] = '<p>Choose from the tabs below to see what blocks you can display in your view. You can drag and drop the blocks in to your View layout. Select the ? Icon for more information.</p>';
+$string['editblockspagedescription'] = '<p>Choose from the tabs below to see what blocks you can display in your View. You can drag and drop the blocks in to your View layout. Select the ? Icon for more information.</p>';
 $string['displaymyview'] = 'Display my View';
 $string['editthisview'] = 'Edit this View';
 
