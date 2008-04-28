d29918c23ba7be1f36453ef11626c4820e6b2929
Nigel McNie <nigel@catalyst.net.nz>
2008-04-29 09:31:59 +1200

    Version bump for 1.0.3testing.

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,7 +28,7 @@ defined('INTERNAL') || die();
 
 $config = new StdClass;
 $config->version = 2008040202;
-$config->release = '1.0.2';
+$config->release = '1.0.3testing';
 $config->minupgradefrom = 2007080700;
 $config->minupgraderelease = '0.8.0 (release tag 0.8.0_RELEASE)';
 $config->disablelogin = true;
