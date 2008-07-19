d14230d397655e17f3a5e78013592cd2e029e791
Nigel McNie <nigel@catalyst.net.nz>
2008-07-19 21:16:18 +1200

    Only show "login as" link if the user is actually an admin for the user being viewed.

--- htdocs/user/view.php
+++ htdocs/user/view.php
@@ -331,7 +331,7 @@ if ($loggedinid != $userid) {
 
 }
 
-if ($userid != $USER->get('id') && is_null($USER->get('parentuser'))) {
+if ($userid != $USER->get('id') && $USER->is_admin_for_user($userid) && is_null($USER->get('parentuser'))) {
     $loginas = get_string('loginasuser', 'admin', $user->username);
 } else {
     $loginas = null;
