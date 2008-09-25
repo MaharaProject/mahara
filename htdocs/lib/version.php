5731bc20674aa6a51aa3138456b38097648fc2c0
Penny Leach <penny@mjollnir.org>
2008-09-25 09:00:30 +0100

    post 1.0.5-relase version & release bump

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -27,8 +27,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2008040207;
-$config->release = '1.0.5';
+$config->version = 2008040208;
+$config->release = '1.0.6testing';
 $config->minupgradefrom = 2007080700;
 $config->minupgraderelease = '0.8.0 (release tag 0.8.0_RELEASE)';
 $config->disablelogin = true;
