64e5c785040f4ca05e6667c2efd0d48cd191f453
Nigel McNie <nigel@catalyst.net.nz>
2008-10-21 10:45:19 +1300

    Fix profile icons being unable to be downloaded. Thanks to sirebral on the forums.
    Related to 7084335d06491cf0a028f45315838d56e4c8f4f8 on 1.0_STABLE but
    reworked as the profileicon artefact is now handled by 'file'.

--- htdocs/artefact/file/lib.php
+++ htdocs/artefact/file/lib.php
@@ -1314,9 +1314,6 @@ class ArtefactTypeProfileIcon extends ArtefactTypeImage {
             . ($this->get('id') % 256) . '/' . $this->get('id'));
 
         $downloadpath = get_config('wwwroot') . 'thumb.php?type=profileiconbyid&id=' . $this->id;
-        if (isset($options['viewid'])) {
-            $downloadpath .= '&id=' . $options['viewid'];
-        }
         $smarty = smarty_core();
         $smarty->assign('iconpath', $this->get_icon($options));
         $smarty->assign('downloadpath', $downloadpath);
