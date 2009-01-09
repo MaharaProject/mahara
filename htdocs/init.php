3f9bdf5cf4a250c733a15fb20006f076d4f922de
Nigel McNie <nigel@catalyst.net.nz>
2009-01-09 13:48:22 +1300

    Prevent infinite loop when installing on Windows/IIS. Thanks to Inaki Arenaza.

--- htdocs/init.php
+++ htdocs/init.php
@@ -207,10 +207,11 @@ if (!defined('INSTALLER')) {
 }
 
 // check to see if we're installed...
+$scriptfilename = str_replace('\\', '/', $_SERVER['SCRIPT_FILENAME']);
 if (!get_config('installed')
-    && false === strpos($_SERVER['SCRIPT_FILENAME'], 'admin/index.php')
-    && false === strpos($_SERVER['SCRIPT_FILENAME'], 'admin/upgrade.php')
-    && false === strpos($_SERVER['SCRIPT_FILENAME'], 'admin/upgrade.json.php')) {
+    && false === strpos($scriptfilename, 'admin/index.php')
+    && false === strpos($scriptfilename, 'admin/upgrade.php')
+    && false === strpos($scriptfilename, 'admin/upgrade.json.php')) {
     redirect('/admin/');
 }
 
