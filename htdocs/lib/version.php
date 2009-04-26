1da411afbc07ef925860f5cb26eac9bb144594f0
Richard Mansfield <richardm@catalyst.net.nz>
2009-04-27 11:35:38 +1200

    Version bump for 1.2.0alpha1

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -27,8 +27,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2009040900;
-$config->release = '1.2.0dev';
+$config->version = 2009042700;
+$config->release = '1.2.0alpha1';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
