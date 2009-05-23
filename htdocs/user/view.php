cac2f1616a241900fa3644bf0353c8ab3db9ea79
Nigel McNie <nigel@catalyst.net.nz>
2009-05-24 00:35:21 +1200

    Move rules for internal artefact plugin to their own stylesheet.
    Tweak relevant pages so they include those rules - including
    user/view.php which should really be provided by artefact/internal.

--- htdocs/user/view.php
+++ htdocs/user/view.php
@@ -25,6 +25,12 @@
  */
 define('INTERNAL', 1);
 define('PUBLIC', 1);
+// Technically these are lies, but we set them like this to hook in the right 
+// plugin stylesheet. This file should be provided by artefact/internal anyway.
+define('SECTION_PLUGINTYPE', 'artefact');
+define('SECTION_PLUGINNAME', 'internal');
+define('SECTION_PAGE', 'view');
+
 require(dirname(dirname(__FILE__)).'/init.php');
 require_once('group.php');
 require_once('pieforms/pieform.php');
