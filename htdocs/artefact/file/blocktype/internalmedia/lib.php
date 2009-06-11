5bb150732a6b8880b285ddab35a3ec69a2111299
Richard Mansfield <richardm@catalyst.net.nz>
2009-06-11 13:18:39 +1200

    Center contents of internalmedia block to make it consistent with external

--- htdocs/artefact/file/blocktype/internalmedia/lib.php
+++ htdocs/artefact/file/blocktype/internalmedia/lib.php
@@ -71,7 +71,7 @@ class PluginBlocktypeInternalmedia extends PluginBlocktype {
             return get_string('typeremoved', 'blocktype.file/internalmedia');
         }
         $callbacks = self::get_all_filetype_players();
-        $result .= '<div class="mediaplayer-container"><div class="mediaplayer">' . call_static_method('PluginBlocktypeInternalmedia', $callbacks[$mimetypefiletypes[$mimetype]], $artefact, $instance, $width, $height) . '</div></div>';
+        $result .= '<div class="mediaplayer-container center"><div class="mediaplayer">' . call_static_method('PluginBlocktypeInternalmedia', $callbacks[$mimetypefiletypes[$mimetype]], $artefact, $instance, $width, $height) . '</div></div>';
         return $result;
     }
 
