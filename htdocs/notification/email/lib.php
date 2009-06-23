0b15858d74b2db1e2171b9216fe53b315a2ce003
Nigel McNie <nigel@catalyst.net.nz>
2009-06-23 17:59:47 +1200

    Allow notifications to override the message contents for email notifications entirely if they choose.

--- htdocs/notification/email/lib.php
+++ htdocs/notification/email/lib.php
@@ -32,39 +32,42 @@ class PluginNotificationEmail extends PluginNotification {
 
     public static function notify_user($user, $data) {
 
-        $lang = (empty($user->lang) || $user->lang == 'default') ? get_config('lang') : $user->lang;
-        $separator = str_repeat('-', 72);
-
-        $sitename = get_config('sitename');
-        $subject = get_string_from_language($lang, 'emailsubject', 'notification.email', $sitename);
-        if (!empty($data->subject)) {
-            $subject .= ': ' . $data->subject;
-        }
-
-        $messagebody = get_string_from_language($lang, 'emailheader', 'notification.email', $sitename) . "\n";
-        $messagebody .= $separator . "\n\n";
-
-        $messagebody .= get_string_from_language($lang, 'subject') . ': ' . $data->subject . "\n\n";
-        if ($data->activityname == 'usermessage') {
-            // Do not include the message body in user messages when they are sent by email
-            // because it encourages people to reply to the email.
-            $messagebody .= get_string_from_language($lang, 'newusermessageemailbody', 'group', display_name($data->userfrom), $data->url);
+        if (!empty($data->overridemessagecontents)) {
+            $subject = $data->subject;
+            $messagebody = $data->message;
         }
         else {
-            $messagebody .= $data->message;
-            if (!empty($data->url)) {
-                $messagebody .= "\n\n" . get_string_from_language($lang, 'referurl', 'notification.email', $data->url);
+            $lang = (empty($user->lang) || $user->lang == 'default') ? get_config('lang') : $user->lang;
+            $separator = str_repeat('-', 72);
+
+            $sitename = get_config('sitename');
+            $subject = get_string_from_language($lang, 'emailsubject', 'notification.email', $sitename);
+            if (!empty($data->subject)) {
+                $subject .= ': ' . $data->subject;
             }
+
+            $messagebody = get_string_from_language($lang, 'emailheader', 'notification.email', $sitename) . "\n";
+            $messagebody .= $separator . "\n\n";
+
+            $messagebody .= get_string_from_language($lang, 'subject') . ': ' . $data->subject . "\n\n";
+            if ($data->activityname == 'usermessage') {
+                // Do not include the message body in user messages when they are sent by email
+                // because it encourages people to reply to the email.
+                $messagebody .= get_string_from_language($lang, 'newusermessageemailbody', 'group', display_name($data->userfrom), $data->url);
+            }
+            else {
+                $messagebody .= $data->message;
+                if (!empty($data->url)) {
+                    $messagebody .= "\n\n" . get_string_from_language($lang, 'referurl', 'notification.email', $data->url);
+                }
+            }
+
+            $messagebody .= "\n\n$separator";
+
+            $prefurl = get_config('wwwroot') . 'account/activity/preferences/';
+            $messagebody .=  "\n\n" . get_string_from_language($lang, 'emailfooter', 'notification.email', $sitename, $prefurl);
         }
 
-        if (isset($data->unsubscribeurl) && isset($data->unsubscribename)) {
-            $messagebody .= "\n\n" . get_string_from_language($lang, 'unsubscribemessage', 'notification.email', $data->unsubscribename, $data->unsubscribeurl);
-        }
-
-        $messagebody .= "\n\n$separator";
-
-        $prefurl = get_config('wwwroot') . 'account/activity/preferences/';
-        $messagebody .=  "\n\n" . get_string_from_language($lang, 'emailfooter', 'notification.email', $sitename, $prefurl);
         email_user($user, null, $subject, $messagebody, null, !empty($data->customheaders) ? $data->customheaders : null);
     }
 }
