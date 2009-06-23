MaharaProject/mahara

commit 1bc1fee40e6338556a80712894372ac5bdf675ef

Call pieform before smarty to avoid js error on admin page

htdocs/admin/index.php
@@ -41,9 +41,13 @@ else {
 require(get_config('libroot') . 'upgrade.php');
 require(get_config('libroot') . 'registration.php');
 
-$smarty = smarty();
+if (!get_config('registration_lastsent')) {
+    $register = register_site();
+}
 
 $upgrades = check_upgrades();
+
+$smarty = smarty();
 
 if (isset($upgrades['core']) && !empty($upgrades['core']->install)) {
     $smarty->assign('installing', true);
@@ -55,8 +59,7 @@ if (isset($upgrades['core']) && !empty($upgrades['core']->install)) {
 // normal admin page starts here
 $smarty->assign('upgrades', $upgrades);
 
-if (!get_config('registration_lastsent')) {
-    $register = register_site();
+if (isset($register)) {
     $smarty->assign('register', $register);
 }
 

commit 77deec6a7e68be3f6e925dc8e69fc25c76f80bc0

Fix installation broken by previous commit 1bc1fee4

htdocs/admin/index.php
@@ -41,20 +41,21 @@ else {
 require(get_config('libroot') . 'upgrade.php');
 require(get_config('libroot') . 'registration.php');
 
-if (!get_config('registration_lastsent')) {
-    $register = register_site();
-}
-
 $upgrades = check_upgrades();
 
-$smarty = smarty();
-
 if (isset($upgrades['core']) && !empty($upgrades['core']->install)) {
+    $smarty = smarty();
     $smarty->assign('installing', true);
     $smarty->assign('releaseargs', array($upgrades['core']->torelease, $upgrades['core']->to));
     $smarty->display('admin/installgpl.tpl');
     exit;
 }
+
+if (!get_config('registration_lastsent')) {
+    $register = register_site();
+}
+
+$smarty = smarty();
 
 // normal admin page starts here
 $smarty->assign('upgrades', $upgrades);