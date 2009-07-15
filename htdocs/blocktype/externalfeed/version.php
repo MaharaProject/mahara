0a2e677b632893e14a4d84c13898c05a817556dc
Ben Bradshaw <ben@catalyst.net.nz>
2009-07-15 17:18:29 +1200

    [#3268] Max RSS Feed length is now 2048 chars MySQL installs will fail if 2 RSS feeds with the first same 255 chars are added but that's tough. Postgres will be fine.

--- htdocs/blocktype/externalfeed/version.php
+++ htdocs/blocktype/externalfeed/version.php
@@ -27,7 +27,7 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2008042100;
-$config->release = '1.0.1';
+$config->version = 2008042101;
+$config->release = '1.0.2';
 
 ?>
