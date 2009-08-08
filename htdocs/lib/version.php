f80a262a2623bef686d11a100ccba6e668023ca4
Nigel McNie <nigel@catalyst.net.nz>
2009-08-09 00:27:53 +1200

    Version bump for view theme upgrade.

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -27,7 +27,7 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2009080601;
+$config->version = 2009080800;
 $config->release = '1.2.0alpha4dev';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
