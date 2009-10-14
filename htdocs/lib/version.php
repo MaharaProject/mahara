4aaa5b7dfa947b3a04e983a4196bd0f477cc0a0a
Nigel McNie <nigel@catalyst.net.nz>
2009-10-15 03:36:03 +1300

    Version bump for 1.2.0beta5dev

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,8 +28,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2009101500;
-$config->release = '1.2.0beta4';
+$config->version = 2009101501;
+$config->release = '1.2.0beta5dev';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
