82012f8ac065d2415674f53607b4f03449bd9b60
Nigel McNie <nigel@catalyst.net.nz>
2009-11-16 15:54:39 +1300

    Version bump for 1.2.1testing

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,8 +28,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2009111002;
-$config->release = '1.2.0';
+$config->version = 2009111003;
+$config->release = '1.2.1testing';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
