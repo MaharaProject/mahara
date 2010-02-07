cf399ce318be2bb359c429567f327f03462a5fc3
Francois Marier <francois@catalyst.net.nz>
2010-02-08 10:48:53 +1300

    Version bump for 1.2.4testing

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,8 +28,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2009111010;
-$config->release = '1.2.3';
+$config->version = 2009111011;
+$config->release = '1.2.4testing';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
