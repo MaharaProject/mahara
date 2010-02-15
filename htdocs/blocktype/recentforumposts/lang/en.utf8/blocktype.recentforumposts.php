f615c9583df1b14c4b10b88b5adf19c8c749582a
Nigel McNie <nigel@mcnie.name>
2010-02-15 17:15:26 +1300

    Recent forum posts blocktype
    Signed-off-by: Richard Mansfield <richardm@catalyst.net.nz>

---
+++ htdocs/blocktype/recentforumposts/lang/en.utf8/blocktype.recentforumposts.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2009 Nigel McNie (http://nigel.mcnie.name/)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage blocktype-recentforumposts
+ * @author     Nigel McNie
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2009 Nigel McNie http://nigel.mcnie.name/
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'Recent Forum Posts for <group>';
+$string['description'] = 'Display recent forum posts for a group';
+
+$string['group'] = 'Group';
+$string['nogroupstochoosefrom'] = 'Sorry, no groups to choose from';
+$string['poststoshow'] = 'Maximum number of posts to show';
+$string['poststoshowdescription'] = 'Between 1 and 100';
+$string['recentforumpostsforgroup'] = "Recent Forum Posts for %s";
+
+?>
---
+++ htdocs/blocktype/recentforumposts/theme/raw/recentforumposts.tpl
@@ -0,0 +1,17 @@
+                <div class="group-info-para">
+                {if $foruminfo}
+                {foreach from=$foruminfo item=postinfo}
+                <div>
+                  <h4><a href="{$WWWROOT}interaction/forum/topic.php?id={$postinfo->topic|escape}#post{$postinfo->id|escape}">{$postinfo->topicname|escape}</a></h4>
+                  <div>
+                    <img src="{$WWWROOT}thumb.php?type=profileicon&amp;maxsize=20&amp;id={$postinfo->poster|escape}" alt="">
+                    <a href="{$WWWROOT}user/view.php?id={$postinfo->poster|escape}">{$postinfo->poster|display_name|escape}</a>
+                  </div>
+                  <p>{$postinfo->body|str_shorten_html:100:true}</p>
+                </div>
+                {/foreach}
+                {else}
+                <p>{str tag=noforumpostsyet section=interaction.forum}</p>
+                {/if}
+                <p><a href="{$WWWROOT}interaction/forum/?group={$group->id|escape}" target="_blank">{str tag=gotoforums section=interaction.forum} &raquo;</a></p>
+                </div>
