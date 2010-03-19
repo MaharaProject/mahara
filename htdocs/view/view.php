2e930abbfe8f59f1e0102ca57a7a09c327d55493
Andrew Robert Nicols <andrew.nicols@luns.net.uk>
2010-03-19 11:10:26 +0000

    Show the 'submit to group' pieform within a view.
    Feedback we've had from some of our users suggests that having the ability
    to submit a view to a course group in other locations (e.g. within a view,
    and within the relevant group) would make a lot of sense as it reduces
    context switches for users.
    
    Signed-off-by: Andrew Robert Nicols <andrew.nicols@luns.net.uk>
    Signed-off-by: Evan Goldenberg <evang@catalyst.net.nz>

--- htdocs/view/view.php
+++ htdocs/view/view.php
@@ -274,6 +274,13 @@ if (isset($objectionform)) {
 }
 $smarty->assign('viewbeingwatched', $viewbeingwatched);
 
+if ($tutorgroupdata = group_get_user_course_groups()) {
+    $options = array();
+    if (!$view->get('submittedgroup') && !$view->get('submittedhost')) {
+        $smarty->assign('view_group_submission_form', view_group_submission_form($view->get('id'), $tutorgroupdata));
+    }
+}
+
 $smarty->display('view/view.tpl');
 
 ?>
