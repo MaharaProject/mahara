583a6f9d06ed62b7f6fd2a2beb8fe14dc9227265
Richard Mansfield <richardm@catalyst.net.nz>
2010-04-06 14:51:26 +1200

    Version bump for 1.2.4

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,8 +28,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2009111012;
-$config->release = '1.2.4testing';
+$config->version = 2009111013;
+$config->release = '1.2.4';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
