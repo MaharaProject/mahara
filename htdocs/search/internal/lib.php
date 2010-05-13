3e2b66797015c6a045b7e4e99d356443c040b2b1
Ruslan Kabalin <ruslan.kabalin@luns.net.uk>
2010-05-13 09:12:45 +0100

    Revert "Made non-public groups invisible in search results."
    As discussed on IRC, it appeared that non-public groups are designed solely to
    control group visibility for people who are not the members of site (not
    logged in users). Thus, the patch is reverted.
    
    This reverts commit 284d8620e9df34c709f92f624c52b34cec97c313.
    
    Signed-off-by: Ruslan Kabalin <ruslan.kabalin@luns.net.uk>

--- htdocs/search/internal/lib.php
+++ htdocs/search/internal/lib.php
@@ -535,13 +535,10 @@ class PluginSearchInternal extends PluginSearch {
         }
 
        if ($type == 'member') {
-            $sql .=  'AND (id IN (' . $grouproles . '))';
+            $sql .=  'AND id IN (' . $grouproles . ')';
         }
         else if ($type == 'notmember') {
-            $sql .= 'AND public = 1 AND (id NOT IN (' . $grouproles . '))';
-        }
-        else {
-            $sql .= 'AND (public = 1 OR id IN (' . $grouproles . '))';
+            $sql .= 'AND id NOT IN (' . $grouproles . ')';
         }
 
         $count = get_field_sql('SELECT COUNT(*) '.$sql, $values);
