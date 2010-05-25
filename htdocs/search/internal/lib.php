61e079ff9b91a6934d3889cdb63ef489aea856a0
Richard Mansfield <richardm@catalyst.net.nz>
2010-05-25 18:13:57 +1200

    Run clean_html over summaries in portfolio self search results
    Signed-off-by: Richard Mansfield <richardm@catalyst.net.nz>
    (cherry picked from commit f30e88c1a959685a26df7eb47d5f10d9be168952)

--- htdocs/search/internal/lib.php
+++ htdocs/search/internal/lib.php
@@ -822,6 +822,7 @@ class PluginSearchInternal extends PluginSearch {
                 else {
                     $newresult['summary'] = $newresult['description'];
                 }
+                $newresult['summary'] = clean_html($newresult['summary']);
                 $result = $newresult;
             }
 
