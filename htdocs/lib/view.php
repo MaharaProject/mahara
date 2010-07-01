MaharaProject/mahara

commit 9fedce812984dc6c7506072934a2cabf4fe5bcf8

Don't return system user's views in view search
Signed-off-by: Richard Mansfield <richardm@catalyst.net.nz>

htdocs/lib/view.php
@@ -2302,7 +2302,7 @@ class View {
         if (!$admin) { // only show the grouphomepage viewtype to admins
             $where .= ",'grouphomepage'";
         }
-        $where .= ")";
+        $where .= ") AND v.owner != 0";
 
         if ($ownedby) {
             $where .= ' AND v.' . self::owner_sql($ownedby);

commit 5e30b486b35749196f6907d44051a5ab4c18f655

Fix view_search (broken for group views by 9fedce8129)
Signed-off-by: Richard Mansfield <richardm@catalyst.net.nz>

htdocs/lib/view.php
@@ -2303,7 +2303,7 @@ class View {
         if (!$admin) { // only show the grouphomepage viewtype to admins
             $where .= ",'grouphomepage'";
         }
-        $where .= ") AND v.owner != 0";
+        $where .= ") AND (v.owner IS NULL OR v.owner > 0)";
 
         if ($ownedby) {
             $where .= ' AND v.' . self::owner_sql($ownedby);