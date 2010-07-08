67af209bdc316e138a7e8a57f74f028263c266e8
Francois Marier <francois@catalyst.net.nz>
2010-07-08 17:30:40 +1200

    Version bump for 1.3.0beta2

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,8 +28,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2010070700;
-$config->release = '1.3.0beta2dev';
+$config->version = 2010070800;
+$config->release = '1.3.0beta2';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
