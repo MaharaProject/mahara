e84126fb538117d9c9ff5da47defeb747888b82c
Francois Marier <francois@catalyst.net.nz>
2010-07-15 16:34:47 +1200

    jscalendar: replace Japanase translation
    Deleted the one that comes with jscalendar and replaced it with a
    better one done by Mits.
    
    Also got rid of the incomplete Slovenian translation which was
    already superseded by Gregor's one.
    
    LP: #597909
    
    Signed-off-by: Francois Marier <francois@catalyst.net.nz>

---
+++ htdocs/js/jscalendar/lang/calendar-ja.js
@@ -0,0 +1,126 @@
+// ** I18N
+
+// Calendar JA language
+// Author: Mitsuhiro Yoshida, <mits@mitstek.com>
+// Encoding: UTF-8
+// Distributed under the same terms as the calendar itself.
+
+// For translators: please use UTF-8 if possible.  We strongly believe that
+// Unicode is the answer to a real internationalized world.  Also please
+// include your contact information in the header, as can be seen above.
+
+// full day names
+Calendar._DN = new Array
+("日曜日",
+ "月曜日",
+ "火曜日",
+ "水曜日",
+ "木曜日",
+ "金曜日",
+ "土曜日",
+ "日曜日");
+
+// Please note that the following array of short day names (and the same goes
+// for short month names, _SMN) isn't absolutely necessary.  We give it here
+// for exemplification on how one can customize the short day names, but if
+// they are simply the first N letters of the full name you can simply say:
+//
+//   Calendar._SDN_len = N; // short day name length
+//   Calendar._SMN_len = N; // short month name length
+//
+// If N = 3 then this is not needed either since we assume a value of 3 if not
+// present, to be compatible with translation files that were written before
+// this feature.
+
+// short day names
+Calendar._SDN = new Array
+("日",
+ "月",
+ "火",
+ "水",
+ "木",
+ "金",
+ "土",
+ "日");
+
+// First day of the week. "0" means display Sunday first, "1" means display
+// Monday first, etc.
+Calendar._FD = 0;
+
+// full month names
+Calendar._MN = new Array
+("1月",
+ "2月",
+ "3月",
+ "4月",
+ "5月",
+ "6月",
+ "7月",
+ "8月",
+ "9月",
+ "10月",
+ "11月",
+ "12月");
+
+// short month names
+Calendar._SMN = new Array
+("1月",
+ "2月",
+ "3月",
+ "4月",
+ "5月",
+ "6月",
+ "7月",
+ "8月",
+ "9月",
+ "10月",
+ "11月",
+ "12月");
+
+// tooltips
+Calendar._TT = {};
+Calendar._TT["INFO"] = "About the calendar";
+
+Calendar._TT["ABOUT"] =
+"Date/Time Selector\n" +
+"(c) dynarch.com 2002-2005 / Author: Mihai Bazon\n" + // don't translate this this ;-)
+"Distributed under GNU LGPL.  See http://gnu.org/licenses/lgpl.html for details." +
+"\n\n" +
+"Date selection:\n" +
+"- Use the \xab, \xbb buttons to select year\n" +
+"- Use the " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " buttons to select month\n" +
+"- Hold mouse button on any of the above buttons for faster selection.";
+Calendar._TT["ABOUT_TIME"] = "\n\n" +
+"Time selection:\n" +
+"- Click on any of the time parts to increase it\n" +
+"- or Shift-click to decrease it\n" +
+"- or click and drag for faster selection.";
+
+Calendar._TT["PREV_YEAR"] = "前年 (日付を保持する)";
+Calendar._TT["PREV_MONTH"] = "前月 (日付を保持する)";
+Calendar._TT["GO_TODAY"] = "今日に移動する";
+Calendar._TT["NEXT_MONTH"] = "次月 (日付を保持する)";
+Calendar._TT["NEXT_YEAR"] = "次年 (日付を保持する)";
+Calendar._TT["SEL_DATE"] = "日付を選択してください。";
+Calendar._TT["DRAG_TO_MOVE"] = "移動するにはドラッグしてください。";
+Calendar._TT["PART_TODAY"] = " (今日)";
+
+// the following is to inform that "%s" is to be the first day of week
+// %s will be replaced with the day name.
+Calendar._TT["DAY_FIRST"] = "%s を最初に表示する";
+
+// This may be locale-dependent.  It specifies the week-end days, as an array
+// of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
+// means Monday, etc.
+Calendar._TT["WEEKEND"] = "0,6";
+
+Calendar._TT["CLOSE"] = "閉じる";
+Calendar._TT["TODAY"] = "今日";
+Calendar._TT["TIME_PART"] = "時間変更: (Shift-)クリックまたはドラッグ";
+
+// date formats
+Calendar._TT["DEF_DATE_FORMAT"] = "%Y/%m/%d";
+Calendar._TT["TT_DATE_FORMAT"] = "%m月%d日(%a)";
+
+Calendar._TT["WK"] = "週";
+Calendar._TT["TIME"] = "時間:";
