b24dada329af2380a71a589631e2695d02b4aff7
Richard Mansfield <richardm@catalyst.net.nz>
2010-08-31 17:42:10 +1200

    Bump version to 1.4.0dev
    Signed-off-by: Richard Mansfield <richardm@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -29,7 +29,7 @@ defined('INTERNAL') || die();
 
 $config = new StdClass;
 $config->version = 2010082000;
-$config->release = '1.3.0beta5dev';
+$config->release = '1.4.0dev';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
