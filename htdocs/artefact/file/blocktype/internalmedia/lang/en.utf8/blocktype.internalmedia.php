0094848395ee58191dc805d136d51ae7d1368872
Rich Trott <Richard.Trott@ucsf.edu>
2010-12-22 10:24:07 +1300

    internal media lang string: fix inconsistency
    Bug #693149
    
    Signed-off-by: Rich Trott <Richard.Trott@ucsf.edu>
    Signed-off-by: Francois Marier <francois@catalyst.net.nz>

--- htdocs/artefact/file/blocktype/internalmedia/lang/en.utf8/blocktype.internalmedia.php
+++ htdocs/artefact/file/blocktype/internalmedia/lang/en.utf8/blocktype.internalmedia.php
@@ -34,5 +34,4 @@ $string['media'] = 'Media';
 $string['flashanimation'] = 'Flash animation';
 
 $string['typeremoved'] = 'This block points to a media type that has been disallowed by the administrator';
-$string['configdesc'] = 'Configure which file types users can embed into this block.  If you disable a filetype that has already been used in a block, it will not be rendered anymore';
-?>
+$string['configdesc'] = 'Configure which file types users can embed into this block.  If you disable a filetype that has already been used in a block, it will not be rendered any more';
