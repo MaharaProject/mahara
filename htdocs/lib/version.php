a85844c353bf2b8c31f93bdbc675f7ecd0e5161b
Richard Mansfield <richard.mansfield@catalyst.net.nz>
2011-01-28 14:22:28 +1300

    Version bump for 1.4.0alpha1
    Signed-off-by: Richard Mansfield <richard.mansfield@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,8 +28,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2011012800;
-$config->release = '1.4.0dev';
+$config->version = 2011012801;
+$config->release = '1.4.0alpha1';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
