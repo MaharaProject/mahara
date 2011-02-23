cc45c629f3c1f67c52e51f9fc4837d6704a0f7c4
Richard Mansfield <richard.mansfield@catalyst.net.nz>
2011-02-24 10:37:50 +1300

    Update tinymce language packs
    Signed-off-by: Richard Mansfield <richard.mansfield@catalyst.net.nz>

--- htdocs/js/tinymce/plugins/advhr/langs/no_dlg.js
+++ htdocs/js/tinymce/plugins/advhr/langs/no_dlg.js
@@ -1,5 +1,1 @@
-tinyMCE.addI18n('no.advhr_dlg',{
-width:"Bredde",
-size:"St\u00F8rrelse",
-noshade:"Ingen skygge"
-});
+tinyMCE.addI18n('no.advhr_dlg',{size:"H\u00f8yde",noshade:"Ingen skygge",width:"Bredde"});
