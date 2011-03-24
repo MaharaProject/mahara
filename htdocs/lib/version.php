07f468cfa9a20e70c6824e63e6a793716b97d842
Stacey Walker <stacey@catalyst-eu.net>
2011-03-24 22:42:25 +0000

    Version bump for 1.3.4

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,8 +28,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2010083111;
-$config->release = '1.3.4testing';
+$config->version = 2010083112;
+$config->release = '1.3.4';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
