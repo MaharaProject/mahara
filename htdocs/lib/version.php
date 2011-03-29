56536122a7d8d2dbcedb93322342fc176250d2fc
Francois Marier <francois@catalyst.net.nz>
2011-03-29 14:21:59 +1300

    Version bump for 1.2.8
    Signed-off-by: Francois Marier <francois@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,8 +28,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2009111020;
-$config->release = '1.2.8testing';
+$config->version = 2009111021;
+$config->release = '1.2.8';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
