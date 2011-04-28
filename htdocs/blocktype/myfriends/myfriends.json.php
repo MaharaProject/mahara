a5fb894c11d29e8cd723509420d88b33cf783a75
Richard Mansfield <richard.mansfield@catalyst.net.nz>
2011-04-28 15:28:38 +1200

    Fixes for myfriends pagination script (bug #772140)
    Change-Id: I1681a260f5012597c86ff856ee9a0ef67dc5906e
    Signed-off-by: Richard Mansfield <richard.mansfield@catalyst.net.nz>

--- htdocs/blocktype/myfriends/myfriends.json.php
+++ htdocs/blocktype/myfriends/myfriends.json.php
@@ -32,13 +32,17 @@ require(dirname(dirname(dirname(__FILE__))) . '/init.php');
 safe_require('blocktype', 'myfriends');
 require_once('user.php');
 
-$userid = param_integer('user');
 $offset = param_integer('offset');
 $limit  = param_integer('limit', MAXFRIENDDISPLAY);
 $bi = new BlockInstance(param_integer('block'));
+if (!can_view_view($bi->get('view'))) {
+    json_reply(true, get_string('accessdenied', 'error'));
+}
+$userid = $bi->get_view()->get('owner');
 
 $friends = get_friends($userid, $limit, $offset);
 PluginBlocktypeMyfriends::build_myfriends_html($friends, $userid, $bi);
+unset($friends['data']);
 
 json_reply(false, array('data' => $friends));
 ?>
