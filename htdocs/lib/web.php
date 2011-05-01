acffc8a148a2385c784ce4b979feb779b3643ba8
Darryl Hamilton <darrylh@catalyst.net.nz>
2011-05-02 08:12:38 +1200

    Upgrade jQuery from 1.3.2 to 1.5.2
    Change-Id: Ie794ff5881fbc4e8701d41376835bd8413114fc0
    Signed-off-by: Darryl Hamilton <darrylh@catalyst.net.nz>

--- htdocs/lib/web.php
+++ htdocs/lib/web.php
@@ -207,7 +207,7 @@ EOF;
         }
         // Load jquery first, so that it doesn't break Mochikit
         if (($key = array_search('jquery', $check)) !== false) {
-            $jquery = (get_config('developermode') & DEVMODE_UNPACKEDJS) ? 'jquery-1.3.2.js' : 'jquery-1.3.2.min.js';
+            $jquery = (get_config('developermode') & DEVMODE_UNPACKEDJS) ? 'jquery-1.5.2.js' : 'jquery-1.5.2.min.js';
             array_unshift($javascript_array, $jsroot . 'jquery/' . $jquery);
             // Make jQuery accessible with $j (Mochikit has $)
             $headers[] = '<script type="text/javascript">$j=jQuery;</script>';
