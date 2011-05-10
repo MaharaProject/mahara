fecbde7351870d3a3a5e622078e7370a5c3ee6a2
Richard Mansfield <richard.mansfield@catalyst.net.nz>
2011-05-10 12:28:19 +1200

    Version bump for 1.3.6
    Signed-off-by: Richard Mansfield <richard.mansfield@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,8 +28,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2010083115;
-$config->release = '1.3.6testing';
+$config->version = 2010083116;
+$config->release = '1.3.6';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
