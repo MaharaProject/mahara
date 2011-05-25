ad0353a26dc09a254de1264a2d1fbfe26c64c001
Francois Marier <francois@catalyst.net.nz>
2011-05-25 16:08:07 +1200

    Bump the version number on master to 1.5dev
    Change-Id: If92eb06f7a2864de278f929b9301eba9023d864f
    Signed-off-by: Francois Marier <francois@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,8 +28,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2011052400;
-$config->release = '1.4.0alpha2dev';
+$config->version = 2011052500;
+$config->release = '1.5.0dev';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
