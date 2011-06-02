MaharaProject/mahara

commit c243ea697da914e89b39efa702291893fabb4a5b

Correction: Only show 'Edit group membership' link if relevant (Fixes #619115).
The 'Edit group membership' link should only be shown if there are invite
or controlled groups.  Originally, I misread the way this worked and
thought that the javascript was simply missing.  On a second look, I
realised that the links shouldn't be shown unless there are invite or
controlled groups.

Signed-off-by: Andrew Robert Nicols <andrew.nicols@luns.net.uk>

htdocs/theme/raw/templates/user/user.tpl
@@ -39,7 +39,7 @@
 			{/if}
 		</li>
       {/if}
-      {if $admingroups}
+      {if $admingroups->invite || $admingroups->controlled}
       <li><a href="" onclick="showGroupBox(event, {$user->id})">{str tag='editgroupmembership' section='group'}</a></li>
       {/if}
 	</ul>

htdocs/user/myfriends.php
@@ -106,7 +106,7 @@ if (!$data['count']) {
     }
 }
 
-$javascript = array('paginator', 'groupbox');
+$javascript = array('paginator');
 if ($admingroups->invite || $admingroups->controlled) {
     array_push($javascript, 'groupbox');
 }

commit bdf3fad47d3eec765defddbe0cdd74fea22fc579

Restore edit membership link on find friends page (bug #791688)
This bug was introduced by commit c243ea697d (see bug #619115).

Change-Id: Ia756644803ab00b7b0d1face21a7b84484c25ad4
Signed-off-by: Richard Mansfield <richard.mansfield@catalyst.net.nz>

htdocs/theme/raw/templates/user/user.tpl
@@ -39,7 +39,7 @@
 			{/if}
 		</li>
       {/if}
-      {if $admingroups->invite || $admingroups->controlled}
+      {if $admingroups}
       <li><a href="" onclick="showGroupBox(event, {$user->id})" class="btn-edit">{str tag='editgroupmembership' section='group'}</a></li>
       {/if}
 	</ul>

htdocs/user/myfriends.php
@@ -59,9 +59,7 @@ $invite = count_records_sql("SELECT COUNT(g.id)
         AND gm.role = 'admin'
         AND g.deleted = 0", array($USER->get('id')));
 
-$admingroups = new StdClass;
-$admingroups->controlled = $controlledgroups;
-$admingroups->invite = $invite;
+$admingroups = $controlledgroups || $invite;
 
 build_userlist_html($data, 'myfriends', $admingroups);
 
@@ -107,7 +105,7 @@ if (!$data['count']) {
 }
 
 $javascript = array('paginator');
-if ($admingroups->invite || $admingroups->controlled) {
+if ($admingroups) {
     array_push($javascript, 'groupbox');
 }
 $smarty = smarty($javascript, array(), array('applychanges' => 'mahara', 'nogroups' => 'group'), array('sideblocks' => array(friends_control_sideblock())));