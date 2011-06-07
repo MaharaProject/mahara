3f8f9db1f6d82dcda1bab49b70edf987bcbb158b
Francois Marier <francois@catalyst.net.nz>
2011-06-07 17:30:57 +1200

    Version bump for 1.4rc4dev
    Signed-off-by: Francois Marier <francois@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,8 +28,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2011060700;
-$config->release = '1.4rc3';
+$config->version = 2011060701;
+$config->release = '1.4rc4dev';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
