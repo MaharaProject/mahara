MaharaProject/mahara

commit 9ca6bb553673486287bba8000e18c7027b8ad13f

Reset the site language immediately after config is loaded (bug #787123)
Because the $lang variable is declared as static in current_language(), a
call to get_string in the sanity checks can set the language for the
entire request, even before the site language has been read in from the
config table.  This ensures the language is reset immediately after config
has been read.

Change-Id: Ic81dfaedd70de75d93a6d38dd89915eeabcccd6e
Signed-off-by: Richard Mansfield <richard.mansfield@catalyst.net.nz>

htdocs/lib/mahara.php
@@ -926,7 +926,13 @@ function print_object($mixed) {
 function current_language($reset=null) {
     global $USER, $CFG, $SESSION;
 
-    static $lang;
+    static $lang, $cfglang;
+
+    if (!isset($cfglang) && isset($CFG->lang) && !empty($CFG->lang) && is_null($reset)) {
+        // This is the first call to current_language after load_config, so ensure the
+        // language is reset.
+        $reset = $cfglang = $CFG->lang;
+    }
 
     if (!empty($reset)) {
         $lang = $reset;  // Set the language for this request

commit 966e804bb39e2151f00dbb4a7881994139fc7288

Only cache user preference in current_language function (bug #800512)
Logged-in user language preferences were broken in commit 9ca6bb5536.
This change removes caching on the session and config languages, which
are quick to retrieve anyway, but leaves it in place for the user
language preference and locale setting.

Change-Id: Ib601f2227409bbbc63d269fe079915cc0e902fc2
Signed-off-by: Richard Mansfield <richard.mansfield@catalyst.net.nz>

htdocs/auth/lib.php
@@ -484,7 +484,6 @@ function auth_setup () {
         if (defined('PUBLIC') && !isset($_GET['login'])) {
             if ($lang = param_alphanumext('lang', null)) {
                 $SESSION->set('lang', $lang);
-                current_language($lang);
             }
             return;
         }

htdocs/auth/user.php
@@ -1175,11 +1175,6 @@ class LiveUser extends User {
             $this->set_account_preference('lang', $sessionlang);
         }
 
-        // Set language for the current request
-        if (!empty($this->accountprefs['lang'])) {
-            current_language($this->accountprefs['lang']);
-        }
-
         $this->reset_institutions();
         $this->reset_grouproles();
         $this->load_views();

htdocs/lib/mahara.php
@@ -940,42 +940,29 @@ function print_object($mixed) {
  * language to use, either for a given user
  * or sitewide, or the default
  *
- * param @string $reset If passed, reset the current language to this
- * 
  * @return string
  */
-function current_language($reset=null) {
+function current_language() {
     global $USER, $CFG, $SESSION;
 
-    static $lang, $cfglang;
+    static $userlang, $lastlang;
 
-    if (!isset($cfglang) && isset($CFG->lang) && !empty($CFG->lang) && is_null($reset)) {
-        // This is the first call to current_language after load_config, so ensure the
-        // language is reset.
-        $reset = $cfglang = $CFG->lang;
-    }
+    $loggedin = $USER instanceof User && $USER->is_logged_in();
 
-    if (!empty($reset)) {
-        $lang = $reset;  // Set the language for this request
-    }
-
-    if (!empty($lang)) {
-        return $lang;
-    }
-
-    if ($USER instanceof User) {
+    if (!isset($userlang) && $loggedin) {
         $userlang = $USER->get_account_preference('lang');
         if ($userlang !== null && $userlang != 'default') {
-            if (language_installed($userlang)) {
-                $lang = $userlang;
-            }
-            else {
+            if (!language_installed($userlang)) {
                 $USER->set_account_preference('lang', 'default');
+                $userlang = 'default';
             }
         }
     }
 
-    if (empty($lang) && is_a($SESSION, 'Session')) {
+    if (!empty($userlang) && $userlang != 'default') {
+        $lang = $userlang;
+    }
+    else if (!$loggedin && is_a($SESSION, 'Session')) {
         $sesslang = $SESSION->get('lang');
         if (!empty($sesslang) && $sesslang != 'default') {
             $lang = $sesslang;
@@ -986,6 +973,10 @@ function current_language($reset=null) {
         $lang = !empty($CFG->lang) ? $CFG->lang : 'en.utf8';
     }
 
+    if ($lang == $lastlang) {
+        return $lang;
+    }
+
     // Set locale.  We are probably being called from get_string_location.
     // $lang had better be non-empty, or it will call us again.
     if ($args = split(',', get_string_location('locales', 'langconfig', array(), 'raw_langstring', $lang))) {
@@ -993,7 +984,7 @@ function current_language($reset=null) {
         call_user_func_array('setlocale', $args);
     }
 
-    return $lang;
+    return $lastlang = $lang;
 }
 
 /**