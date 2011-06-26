f1175eba7a2cf46afdd977827624887c8d2a4071
Hugh Davenport <hugh@catalyst.net.nz>
2011-06-27 11:43:05 +1200

    Fix bug with show tags for user tags (bug #802309)
    This fix lets both site admins and institutional admins
    view previous tags applied to all users that the current
    user has adminstration rights over.
    
    Change-Id: I6f54cf852cdb4e8829f6e19fb198445a04b4007a
    Signed-off-by: Hugh Davenport <hugh@catalyst.net.nz>

--- htdocs/json/taglist.php
+++ htdocs/json/taglist.php
@@ -30,12 +30,25 @@ define('JSON', 1);
 require(dirname(dirname(__FILE__)) . '/init.php');
 
 if ($USER->is_logged_in()) {
+    $usertags = "";
+    if ($USER->get('admin')) {
+        $usertags = "
+            UNION ALL
+            SELECT tag,COUNT(*) AS count FROM {usr_tag} t INNER JOIN {usr} u ON t.usr=u.id GROUP BY 1";
+    }
+    else if ($admininstitutions = $USER->get('admininstitutions')) {
+        $insql = "'" . join("','", $admininstitutions) . "'";
+        $usertags = "
+            UNION ALL
+            SELECT tag,COUNT(*) AS count FROM {usr_tag} t INNER JOIN {usr} u ON t.usr=u.id INNER JOIN {usr_institution} ui ON ui.usr=u.id WHERE ui.institution IN ($insql) GROUP BY 1";
+    }
     $result = get_records_sql_array("
         SELECT tag, SUM(count) AS count
         FROM (
             SELECT tag,COUNT(*) AS count FROM {artefact_tag} t INNER JOIN {artefact} a ON t.artefact=a.id WHERE a.owner=? GROUP BY 1
             UNION ALL
-            SELECT tag,COUNT(*) AS count FROM {view_tag} t INNER JOIN {view} v ON t.view=v.id WHERE v.owner=? GROUP BY 1
+            SELECT tag,COUNT(*) AS count FROM {view_tag} t INNER JOIN {view} v ON t.view=v.id WHERE v.owner=? GROUP BY 1" .
+            $usertags . "
         ) tags
         GROUP BY tag 
         ORDER BY LOWER(tag)
