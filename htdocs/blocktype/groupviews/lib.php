MaharaProject/mahara

commit 13485914ff6fa7a9334ee0656b9d50f6279a83e8

Fix display issue with groupviews blocktype
When not configured, it doesn't use the default values

Bug #787444

Change-Id: I0274986fc40d418ecf8d876bf811796e18b53e4c
Signed-off-by: Hugh Davenport <hugh@catalyst.net.nz>

htdocs/blocktype/groupviews/lib.php
@@ -56,6 +56,10 @@ class PluginBlocktypeGroupViews extends SystemBlocktype {
 
     public static function render_instance(BlockInstance $instance, $editing=false) {
         $configdata = $instance->get('configdata');
+        if (!isset($configdata['showgroupview'])) {
+            // If not set, use default
+            $configdata['showgroupviews'] = 1;
+        }
         $groupid = $instance->get_view()->get('group');
         if (!$groupid) {
             return '';

commit 1f5baed542edbeb7b7d87e6492b58aa6bf707a91

Fix typo in checking for default views
introduced in 13485914ff6fa7a9334ee0656b9d50f6279a83e8

Change-Id: I17e9880da3e5a426a651f0d4c237f7a16ea526ba
Signed-off-by: Hugh Davenport <hugh@catalyst.net.nz>

htdocs/blocktype/groupviews/lib.php
@@ -56,7 +56,7 @@ class PluginBlocktypeGroupViews extends SystemBlocktype {
 
     public static function render_instance(BlockInstance $instance, $editing=false) {
         $configdata = $instance->get('configdata');
-        if (!isset($configdata['showgroupview'])) {
+        if (!isset($configdata['showgroupviews'])) {
             // If not set, use default
             $configdata['showgroupviews'] = 1;
         }