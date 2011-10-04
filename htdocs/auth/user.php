MaharaProject/mahara

commit aba54873f4c872ac4f511f361896bdd1c6738e26

Allow users to see images that have been published inside forum posts they're allowed to see (bug #655631)
Change-Id: Ib93464176d0093a27d2aa63d9fb73bc9b91b9879
Signed-off-by: Richard Mansfield <richard.mansfield@catalyst.net.nz>
Reviewed-on: https://reviews.mahara.org/2
Reviewed-by: Ruslan Kabalin <ruslan.kabalin@luns.net.uk>
Reviewed-by: Francois Marier <francois@catalyst.net.nz>
Tested-by: Richard Mansfield <richardm@mahara.org>

htdocs/auth/user.php
@@ -861,6 +861,20 @@ class User {
         return false;
     }
 
+    public function can_publish_artefact($a) {
+        if (($this->get('id') and $this->get('id') == $a->get('owner'))
+            || ($a->get('institution') and $this->is_institutional_admin($a->get('institution')))) {
+            return true;
+        }
+        $group = $a->get('group');
+        if ($group) {
+            return count_records_sql("SELECT COUNT(*) FROM {artefact_access_role} ar
+                INNER JOIN {group_member} g ON ar.role = g.role
+                WHERE ar.artefact = ? AND g.member = ? AND ar.can_publish = 1 AND g.group = ?", array($a->get('id'), $this->get('id'), $group));
+        }
+        return false;
+    }
+
     public function can_edit_view($v) {
         $owner = $v->get('owner');
         if ($owner == $this->get('id')) {

htdocs/artefact/file/download.php
@@ -33,6 +33,7 @@ require_once('file.php');
 
 $fileid = param_integer('file');
 $viewid = param_integer('view', null);
+$postid = param_integer('post', null);
 $size   = get_imagesize_parameters();
 $forcedl = param_boolean('download');
 
@@ -96,7 +97,17 @@ else {
             // Alternatively, if you own the file or you are an admin, it should always work
 
             if (!$USER->can_view_artefact($file)) {
-                throw new AccessDeniedException(get_string('accessdenied', 'error'));
+
+                // Check for images sitting in visible forum posts
+                $visibleinpost = false;
+                if ($postid && $file instanceof ArtefactTypeImage) {
+                    safe_require('interaction', 'forum');
+                    $visibleinpost = PluginInteractionForum::can_see_attached_file($file, $postid);
+                }
+
+                if (!$visibleinpost) {
+                    throw new AccessDeniedException(get_string('accessdenied', 'error'));
+                }
             }
         }
     }

htdocs/interaction/forum/editpost.php
@@ -195,7 +195,7 @@ function editpost_submit(Pieform $form, $values) {
         'interaction_forum_post',
         array(
             'subject' => $values['subject'],
-            'body' => $values['body']
+            'body' => PluginInteractionForum::prepare_post_body($values['body'], $postid),
         ),
         array('id' => $postid)
     );
@@ -217,18 +217,22 @@ function editpost_submit(Pieform $form, $values) {
 function addpost_submit(Pieform $form, $values) {
     global $USER, $SESSION;
     $parentid = param_integer('parent');
-    $postid = insert_record(
-        'interaction_forum_post',
-        (object)array(
-            'topic' => $values['topic'],
-            'poster' => $USER->get('id'),
-            'parent' => $parentid,
-            'subject' => $values['subject'],
-            'body' => $values['body'],
-            'ctime' =>  db_format_timestamp(time())
-        ),
-        'id', true
+    $post = (object)array(
+        'topic'   => $values['topic'],
+        'poster'  => $USER->get('id'),
+        'parent'  => $parentid,
+        'subject' => $values['subject'],
+        'body'    => $values['body'],
+        'ctime'   =>  db_format_timestamp(time())
     );
+    $postid = insert_record('interaction_forum_post', $post, 'id', true);
+
+    // Rewrite the post id into links in the body
+    $newbody = PluginInteractionForum::prepare_post_body($post->body, $postid);
+    if (!empty($newbody) && $newbody != $post->body) {
+        set_field('interaction_forum_post', 'body', $newbody, 'id', $postid);
+    }
+
     $delay = get_config_plugin('interaction', 'forum', 'postdelay');
     if (!is_null($delay) && $delay == 0) {
         PluginInteractionForum::interaction_forum_new_post(array($postid));

htdocs/interaction/forum/edittopic.php
@@ -194,17 +194,21 @@ function addtopic_submit(Pieform $form, $values) {
             'closed' => isset($values['closed']) && $values['closed'] ? 1 : 0
         ), 'id', true
     );
-    $postid = insert_record(
-        'interaction_forum_post',
-        (object)array(
-            'topic' => $topicid,
-            'poster' => $USER->get('id'),
-            'subject' => $values['subject'],
-            'body' => $values['body'],
-            'ctime' =>  db_format_timestamp(time())
-        ),
-        'id', true
+    $post = (object)array(
+        'topic'   => $topicid,
+        'poster'  => $USER->get('id'),
+        'subject' => $values['subject'],
+        'body'    => $values['body'],
+        'ctime'   =>  db_format_timestamp(time())
     );
+    $postid = insert_record('interaction_forum_post', $post, 'id', true);
+
+    // Rewrite the post id into links in the body
+    $newbody = PluginInteractionForum::prepare_post_body($post->body, $postid);
+    if (!empty($newbody) && $newbody != $post->body) {
+        set_field('interaction_forum_post', 'body', $newbody, 'id', $postid);
+    }
+
     if (!record_exists('interaction_forum_subscription_forum', 'user', $USER->get('id'), 'forum', $forumid)) {
         insert_record('interaction_forum_subscription_topic', (object)array(
             'user'  => $USER->get('id'),
@@ -234,7 +238,7 @@ function edittopic_submit(Pieform $form, $values) {
             'interaction_forum_post',
             array(
                 'subject' => $values['subject'],
-                'body' => $values['body']
+                'body' => PluginInteractionForum::prepare_post_body($values['body'], $values['post']),
             ),
             array('id' => $values['post'])
         );

htdocs/interaction/forum/lib.php
@@ -620,6 +620,91 @@ EOF;
 
         return $result;
     }
+
+    // Rewrite download links in the post body to add a post id parameter.
+    // Used in download.php to determine permission to view the file.
+    static $replacement_postid;
+
+    public static function replace_download_link($matches) {
+        parse_str(html_entity_decode($matches[1]), $params);
+        if (empty($params['file'])) {
+            return $matches[0];
+        }
+        $url = get_config('wwwroot') . 'artefact/file/download.php?file=' . (int) $params['file'];
+        unset($params['post']);
+        unset($params['file']);
+        if (!empty($params)) {
+            $url .= '&' . http_build_query($params);
+        }
+        return $url . '&post=' . (int) self::$replacement_postid;
+    }
+
+    public static function prepare_post_body($body, $postid) {
+        self::$replacement_postid = $postid;
+        return preg_replace_callback(
+            '#(?<=[\'"])' . get_config('wwwroot') . 'artefact/file/download\.php\?(file=\d+(?:(?:&amp;|&)(?:[a-z]+=[x0-9]+)+)*)#',
+            array('self', 'replace_download_link'),
+            $body
+        );
+    }
+
+    /**
+     * Given a post id & the id of an image artefact, check that the logged-in user
+     * has permission to see the image in the context of the post.
+     */
+    public static function can_see_attached_file($file, $postid) {
+        global $USER;
+        require_once('group.php');
+
+        if (!$file instanceof ArtefactTypeImage) {
+            return false;
+        }
+
+        $post = get_record_sql('
+            SELECT
+                p.body, p.poster, g.id AS groupid, g.public
+            FROM {interaction_forum_post} p
+            INNER JOIN {interaction_forum_topic} t ON (t.id = p.topic AND t.deleted = 0)
+            INNER JOIN {interaction_forum_post} fp ON (fp.parent IS NULL AND fp.topic = t.id)
+            INNER JOIN {interaction_instance} f ON (t.forum = f.id AND f.deleted = 0)
+            INNER JOIN {group} g ON (f.group = g.id AND g.deleted = 0)
+            WHERE p.id = ? AND p.deleted = 0',
+            array($postid)
+        );
+
+        if (!$post) {
+            return false;
+        }
+
+        if (!$post->public && !group_user_access($post->groupid, $USER->get('id'))) {
+            return false;
+        }
+
+        // Check that the author of the post is allowed to publish the file
+        $poster = new User();
+        $poster->find_by_id($post->poster);
+        if (!$poster->can_publish_artefact($file)) {
+            return false;
+        }
+
+        // Load the post as an html fragment & make sure it has the image in it
+        $page = new DOMDocument();
+        libxml_use_internal_errors(true);
+        $success = $page->loadHTML($post->body);
+        libxml_use_internal_errors(false);
+        if (!$success) {
+            return false;
+        }
+        $xpath = new DOMXPath($page);
+        $srcstart = get_config('wwwroot') . 'artefact/file/download.php?file=' . $file->get('id') . '&';
+        $query = '//img[starts-with(@src,"' . $srcstart . '")]';
+        $elements = $xpath->query($query);
+        if ($elements->length < 1) {
+            return false;
+        }
+
+        return true;
+    }
 }
 
 class InteractionForumInstance extends InteractionInstance {

commit 4165e4ab545a96b63c099ea33e6098751fef8e4d

User can_publish_artefact method too restrictive (bug #865911)
The can_publish_artefact method on the User class, introduced in
commit aba54873f4 (see bug #655631) is out of line with other
permissions on the site when it comes to institution artefacts.

Any institution member can include institution files on one of their
own pages, but this method (currently used when putting images into
forum posts) only allows publishing by institutional admins.

This change adds publishing permission on institution artefacts for
all institution members.

Change-Id: I3f8a15de573de6f58497ae45839647b462fa5e89
Signed-off-by: Richard Mansfield <richard.mansfield@catalyst.net.nz>

htdocs/auth/user.php
@@ -935,9 +935,15 @@ class User {
     }
 
     public function can_publish_artefact($a) {
-        if (($this->get('id') and $this->get('id') == $a->get('owner'))
-            || ($a->get('institution') and $this->is_institutional_admin($a->get('institution')))) {
+        if (($this->get('id') and $this->get('id') == $a->get('owner'))) {
             return true;
+        }
+
+        if ($i = $a->get('institution')) {
+            if ($i == 'mahara') {
+                return $this->get('admin');
+            }
+            return $this->in_institution($i);
         }
 
         if (!$group = $a->get('group')) {