MaharaProject/mahara

commit 79810c3d321b012df0b86a63459cc5de1cfd6e6f

Fix whitespace issues with param_... functions
bug #838429

Basically param_... functions don't check for trailing whitespace
This patch fixes this by trimming the value after it retrieves it.

Also, the param_integer_list function says in the comment it allows
optional whitespace, but the regex expression used doesn't allow it.
This patch fixes that.

Also, the param_signed_integer function didn't check on the entire value
and you could have a value like "ab+10cd".
This patch alters the regex to capture the start and end of string ^ and $

Change-Id: Ic26cc257f0057b6b686fc8c2e3c789610e055507
Signed-off-by: Hugh Davenport <hugh@catalyst.net.nz>

htdocs/lib/web.php
@@ -1056,6 +1056,8 @@ function param_integer($name) {
 
     list ($value, $defaultused) = call_user_func_array('_param_retrieve', $args);
 
+    $value = trim($value);
+
     if ($defaultused) {
         return $value;
     }
@@ -1087,11 +1089,13 @@ function param_signed_integer($name) {
 
     list ($value, $defaultused) = call_user_func_array('_param_retrieve', $args);
 
+    $value = trim($value);
+
     if ($defaultused) {
         return $value;
     }
 
-    if (preg_match('#[+-]?[0-9]+#', $value)) {
+    if (preg_match('/^[+-]?[0-9]+$/', $value)) {
         return (int)$value;
     }
     else if ($value == '' && isset($args[1])) {
@@ -1119,6 +1123,8 @@ function param_alpha($name) {
     $args = func_get_args();
 
     list ($value, $defaultused) = call_user_func_array('_param_retrieve', $args);
+
+    $value = trim($value);
 
     if ($defaultused) {
         return $value;
@@ -1150,6 +1156,8 @@ function param_alphanum($name) {
 
     list ($value, $defaultused) = call_user_func_array('_param_retrieve', $args);
 
+    $value = trim($value);
+
     if ($defaultused) {
         return $value;
     }
@@ -1179,6 +1187,8 @@ function param_alphanumext($name) {
     $args = func_get_args();
 
     list ($value, $defaultused) = call_user_func_array('_param_retrieve', $args);
+
+    $value = trim($value);
 
     if ($defaultused) {
         return $value;
@@ -1211,6 +1221,8 @@ function param_integer_list($name) {
 
     list ($value, $defaultused) = call_user_func_array('_param_retrieve', $args);
 
+    $value = trim($value);
+
     if ($defaultused) {
         return $value;
     }
@@ -1219,7 +1231,7 @@ function param_integer_list($name) {
         return array();
     }
 
-    if (preg_match('/^(\d+(,\d+)*)$/',$value)) {
+    if (preg_match('/^(\d+(\s*,\s*\d+)*)$/',$value)) {
         return array_map('intval', explode(',', $value));
     }
 
@@ -1237,6 +1249,8 @@ function param_integer_list($name) {
 function param_boolean($name) {
     
     list ($value) = _param_retrieve($name, false);
+
+    $value = trim($value);
 
     if (empty($value) || $value == 'off' || $value == 'no' || $value == 'false') {
         return false;
@@ -1260,6 +1274,8 @@ function param_imagesize($name) {
     $args = func_get_args();
 
     list ($value, $defaultused) = call_user_func_array('_param_retrieve', $args);
+
+    $value = trim($value);
 
     if ($defaultused) {
         return $value;

commit 0043cc8322c59ebd37bf12e0b07372bcdc755f21

Fix issue when param_... functions have default of null
Bug #872619

Regression from 79810c3d321b012df0b86a63459cc5de1cfd6e6f

Basically, a default parameter of null was given, then trim
was called, which made it into the string '' which then failed
the test is_null in the files it was used.

This reorders the functions so that the trim is called after
the test to see whether the default should be used. This means
that the default value is never trimmed, and can be null.

One exception to this, is the param_boolean which doesn't test
to see whether it is the default that is returned, so a check
is made first to see whether it is null.

Change-Id: I66d7253a8414dec7eb3ea67ebd363ea068a32012
Signed-off-by: Hugh Davenport <hugh@catalyst.net.nz>

htdocs/lib/web.php
@@ -1017,11 +1017,11 @@ function param_integer($name) {
 
     list ($value, $defaultused) = call_user_func_array('_param_retrieve', $args);
 
-    $value = trim($value);
-
     if ($defaultused) {
         return $value;
     }
+
+    $value = trim($value);
 
     if (preg_match('/^\d+$/',$value)) {
         return (int)$value;
@@ -1050,11 +1050,11 @@ function param_signed_integer($name) {
 
     list ($value, $defaultused) = call_user_func_array('_param_retrieve', $args);
 
-    $value = trim($value);
-
     if ($defaultused) {
         return $value;
     }
+
+    $value = trim($value);
 
     if (preg_match('/^[+-]?[0-9]+$/', $value)) {
         return (int)$value;
@@ -1085,11 +1085,12 @@ function param_alpha($name) {
 
     list ($value, $defaultused) = call_user_func_array('_param_retrieve', $args);
 
-    $value = trim($value);
 
     if ($defaultused) {
         return $value;
     }
+
+    $value = trim($value);
 
     if (preg_match('/^[a-zA-Z]+$/',$value)) {
         return $value;
@@ -1117,11 +1118,11 @@ function param_alphanum($name) {
 
     list ($value, $defaultused) = call_user_func_array('_param_retrieve', $args);
 
-    $value = trim($value);
-
     if ($defaultused) {
         return $value;
     }
+
+    $value = trim($value);
 
     if (preg_match('/^[a-zA-Z0-9]+$/',$value)) {
         return $value;
@@ -1149,11 +1150,11 @@ function param_alphanumext($name) {
 
     list ($value, $defaultused) = call_user_func_array('_param_retrieve', $args);
 
-    $value = trim($value);
-
     if ($defaultused) {
         return $value;
     }
+
+    $value = trim($value);
 
     if (preg_match('/^[a-zA-Z0-9_.-]+$/',$value)) {
         return $value;
@@ -1182,11 +1183,11 @@ function param_integer_list($name) {
 
     list ($value, $defaultused) = call_user_func_array('_param_retrieve', $args);
 
-    $value = trim($value);
-
     if ($defaultused) {
         return $value;
     }
+
+    $value = trim($value);
 
     if ($value == '') {
         return array();
@@ -1211,7 +1212,9 @@ function param_boolean($name) {
     
     list ($value) = _param_retrieve($name, false);
 
-    $value = trim($value);
+    if (!is_null($value)) {
+        $value = trim($value);
+    }
 
     if (empty($value) || $value == 'off' || $value == 'no' || $value == 'false') {
         return false;
@@ -1236,11 +1239,11 @@ function param_imagesize($name) {
 
     list ($value, $defaultused) = call_user_func_array('_param_retrieve', $args);
 
-    $value = trim($value);
-
     if ($defaultused) {
         return $value;
     }
+
+    $value = trim($value);
 
     if (!preg_match('/\d+x\d+/', $value)) {
         throw new ParameterException('Invalid size for image specified');