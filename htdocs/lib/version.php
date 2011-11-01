bed8c35c5b8b3044746a7133cdd2a69baa3163a7
Francois Marier <francois@catalyst.net.nz>
2011-11-01 19:58:04 +1300

    Version bump for 1.3.7
    Signed-off-by: Francois Marier <francois@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,8 +28,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2010083117;
-$config->release = '1.3.7testing';
+$config->version = 2010083118;
+$config->release = '1.3.7';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
