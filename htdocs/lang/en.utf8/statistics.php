1c9453319b2d19ee0cce4606a6bc6b3e35fdcbbb
Kristina Hoeppner <kristina@catalyst.net.nz>
2012-06-29 11:14:04 +1200

    Added statistics language strings
    Change-Id: I0202fed482ba192862aa29a8bc497175ee81e163
    Signed-off-by: Hugh Davenport <hugh@catalyst.net.nz>

---
+++ htdocs/lang/en.utf8/statistics.php
@@ -0,0 +1,205 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2012 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage lang
+ * @author     Hugh Davenport <hugh@catalyst.net.nz>, Catalyst IT Ltd
+ *
+ */
+
+defined('INTERNAL') || die();
+
+// Strings for statistics
+$string['fieldname'] = 'translation goes here';
+$string['allowpublicprofiles'] = 'Allow public profiles';
+$string['allowpublicviews'] = 'Allow public pages';
+$string['artefact_type_academicgoal'] = 'Artefact: Academic goals';
+$string['artefact_type_academicskill'] = 'Artefact: Academic skills';
+$string['artefact_type_activity'] = 'Artefact: PLD activity';
+$string['artefact_type_address'] = 'Artefact: Postal address';
+$string['artefact_type_aimscreenname'] = 'Artefact: AIM screen name';
+$string['artefact_type_archive'] = 'Artefact: Compressed file';
+$string['artefact_type_audio'] = 'Artefact: Audio file';
+$string['artefact_type_blog'] = 'Artefact: Journal';
+$string['artefact_type_blogaddress'] = 'Artefact: Blog address';
+$string['artefact_type_blogpost'] = 'Artefact: Journal entry';
+$string['artefact_type_book'] = 'Artefact: Books and publications';
+$string['artefact_type_businessnumber'] = 'Artefact: Business phone';
+$string['artefact_type_careergoal'] = 'Artefact: Career goals';
+$string['artefact_type_certification'] = 'Artefact: Certifications, accreditations and awards';
+$string['artefact_type_city'] = 'Artefact: City/region';
+$string['artefact_type_comment'] = 'Artefact: Comment';
+$string['artefact_type_contactinformation'] = 'Artefact: Contact information';
+$string['artefact_type_country'] = 'Artefact: Country';
+$string['artefact_type_coverletter'] = 'Artefact: Cover letter';
+$string['artefact_type_cpd'] = 'Artefact: PLD';
+$string['artefact_type_educationhistory'] = 'Artefact: Education history';
+$string['artefact_type_email'] = 'Artefact: Email';
+$string['artefact_type_employmenthistory'] = 'Artefact: Employment history';
+$string['artefact_type_faxnumber'] = 'Artefact: Fax number';
+$string['artefact_type_file'] = 'Artefact: File';
+$string['artefact_type_firstname'] = 'Artefact: First name';
+$string['artefact_type_folder'] = 'Artefact: Folder';
+$string['artefact_type_homenumber'] = 'Artefact: Home phone';
+$string['artefact_type_html'] = 'Artefact: HTML file';
+$string['artefact_type_icqnumber'] = 'Artefact: ICQ number';
+$string['artefact_type_image'] = 'Artefact: Image file';
+$string['artefact_type_industry'] = 'Artefact: Industry';
+$string['artefact_type_interest'] = 'Artefact: Interests';
+$string['artefact_type_introduction'] = 'Artefact: Introduction';
+$string['artefact_type_jabberusername'] = 'Artefact: Jabber username';
+$string['artefact_type_lastname'] = 'Artefact: Last name';
+$string['artefact_type_membership'] = 'Artefact: Professional memberships';
+$string['artefact_type_mobilenumber'] = 'Artefact: Mobile phone';
+$string['artefact_type_msnnumber'] = 'Artefact: MSN chat';
+$string['artefact_type_occupation'] = 'Artefact: Occupation';
+$string['artefact_type_officialwebsite'] = 'Artefact: Official web site address';
+$string['artefact_type_personalgoal'] = 'Artefact: Personal goals';
+$string['artefact_type_personalinformation'] = 'Artefact: Personal information';
+$string['artefact_type_personalskill'] = 'Artefact: Personal skills';
+$string['artefact_type_personalwebsite'] = 'Artefact: Personal web site address';
+$string['artefact_type_plan'] = 'Artefact: Plans';
+$string['artefact_type_preferredname'] = 'Artefact: Display name';
+$string['artefact_type_profileicon'] = 'Artefact: Profile pictures';
+$string['artefact_type_skypeusername'] = 'Artefact: Skype username';
+$string['artefact_type_studentid'] = 'Artefact: Student ID';
+$string['artefact_type_task'] = 'Artefact: Task';
+$string['artefact_type_town'] = 'Artefact: Town';
+$string['artefact_type_video'] = 'Artefact: Video file';
+$string['artefact_type_workskill'] = 'Artefact: Work skills';
+$string['artefact_type_yahoochat'] = 'Artefact: Yahoo chat';
+$string['blocktype_blog'] = 'Block: Journal';
+$string['blocktype_blogpost'] = 'Block: Journal entry';
+$string['blocktype_contactinfo'] = 'Block: Contact information';
+$string['blocktype_cpds'] = 'Block: PLD';
+$string['blocktype_creativecommons'] = 'Block: Creative Commons';
+$string['blocktype_entireresume'] = 'Block: Entire résumé';
+$string['blocktype_externalfeed'] = 'Block: External feed';
+$string['blocktype_externalvideo'] = 'Block: External media';
+$string['blocktype_filedownload'] = 'Block: File(s) to download';
+$string['blocktype_folder'] = 'Block: Folder';
+$string['blocktype_gallery'] = 'Block: Image gallery';
+$string['blocktype_googleapps'] = 'Block: Google Apps';
+$string['blocktype_groupinfo'] = 'Block: Group info';
+$string['blocktype_groupmembers'] = 'Block: Group members';
+$string['blocktype_groupviews'] = 'Block: Group pages';
+$string['blocktype_html'] = 'Block: Some HTML';
+$string['blocktype_image'] = 'Block: Image';
+$string['blocktype_inbox'] = 'Block: My inbox';
+$string['blocktype_internalmedia'] = 'Block: Embedded media';
+$string['blocktype_myfriends'] = 'Block: My friends';
+$string['blocktype_mygroups'] = 'Block: My groups';
+$string['blocktype_myviews'] = 'Block: My pages';
+$string['blocktype_navigation'] = 'Block: Navigation';
+$string['blocktype_newviews'] = 'Block: Latest pages';
+$string['blocktype_plans'] = 'Block: Plans';
+$string['blocktype_profileinfo'] = 'Block: Profile information';
+$string['blocktype_recentforumposts'] = 'Block: Recent forum posts';
+$string['blocktype_recentposts'] = 'Block: Recent journal entries';
+$string['blocktype_resumefield'] = 'Block: Résumé field';
+$string['blocktype_taggedposts'] = 'Block: Tagged journal entries';
+$string['blocktype_textbox'] = 'Block: Text box';
+$string['blocktype_wall'] = 'Block: Wall';
+$string['blocktype_watchlist'] = 'Block: My watched pages';
+$string['count_block_instance'] = 'Number of blocks';
+$string['count_blocktype_wall_post'] = 'Number of wall posts';
+$string['count_group'] = 'Number of groups';
+$string['count_group_member'] = 'Number of group members';
+$string['count_host'] = 'Number of Moodle hosts';
+$string['count_institution'] = 'Number of institutions';
+$string['count_interaction_forum_post'] = 'Number of forum posts';
+$string['count_usr'] = 'Number of users';
+$string['count_usr_friend'] = 'Number of friend relationships';
+$string['count_usr_institution'] = 'Number of users in institutions';
+$string['count_members'] = 'Number of members of institution';
+$string['count_artefacts'] = 'Number of artefacts';
+$string['count_blocks'] = 'Number of blocks';
+$string['count_views'] = 'Number of pages';
+$string['dbtype'] = 'Database type';
+$string['enablenetworking'] = 'Enabled networking';
+$string['installation_key'] = 'Installation key';
+$string['lang'] = 'Language';
+$string['newstats'] = 'New statistics';
+$string['plugin_artefact_blog_version'] = 'Artefact plugin Journal version';
+$string['plugin_artefact_comment_version'] = 'Artefact plugin Comment version';
+$string['plugin_artefact_file_version'] = 'Artefact plugin File version';
+$string['plugin_artefact_internal_version'] = 'Artefact plugin XXXXXX version';
+$string['plugin_artefact_plans_version'] = 'Artefact plugin Plans version';
+$string['plugin_artefact_resume_version'] = 'Artefact plugin Résumé version';
+$string['plugin_auth_browserid_version'] = 'Authentication plugin BrowserID version';
+$string['plugin_auth_imap_version'] = 'Authentication pluginn IMAP version';
+$string['plugin_auth_internal_version'] = 'Authentication plugin Internal version';
+$string['plugin_auth_ldap_version'] = 'Authentication plugin LDAP version';
+$string['plugin_auth_none_version'] = 'Authentication plugin None version';
+$string['plugin_auth_saml_version'] = 'Authentication plugin SAML version';
+$string['plugin_auth_xmlrpc_version'] = 'Authentication plugin XML-RPC version';
+$string['plugin_blocktype_blogpost_version'] = 'Block type plugin Journal entry version';
+$string['plugin_blocktype_blog_version'] = 'Block type plugin Journal version';
+$string['plugin_blocktype_contactinfo_version'] = 'Block type plugin Contact information version';
+$string['plugin_blocktype_cpds_version'] = 'Block type plugin PLD version';
+$string['plugin_blocktype_creativecommons_version'] = 'Block type plugin Creative Commons version';
+$string['plugin_blocktype_entireresume_version'] = 'Block type plugin Entire résumé version';
+$string['plugin_blocktype_externalfeed_version'] = 'Block type plugin External feed version';
+$string['plugin_blocktype_externalvideo_version'] = 'Block type plugin External media version';
+$string['plugin_blocktype_filedownload_version'] = 'Block type plugin File(s) to download version';
+$string['plugin_blocktype_folder_version'] = 'Block type plugin Folder version';
+$string['plugin_blocktype_gallery_version'] = 'Block type plugin Image gallery version';
+$string['plugin_blocktype_googleapps_version'] = 'Block type plugin Google Apps version';
+$string['plugin_blocktype_groupinfo_version'] = 'Block type plugin Group info version';
+$string['plugin_blocktype_groupmembers_version'] = 'Block type plugin Group members version';
+$string['plugin_blocktype_groupviews_version'] = 'Block type plugin Group pages version';
+$string['plugin_blocktype_html_version'] = 'Block type plugin Some HTML version';
+$string['plugin_blocktype_image_version'] = 'Block type plugin Image version';
+$string['plugin_blocktype_inbox_version'] = 'Block type plugin My inbox version';
+$string['plugin_blocktype_internalmedia_version'] = 'Block type plugin Embedded media version';
+$string['plugin_blocktype_myfriends_version'] = 'Block type plugin My friends version';
+$string['plugin_blocktype_mygroups_version'] = 'Block type plugin My groups version';
+$string['plugin_blocktype_myviews_version'] = 'Block type plugin My pages version';
+$string['plugin_blocktype_navigation_version'] = 'Block type plugin Navigation version';
+$string['plugin_blocktype_newviews_version'] = 'Block type plugin Latest pages version';
+$string['plugin_blocktype_plans_version'] = 'Block type plugin Plans version';
+$string['plugin_blocktype_profileinfo_version'] = 'Block type plugin Profile information version';
+$string['plugin_blocktype_recentforumposts_version'] = 'Block type plugin Recent forum posts version';
+$string['plugin_blocktype_recentposts_version'] = 'Block type plugin Recent journal posts version';
+$string['plugin_blocktype_resumefield_version'] = 'Block type plugin Single résumé field version';
+$string['plugin_blocktype_taggedposts_version'] = 'Block type plugin Tagged journal entries version';
+$string['plugin_blocktype_textbox_version'] = 'Block type plugin Text box version';
+$string['plugin_blocktype_wall_version'] = 'Block type plugin Wall version';
+$string['plugin_blocktype_watchlist_version'] = 'Block type plugin My watched pages version';
+$string['plugin_export_html_version'] = 'Export plugin HTML version';
+$string['plugin_export_leap_version'] = 'Export plugin Leap2A version';
+$string['plugin_grouptype_course_version'] = 'Group type plugin Course version';
+$string['plugin_grouptype_standard_version'] = 'Group type plugin Standard version';
+$string['plugin_import_file_version'] = 'Import plugin File version';
+$string['plugin_import_leap_version'] = 'Import plugin Leap2A version';
+$string['plugin_interaction_forum_version'] = 'Plugin Forum version';
+$string['plugin_notification_emaildigest_version'] = 'Notification plugin Email digest verstion';
+$string['plugin_notification_email_version'] = 'Notification plugin Email version';
+$string['plugin_notification_internal_version'] = 'Notification plugin Internal version';
+$string['plugin_search_internal_version'] = 'Search plugin Internal version';
+$string['release'] = 'Release';
+$string['sitename'] = 'Site name';
+$string['theme'] = 'Theme';
+$string['usersloggedin'] = 'Number of logged-in users in the last week';
+$string['version'] = 'Version';
+$string['view_type_dashboard'] = 'Number of dashboards';
+$string['view_type_grouphomepage'] = 'Number of group homepages';
+$string['view_type_portfolio'] = 'Number of portfolio pages';
+$string['view_type_profile'] = 'Number of profile pages';
+$string['wwwroot'] = 'WWW root';
