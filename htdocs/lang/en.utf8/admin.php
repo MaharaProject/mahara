3dc2558692141dbdcea0915552a1b83fd024faec
Hugh Davenport <hugh@catalyst.net.nz>
2012-07-24 15:59:02 +1200

    Reword institution statistics page when no institutions
    Change-Id: Ib089413f4d35a672e3526865e459ed8ab407528f
    Signed-off-by: Hugh Davenport <hugh@catalyst.net.nz>

--- htdocs/lang/en.utf8/admin.php
+++ htdocs/lang/en.utf8/admin.php
@@ -864,6 +864,7 @@ $string['Admins'] = 'Admins';
 
 $string['noinstitutions'] = 'No institutions';
 $string['noinstitutionsdescription'] = 'If you would like to associate users with an institution, you should create the institution first.';
+$string['noinstitutionsstatsdescription'] = 'Institution statistics are not available because there are no institutions available to your user.';
 
 $string['Lockedfields'] = 'Locked fields';
 $string['disabledlockedfieldhelp'] = 'Note: Disabled check boxes are for profile fields which are locked in the institution settings for "%s". These profile fields are locked at the site level and cannot be unlocked here.';
