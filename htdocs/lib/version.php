6faed1f6a6ebaa92c608f3a67bbfd7561323ae99
Melissa Draper <melissa@catalyst.net.nz>
2012-07-31 15:24:58 +1200

    Version bump for 1.4.3
    Signed-off-by: Melissa Draper <melissa@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -28,8 +28,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2011061007;
-$config->release = '1.4.3testing';
+$config->version = 2011061008;
+$config->release = '1.4.3';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
