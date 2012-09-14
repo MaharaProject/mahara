66415f2fae52ae8ed5f9b740e841375c8ab804d7
Melissa Draper <melissa@catalyst.net.nz>
2012-09-14 13:11:35 +1200

    Version bump for 1.5.4testing
    Signed-off-by: Melissa Draper <melissa@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -30,8 +30,8 @@ defined('INTERNAL') || die();
 $config = new StdClass;
 // See https://wiki.mahara.org/index.php/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.
-$config->version = 2012041008;
-$config->release = '1.5.3';
+$config->version = 2012041009;
+$config->release = '1.5.4testing';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
