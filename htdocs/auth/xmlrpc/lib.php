MaharaProject/mahara

commit 544c62b7964d384713e565b74ca24bd310b126fc

Use institution display name  (Bug #941551)
Replace institution name with institution display name.

Change-Id: Id1c358802b22665b300601062056c72f39a71723
Signed-off-by: Christopher Tombleson <christopher@catalyst.net.nz>

htdocs/auth/xmlrpc/lib.php
@@ -30,6 +30,7 @@ require_once(get_config('docroot') . 'auth/lib.php');
 require_once(get_config('libroot') . 'peer.php');
 require_once(get_config('libroot') . 'applicationset.php');
 require_once(get_config('docroot') . 'api/xmlrpc/lib.php');
+require_once(get_config('libroot') . 'institution.php');
 
 /**
  * The XMLRPC authentication method, which authenticates users against the
@@ -108,7 +109,7 @@ class AuthXmlrpc extends Auth {
         $peer = get_peer($remotewwwroot);
 
         if ($peer->deleted != 0 || $this->config['theyssoin'] != 1) {
-            throw new XmlrpcClientException('We don\'t accept SSO connections from ' . $peer->name);
+            throw new XmlrpcClientException('We don\'t accept SSO connections from ' . institution_display_name($peer->name));
         }
 
         $client = new Client();

htdocs/lib/mahara.php
@@ -2431,6 +2431,7 @@ function profile_sideblock() {
     global $USER, $SESSION;
     safe_require('notification', 'internal');
     require_once('group.php');
+    require_once('institution.php');
     $data = array(
         'id'          => $USER->get('id'),
         'myname'      => display_name($USER, null, true),
@@ -2444,7 +2445,7 @@ function profile_sideblock() {
         if ($authobj->authname == 'xmlrpc') {
             $peer = get_peer($authobj->wwwroot);
             if ($SESSION->get('mnetuser')) {
-                $data['mnetloggedinfrom'] = get_string('youhaveloggedinfrom', 'auth.xmlrpc', $authobj->wwwroot, $peer->name);
+                $data['mnetloggedinfrom'] = get_string('youhaveloggedinfrom', 'auth.xmlrpc', $authobj->wwwroot, institution_display_name($peer->name));
             }
             else {
                 $data['peer'] = array('name' => $peer->name, 'wwwroot' => $peer->wwwroot);

htdocs/view/view.php
@@ -35,6 +35,7 @@ require(dirname(dirname(__FILE__)) . '/init.php');
 
 require_once(get_config('libroot') . 'view.php');
 require_once(get_config('libroot') . 'collection.php');
+require_once('institution.php');
 require_once('group.php');
 safe_require('artefact', 'comment');
 
@@ -310,7 +311,7 @@ if ($mnetviewlist = $SESSION->get('mnetviewaccess')) {
         require_once(get_config('docroot') . 'api/xmlrpc/lib.php');
         if ($peer = get_peer_from_instanceid($SESSION->get('authinstance'))) {
             $smarty->assign('mnethost', array(
-                'name'      => $peer->name,
+                'name'      => institution_display_name($peer->name),
                 'url'       => $returnurl ? $returnurl : $peer->wwwroot,
             ));
         }

commit f84b948ac934caec8f5afed0ab52b2d1f6003c14

Fix mnet jump-back link regression (Bug #1079260)
Was broken by commit 544c62b7964d384713e565b74ca24bd310b126fc for
(Bug #941551)

Change-Id: I5ef546564677927e96541791ca3bbee1869e544b

htdocs/auth/xmlrpc/lib.php
@@ -109,7 +109,7 @@ class AuthXmlrpc extends Auth {
         $peer = get_peer($remotewwwroot);
 
         if ($peer->deleted != 0 || $this->config['theyssoin'] != 1) {
-            throw new XmlrpcClientException('We don\'t accept SSO connections from ' . institution_display_name($peer->name));
+            throw new XmlrpcClientException('We don\'t accept SSO connections from ' . institution_display_name($peer->institution));
         }
 
         $client = new Client();

htdocs/lib/mahara.php
@@ -2453,7 +2453,7 @@ function profile_sideblock() {
         if ($authobj->authname == 'xmlrpc') {
             $peer = get_peer($authobj->wwwroot);
             if ($SESSION->get('mnetuser')) {
-                $data['mnetloggedinfrom'] = get_string('youhaveloggedinfrom', 'auth.xmlrpc', $authobj->wwwroot, institution_display_name($peer->name));
+                $data['mnetloggedinfrom'] = get_string('youhaveloggedinfrom', 'auth.xmlrpc', $authobj->wwwroot, institution_display_name($peer->institution));
             }
             else {
                 $data['peer'] = array('name' => $peer->name, 'wwwroot' => $peer->wwwroot);

htdocs/view/view.php
@@ -311,7 +311,7 @@ if ($mnetviewlist = $SESSION->get('mnetviewaccess')) {
         require_once(get_config('docroot') . 'api/xmlrpc/lib.php');
         if ($peer = get_peer_from_instanceid($SESSION->get('authinstance'))) {
             $smarty->assign('mnethost', array(
-                'name'      => institution_display_name($peer->name),
+                'name'      => $peer->name,
                 'url'       => $returnurl ? $returnurl : $peer->wwwroot,
             ));
         }