6b9f4e5eff69c8789bc75210e20342b0a1154408
Son Nguyen <son.nguyen@catalyst.net.nz>
2013-01-16 11:24:44 +1300

    Upgrade TinyMCE to 3.5.8 (Bug #1040337)
    and
     - config spellchecker
     - rename the conflict error() to mce_error()
    
    Change-Id: Ia763a6e7bb0ba82f5eca95b8650d73c5782eb118
    Signed-off-by: Son Nguyen <son.nguyen@catalyst.net.nz>

--- htdocs/js/tinymce/plugins/spellchecker/config.php
+++ htdocs/js/tinymce/plugins/spellchecker/config.php
@@ -6,26 +6,25 @@
  */
 	// General settings
 	$config['general.engine'] = 'GoogleSpell';
-
+	//$config['general.engine'] = 'PSpell';
+	//$config['general.engine'] = 'PSpellShell';
+	//$config['general.remote_rpc_url'] = 'http://some.other.site/some/url/rpc.php';
+	
 	define('INTERNAL', 1);
 	require_once(dirname(__FILE__).'/../../../../init.php');
 	$aspellpath = get_config('pathtoaspell');
 	if (file_exists($aspellpath) && is_executable($aspellpath)) {
 		$config['general.engine'] = 'PSpellShell';
 	}
-
-	//$config['general.engine'] = 'PSpell';
-	//$config['general.engine'] = 'PSpellShell';
-	//$config['general.remote_rpc_url'] = 'http://some.other.site/some/url/rpc.php';
-
-	// PSpell settings
-	$config['PSpell.mode'] = PSPELL_FAST;
+	
+	    // PSpell settings
+	$config['PSpell.mode'] = 'PSPELL_FAST';
 	$config['PSpell.spelling'] = "";
 	$config['PSpell.jargon'] = "";
 	$config['PSpell.encoding'] = "";
 
 	// PSpellShell settings
-	$config['PSpellShell.mode'] = PSPELL_FAST;
+	$config['PSpellShell.mode'] = 'PSPELL_FAST';
 	$config['PSpellShell.aspell'] = $aspellpath;
 	$config['PSpellShell.tmp'] = '/tmp';
 
