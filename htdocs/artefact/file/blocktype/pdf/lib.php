6c4f26df7a6cce6a755fcecb80a604e2b7eb0949
Son Nguyen <sonn@catalyst.net.nz>
2013-05-02 08:47:47 +1200

    Add PDF viewer blocktype (Bug #1036556)
    added in new pdf icon
    
    Change-Id: Ib351064e1bd7adbcb3ec0243fb5d1436c520a740
    Signed-off-by: Son Nguyen <sonn@catalyst.net.nz>

---
+++ htdocs/artefact/file/blocktype/pdf/lib.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2013 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage blocktype-pdf
+ * @author     Catalyst IT Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006-2009 Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+defined('INTERNAL') || die();
+
+class PluginBlocktypePdf extends PluginBlocktype {
+
+    public static function single_only() {
+        return false;
+    }
+
+    public static function get_title() {
+        return get_string('title', 'blocktype.file/pdf');
+    }
+
+    public static function get_description() {
+        return get_string('description', 'blocktype.file/pdf');
+    }
+
+    public static function get_categories() {
+        return array('fileimagevideo');
+    }
+
+    public static function render_instance(BlockInstance $instance, $editing=false) {
+        $configdata = $instance->get('configdata'); // this will make sure to unserialize it for us
+        $configdata['viewid'] = $instance->get('view');
+
+        if (isset($configdata['artefactid'])) {
+            $pdf = $instance->get_artefact_instance($configdata['artefactid']);
+
+            if (!file_exists($pdf->get_path())) {
+                return '';
+            }
+
+            return '<iframe src="' . get_config('wwwroot') . 'artefact/file/blocktype/pdf/viewer.php?file=' . $configdata['artefactid'] . '&view=' . $instance->get('view')
+                 . '" width="100%" height="500" frameborder="0"></iframe>';
+        }
+
+        return '';
+    }
+
+    public static function has_instance_config() {
+        return true;
+    }
+
+    public static function instance_config_form($instance) {
+        $configdata = $instance->get('configdata');
+        safe_require('artefact', 'file');
+        $instance->set('artefactplugin', 'file');
+        return array(
+            'artefactid' => self::filebrowser_element($instance, (isset($configdata['artefactid'])) ? array($configdata['artefactid']) : null),
+        );
+    }
+
+    private static function get_allowed_mimetypes() {
+        static $mimetypes = array();
+        if (!$mimetypes) {
+            $mimetypes = get_column('artefact_file_mime_types', 'mimetype', 'description', 'pdf');
+        }
+        return $mimetypes;
+    }
+
+    public static function filebrowser_element(&$instance, $default=array()) {
+        $element = ArtefactTypeFileBase::blockconfig_filebrowser_element($instance, $default);
+        $element['title'] = get_string('file', 'artefact.file');
+        $element['name'] = 'artefactid';
+        $element['config']['selectone'] = true;
+        $element['filters'] = array(
+            'artefacttype'    => array('file'),
+            'filetype'        => self::get_allowed_mimetypes(),
+        );
+        return $element;
+    }
+
+    public static function artefactchooser_element($default=null) {
+        return array(
+            'name'  => 'artefactid',
+            'type'  => 'artefactchooser',
+            'title' => get_string('file', 'artefact.file'),
+            'defaultvalue' => $default,
+            'blocktype' => 'html',
+            'limit' => 10,
+            'artefacttypes' => array('file'),
+            'template' => 'artefact:file:artefactchooser-element.tpl',
+        );
+    }
+
+    public static function default_copy_type() {
+        return 'full';
+    }
+
+}
---
+++ htdocs/artefact/file/blocktype/pdf/viewer.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2013 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage blocktype-pdf
+ * @author     Son Nguyen, Catalyst IT Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006-2013 Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+/**
+ * This displays a pdf in an <iframe>
+ *
+ */
+define('INTERNAL', 1);
+define('PUBLIC', 1);
+require(dirname(dirname(dirname(dirname(dirname(__FILE__))))) . '/init.php');
+
+$fileid = param_integer('file');
+$viewid = param_integer('view');
+
+if (!artefact_in_view($fileid, $viewid)) {
+    throw new AccessDeniedException('');
+}
+
+if (!can_view_view($viewid)) {
+    throw new AccessDeniedException('');
+}
+
+$file = artefact_instance_from_id($fileid);
+if (!($file instanceof ArtefactTypeFile)) {
+    throw new NotFoundException();
+}
+
+$smarty = smarty();
+$smarty->assign('url', get_config('wwwroot') . 'artefact/file/download.php?file=' . $fileid);
+$smarty->display('blocktype:pdf:pdf.tpl');
