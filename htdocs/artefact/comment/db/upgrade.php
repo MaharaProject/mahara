d221f3624d791114d4f6fc217884ad9e230337af
Aaron Wells <aaronw@catalyst.net.nz>
2013-07-24 11:28:33 +1200

    Only show "comment updated" message when the comment description or title is updated
    Bug1201055
    
    Change-Id: Id767f48df9cbb04060cb35361eaf943fb7430b2d

--- htdocs/artefact/comment/db/upgrade.php
+++ htdocs/artefact/comment/db/upgrade.php
@@ -39,5 +39,21 @@ function xmldb_artefact_comment_upgrade($oldversion=0) {
         $success = $success && add_field($table, $field);
     }
 
+    if ($oldversion < 2013072400) {
+        $table = new XMLDBTable('artefact_comment_comment');
+        $field = new XMLDBField('lastcontentupdate');
+        $field->setAttributes(XMLDB_TYPE_DATETIME);
+        $success = $success && add_field($table, $field);
+
+        $success = $success && execute_sql(
+            'update {artefact_comment_comment} acc
+            set lastcontentupdate = (
+                select a.mtime
+                from {artefact} a
+                where a.id = acc.artefact
+            )'
+        );
+    }
+
     return $success;
 }
