63f993efdc6930ff5736f25b61c7d71cabb9de1c
Robert Lyon <robertl@catalyst.net.nz>
2013-07-24 14:06:16 +1200

    Version bump for 1.5.12testing
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -30,8 +30,8 @@ defined('INTERNAL') || die();
 $config = new StdClass;
 // See https://wiki.mahara.org/index.php/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.
-$config->version = 2012041022;
-$config->release = '1.5.11';
+$config->version = 2012041023;
+$config->release = '1.5.12testing';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
