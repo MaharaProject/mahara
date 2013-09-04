042715c1b2c1d8e47533ca8aaa8c4eab7658d774
Aaron Wells <aaronw@catalyst.net.nz>
2013-09-04 13:59:10 +1200

    Clean up config-defaults.php and config-dist.php
    Bug1185297 In order to set apart the sample code from the commentary, I'm putting all the commentary
    into phpdocs style /** ... */ blocks, which trendy IDE's will highlight in a different color
    
    Also putting a line in the top of config-dist.php to tell users about the existence of
    config-defaults.php
    
    Change-Id: I3e587cf441ed14808a24d148e969df5d08041254

--- htdocs/config-dist.php
+++ htdocs/config-dist.php
@@ -25,21 +25,28 @@
  *
  */
 
-//
-// MAHARA CONFIGURATION FILE
-//
-// Copy this file from config-dist.php to config.php, and change the values in 
-// it to suit your environment.
-//
-// Information about this file is available on the Mahara wiki:
-// http://wiki.mahara.org/System_Administrator's_Guide/Installing_Mahara#Create_Mahara's_config.php
-//
+/**
+ * MAHARA CONFIGURATION FILE
+ *
+ * INSTRUCTIONS:
+ * 1. Copy this file from config-dist.php to config.php
+ * 2. Change the values in it to suit your environment.
+ *
+ * Information about this file is available on the Mahara wiki:
+ *     http://wiki.mahara.org/System_Administrator's_Guide/Installing_Mahara#Create_Mahara's_config.php
+ *
+ * This file includes only the most commonly used Mahara configuration directives. For more options
+ * that can be placed in this file, see the Mahara lib file
+ *     htdocs/lib/config-defaults.php
+ */
 
-$cfg = new StdClass;
+$cfg = new stdClass();
 
 
-// database connection details
-// valid values for dbtype are 'postgres8' and 'mysql5'
+/**
+ * database connection details
+ * valid values for dbtype are 'postgres8' and 'mysql5'
+ */
 $cfg->dbtype   = 'postgres8';
 $cfg->dbhost   = 'localhost';
 $cfg->dbport   = null;
@@ -47,57 +54,75 @@ $cfg->dbname   = '';
 $cfg->dbuser   = '';
 $cfg->dbpass   = '';
 
-// Note: database prefix is NOT required, you don't need to set one except if 
-// you're installing Mahara into a database being shared with other 
-// applications (this happens most often on shared hosting)
+/**
+ * Note: database prefix is NOT required, you don't need to set one except if
+ * you're installing Mahara into a database being shared with other
+ * applications (this happens most often on shared hosting)
+ */
 $cfg->dbprefix = '';
 
-// wwwroot - the web-visible path to your Mahara installation
-// Normally, this is automatically detected - if it doesn't work for you
-// then try specifying it here.
-// This value must end with a /
-//$cfg->wwwroot = 'http://myhost.com/mahara/';
-// If you want to serve all of your Mahara content via HTTPS, just set
-// $cfg->wwwroot to use HTTPS.
-//$cfg->wwwroot = 'https://myhost.com/mahara/';
+/**
+ * wwwroot - the web-visible path to your Mahara installation
+ * Normally, this is automatically detected - if it doesn't work for you
+ * then try specifying it here.
+ * This value must end with a /
+ *
+ * Example: $cfg->wwwroot = 'http://myhost.com/mahara/';
+ *
+ * If you want to serve all of your Mahara content via HTTPS, just set
+ * $cfg->wwwroot to use HTTPS.
+ */
+// $cfg->wwwroot = 'https://myhost.com/mahara/';
 
-// If you are using a proxy to force HTTPS connections, you will need to
-// enable the next line. If you have set this to true, ensure your wwwroot
-// is a HTTPS address.
-//$cfg->sslproxy = true;
+/**
+ * If you are using a proxy to force HTTPS connections, you will need to
+ * enable the next line. If you have set this to true, ensure your wwwroot
+ * is a HTTPS address.
+ */
+// $cfg->sslproxy = true;
 
-// dataroot - uploaded files are stored here
-// This is a ABSOLUTE FILESYSTEM PATH. This is NOT a URL.
-// For example, valid paths are:
-//  * /home/user/maharadata
-//  * /var/lib/mahara
-//  * c:\maharadata
-// INVALID paths:
-//  * http://yoursite/files
-//  * ~/files
-//  * ../data
-//
-// This path must be writable by the webserver and outside document root (the 
-// place where the Mahara files like index.php have been installed).
-// Mahara will NOT RUN if this is inside your document root, because
-// this is a big security hole.
+/**
+ * dataroot - uploaded files are stored here
+ * This is a ABSOLUTE FILESYSTEM PATH. This is NOT a URL.
+ * For example, valid paths are:
+ *  * /home/user/maharadata
+ *  * /var/lib/mahara
+ *  * c:\maharadata
+ * INVALID paths:
+ *  * http://yoursite/files
+ *  * ~/files
+ *  * ../data
+ *
+ * This path must be writable by the webserver and outside document root (the
+ * place where the Mahara files like index.php have been installed).
+ * Mahara will NOT RUN if this is inside your document root, because
+ * this is a big security hole.
+ */
 $cfg->dataroot = '/path/to/uploaddir';
 
-// If set, this email address will be displayed in the error message if a form
-// submission is suspected of being spam. This reduces the frustration for the
-// user in the event of a false positive.
+/**
+ * If set, this email address will be displayed in the error message if a form
+ * submission is suspected of being spam. This reduces the frustration for the
+ * user in the event of a false positive.
+ */
 $cfg->emailcontact = '';
 
-// Set this to enable a secondary hash that is only present in the config file
+/**
+ * Set this to enable a secondary hash that is only present in the config file
+ */
 // $cfg->passwordsaltmain = 'some long random string here with lots of characters';
 
-// When changing the salt (or disabling it), you will need to set the current salt as an alternate salt
-// There are up to 20 alternate salts
-// $cfg->passwordsaltalt1 = 'old salt value';
+/**
+ * When changing the salt (or disabling it), you will need to set the current salt as an alternate salt
+ * There are up to 20 alternate salts
+ */
+$cfg->passwordsaltalt1 = 'old salt value';
 
-// Uncomment the following line if this server is not a production system.
-// This will put a line up the top of the page saying that it isn't a production
-// site, and that files may not be present.
+/**
+ * Uncomment the following line if this server is not a production system.
+ * This will put a line up the top of the page saying that it isn't a production
+ * site, and that files may not be present.
+ */
 //$cfg->productionmode = false;
 
 // closing php tag intentionally omitted to prevent whitespace issues
