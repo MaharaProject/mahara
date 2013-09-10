d6b5a053a0ff0f9e42497ab6a22a6ddb82b7c3b4
Evonne Cheung <evonne@catalyst.net.nz>
2013-09-11 10:07:46 +1200

    Adding new icon images for plan and tasks
    Change-Id: I631b4a441ee1ba89bf1e2c217b784e45f80db552
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/artefact/plans/lib.php
+++ htdocs/artefact/plans/lib.php
@@ -88,7 +88,7 @@ class ArtefactTypePlan extends ArtefactType {
 
     public static function get_icon($options=null) {
         global $THEME;
-        return $THEME->get_url('images/plan.gif', false, 'artefact/plans');
+        return $THEME->get_url('images/plan.png', false, 'artefact/plans');
     }
 
     public static function is_singular() {
@@ -358,7 +358,7 @@ class ArtefactTypeTask extends ArtefactType {
 
     public static function get_icon($options=null) {
         global $THEME;
-        return $THEME->get_url('images/plantask.gif', false, 'artefact/plans');
+        return $THEME->get_url('images/plantask.png', false, 'artefact/plans');
     }
 
     public static function is_singular() {
