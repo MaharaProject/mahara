5f4f047debc07fd814ed485267f056944a1e6d5b
Son Nguyen <son.nguyen@catalyst.net.nz>
2013-10-03 11:24:40 +1300

    Version bump for 1.6.8testing
    Signed-off-by: Son Nguyen <son.nguyen@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -30,8 +30,8 @@ defined('INTERNAL') || die();
 $config = new StdClass;
 // See https://wiki.mahara.org/index.php/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
-$config->version = 2012080619;
-$config->release = '1.6.7';
+$config->version = 2012080620;
+$config->release = '1.6.8testing';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
