29776e6aa23198c8db3229325c59173600c5504d
Aaron Wells <aaronw@catalyst.net.nz>
2013-10-11 18:18:47 +1300

    Make sure the artefact/file/originals directory exists before trying to upgrade it
    Since I only upgrade to 1.4_STABLE as part of backwards-compatibility tests, I'm
    often upgrading a cleanly installed site where this directory doesn't exist yet.
    
    Change-Id: If270d93f7ea58b6e3b157aa3a68797bf95557e87

--- htdocs/artefact/file/db/upgrade.php
+++ htdocs/artefact/file/db/upgrade.php
@@ -272,18 +272,20 @@ function xmldb_artefact_file_upgrade($oldversion=0) {
     }
 
     if ($oldversion < 2010012703) {
-        $baseiter = new DirectoryIterator(get_config('dataroot') . 'artefact/file/originals/');
-        foreach ($baseiter as $dir) {
-            if ($dir->isDot()) continue;
-            $dirpath = $dir->getPath() . '/' . $dir->getFilename();
-            $fileiter = new DirectoryIterator($dirpath);
-            foreach ($fileiter as $file) {
-                if ($file->isDot()) continue;
-                if (!$file->isFile()) {
-                    log_error("Something was wrong about the dataroot in artefact/file/originals/$dir. Unexpected folder $file");
-                    continue;
+        if (check_dir_exists(get_config('dataroot') . 'artefact/file/originals/', false)) {
+            $baseiter = new DirectoryIterator(get_config('dataroot') . 'artefact/file/originals/');
+            foreach ($baseiter as $dir) {
+                if ($dir->isDot()) continue;
+                $dirpath = $dir->getPath() . '/' . $dir->getFilename();
+                $fileiter = new DirectoryIterator($dirpath);
+                foreach ($fileiter as $file) {
+                    if ($file->isDot()) continue;
+                    if (!$file->isFile()) {
+                        log_error("Something was wrong about the dataroot in artefact/file/originals/$dir. Unexpected folder $file");
+                        continue;
+                    }
+                    chmod($file->getPathname(), $file->getPerms() & 0666);
                 }
-                chmod($file->getPathname(), $file->getPerms() & 0666);
             }
         }
     }
