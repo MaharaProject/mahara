0acbe44a4fef3d766f0b10843384bf3085f6ecf8
Aaron Wells <aaronw@catalyst.net.nz>
2013-10-14 22:56:44 +1300

    Version bump for 1.8rc3dev
    Signed-off-by: Aaron Wells <aaronw@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -15,8 +15,8 @@ $config = new stdClass();
 
 // See https://wiki.mahara.org/index.php/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
-$config->version = 2013101400;
-$config->release = '1.8rc2';
+$config->version = 2013101401;
+$config->release = '1.8rc3dev';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
