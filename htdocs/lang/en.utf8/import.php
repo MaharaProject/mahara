c306cc278e7ad30c676f9feb6c4aaf1d45d30aea
Robert Lyon <robertl@catalyst.net.nz>
2013-10-30 09:03:16 +1300

    Fixing up lang string with extra </a> tag (bug #1244037)
    - also tidied up license header to match new format used
    
    Change-Id: I17a3c82c88e96cd02aeacf0da95d5d2d26143858
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lang/en.utf8/import.php
+++ htdocs/lang/en.utf8/import.php
@@ -1,27 +1,11 @@
 <?php
 /**
- * Mahara: Electronic portfolio, weblog, resume builder and social networking
- * Copyright (C) 2006-2013 Catalyst IT Ltd and others; see:
- *                         http://wiki.mahara.org/Contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package    mahara
  * @subpackage lang
  * @author     Catalyst IT Ltd
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @copyright  (C) 2006-2013 Catalyst IT Ltd http://catalyst.net.nz
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
  */
 
@@ -30,7 +14,7 @@ defined('INTERNAL') || die();
 $string['Import'] = 'Import';
 $string['importexceedquota'] = 'The total size of the new files in your uploaded Leap2A file would exceed your quota. You need to remove some of the files and try again.';
 $string['importsuccessfully'] = 'Your portfolio was imported successfully';
-$string['importfailed'] = '<p><strong>Sorry - the import of your Leap2A file failed.</strong></p><p>This could be because you did not select a valid Leap2A file to upload or because the version of your Leap2A file is not supported by this Mahara version. Alternatively, there may be a bug in Mahara causing the import to fail, even though the Leap2A file is valid.</p><p>Please go back and try again</a>, and if the problem persists, you may want to post to the <a href="http://mahara.org/forums/">Mahara Forums</a> to ask for help. Be prepared to be asked for a copy of your file.</p>';
+$string['importfailed'] = '<p><strong>Sorry - the import of your Leap2A file failed.</strong></p><p>This could be because you did not select a valid Leap2A file to upload or because the version of your Leap2A file is not supported by this Mahara version. Alternatively, there may be a bug in Mahara causing the import to fail, even though the Leap2A file is valid.</p><p>Please go back and try again, and if the problem persists, you may want to post to the <a href="http://mahara.org/forums/">Mahara Forums</a> to ask for help. Be prepared to be asked for a copy of your file.</p>';
 $string['importwitherrors'] = 'Your portfolio was imported with some errors.';
 $string['importartefactplugindata'] = 'Importing artefact plugin data';
 $string['importartefacts'] = 'Importing artefacts';
