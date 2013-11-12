666df54987d91ea3522a8d7e9df36aff73950929
Aaron Wells <aaronw@catalyst.net.nz>
2013-11-12 16:45:16 +1300

    Revert "Enable 'No institution' users to use skins (Bug 1249123) if it is activated in config.php"
    This reverts commit 35a6a62ad98d9ff830291eb14815e8b0704b09c6.
    
    Change-Id: I5902b77157fdb8df7c73dba27921c832c69b338b

--- htdocs/admin/site/options.php
+++ htdocs/admin/site/options.php
@@ -107,13 +107,6 @@ $siteoptionform = array(
                     'defaultvalue' => get_config('homepageinfo'),
                     'disabled'     => in_array('homepageinfo', $OVERRIDDEN),
                 ),
-                'skins' => array(
-                    'type'         => 'checkbox',
-                    'title'        => get_string('skins', 'admin'),
-                    'description'  => get_string('skinssitedescription', 'admin'),
-                    'defaultvalue' => get_config('skins'),
-                    'disabled'     => in_array('skins', $OVERRIDDEN),
-                ),
                 'registration_sendweeklyupdates' => array(
                     'type'         => 'checkbox',
                     'title'        => get_string('sendweeklyupdates', 'admin'),
@@ -688,7 +681,7 @@ function siteoptions_fail(Pieform $form, $field) {
 
 function siteoptions_submit(Pieform $form, $values) {
     $fields = array(
-        'sitename','lang','theme', 'dropdownmenu', 'skins',
+        'sitename','lang','theme', 'dropdownmenu',
         'defaultaccountlifetime', 'defaultregistrationexpirylifetime', 'defaultaccountinactiveexpire', 'defaultaccountinactivewarn',
         'defaultaccountlifetimeupdate', 'allowpublicviews', 'allowpublicprofiles', 'generatesitemap',
         'registration_sendweeklyupdates', 'institutionexpirynotification', 'institutionautosuspend',
