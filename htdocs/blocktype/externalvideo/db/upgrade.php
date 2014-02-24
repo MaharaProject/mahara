f4489fee4e846eb0ae163e68002a351b0b588fd0
Jono Mingard <jonom@catalyst.net.nz>
2014-02-24 16:55:36 +1300

    Use iframe to embed Prezi presentations (Bug #1239928)
    Also upgrades the old <object> syntax to use iframes
    
    Change-Id: I1d58c1a42ddf0e3db52a19c185ff64242f926cd2
    Signed-off-by: Jono Mingard <jonom@catalyst.net.nz>

---
+++ htdocs/blocktype/externalvideo/db/upgrade.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage blocktype-textbox
+ * @author     Catalyst IT Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+safe_require('blocktype', 'externalvideo');
+
+function xmldb_blocktype_externalvideo_upgrade($oldversion=0) {
+    if ($oldversion < 2014030500) {
+        $urlpattern = '#\b(https?://)prezi\.com/bin/preziloader\.swf\?prezi_id=([a-z0-9]+)\b#';
+        $matches = array();
+
+        $sql = "SELECT id, configdata FROM {block_instance} WHERE blocktype='externalvideo'";
+        $records = get_records_sql_array($sql, array());
+
+        if ($records) {
+            foreach ($records as $r) {
+                $configdata = unserialize($r->configdata);
+
+                if (isset($configdata['html'])) {
+                    preg_match($urlpattern, $configdata['html'], $matches);
+                }
+                else if (isset($configdata['videoid'])) {
+                    preg_match($urlpattern, $configdata['videoid'], $matches);
+                }
+
+                if (count($matches) >= 3) {
+                    $newurl = $matches[1] . 'prezi.com/embed/' . $matches[2];
+                    $width  = (!empty($configdata['width'])) ? $configdata['width'] : 0;
+                    $height = (!empty($configdata['height'])) ? $configdata['height'] : 0;
+
+                    $configdata['html'] = $configdata['videoid'] = PluginBlocktypeExternalvideo::iframe_code($newurl, $width, $height);
+                    set_field('block_instance', 'configdata', serialize($configdata), 'id', $r->id);
+                }
+            }
+        }
+
+        ensure_record_exists('iframe_source_icon', (object) array('name' => 'Prezi', 'domain' => 'prezi.com'), (object) array('name' => 'Prezi', 'domain' => 'prezi.com'));
+        ensure_record_exists('iframe_source', (object) array('prefix' => 'prezi.com/embed/', 'name' => 'Prezi'), (object) array('prefix' => 'prezi.com/embed/', 'name' => 'Prezi'));
+        update_safe_iframe_regex();
+    }
+
+    return true;
+}
