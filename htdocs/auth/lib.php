e700f2df82b3eb0e34ee4c7987bfdc371803eebf
Aaron Wells <aaronw@catalyst.net.nz>
2014-02-26 12:53:07 +1300

    Don't update "lastaccess" and "lastlogin" when login fails due to being suspended
    Bug 1284869
    
    Change-Id: Ic7c116dd09b501c35df61f735063f4c187016585

--- htdocs/auth/lib.php
+++ htdocs/auth/lib.php
@@ -1532,6 +1532,9 @@ function login_submit(Pieform $form, $values) {
     }
 
     auth_check_admin_section();
+
+    // This is also checked in $USER->login(), but it's good to check it again here in case a buggy auth plugin
+    // lets a suspended user through somehow.
     ensure_user_account_is_active();
 
     // User is allowed to log in
