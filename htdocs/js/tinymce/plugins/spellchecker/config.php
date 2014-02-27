75797883be7b3ed7e32a039ca1434f51ade69519
Aaron Wells <aaronw@catalyst.net.nz>
2014-02-27 14:31:57 +1300

    Replace obsolete TinyMCE GoogleSpell with in-browser spellcheck as default
    Bug 1212541: The web service the TinyMCE GoogleSpell code was using,
    is no longer in existence. This patch changes things so that TinyMCE will
    let the browser's spellchecker work, by default.
    
    The "pathtoaspell" is now null by default. If pathtoaspell is null, we
    eliminate the TinyMCE spellcheck button and set the "gecko_spellcheck"
    flag to let the browser spellcheck work. If pathtoaspell is not null,
    we show the TinyMCE spellcheck button as before.
    
    Change-Id: I9fd2b12169669d3d9705a1e23dc6c03af6f01948

--- htdocs/js/tinymce/plugins/spellchecker/config.php
+++ htdocs/js/tinymce/plugins/spellchecker/config.php
@@ -4,31 +4,28 @@
  *
  * @package MCManager.includes
  */
-	// General settings
-	$config['general.engine'] = 'GoogleSpell';
-	//$config['general.engine'] = 'PSpell';
-	//$config['general.engine'] = 'PSpellShell';
-	//$config['general.remote_rpc_url'] = 'http://some.other.site/some/url/rpc.php';
-	
-	define('INTERNAL', 1);
-	require_once(dirname(__FILE__).'/../../../../init.php');
-	$aspellpath = get_config('pathtoaspell');
-	if (file_exists($aspellpath) && is_executable($aspellpath)) {
-		$config['general.engine'] = 'PSpellShell';
-	}
-	
-	    // PSpell settings
-	$config['PSpell.mode'] = 'PSPELL_FAST';
-	$config['PSpell.spelling'] = "";
-	$config['PSpell.jargon'] = "";
-	$config['PSpell.encoding'] = "";
 
-	// PSpellShell settings
-	$config['PSpellShell.mode'] = 'PSPELL_FAST';
-	$config['PSpellShell.aspell'] = $aspellpath;
-	$config['PSpellShell.tmp'] = '/tmp';
+define('INTERNAL', 1);
+require_once(dirname(__FILE__).'/../../../../init.php');
+$aspellpath = get_config('pathtoaspell');
+if (file_exists($aspellpath) && is_executable($aspellpath)) {
+    $config['general.engine'] = 'PSpellShell';
+}
+else {
+    throw FeatureNotEnabledException;
+}
 
-	// Windows PSpellShell settings
-	//$config['PSpellShell.aspell'] = '"c:\Program Files\Aspell\bin\aspell.exe"';
-	//$config['PSpellShell.tmp'] = 'c:/temp';
-?>
+// PSpell settings
+$config['PSpell.mode'] = 'PSPELL_FAST';
+$config['PSpell.spelling'] = "";
+$config['PSpell.jargon'] = "";
+$config['PSpell.encoding'] = "";
+
+// PSpellShell settings
+$config['PSpellShell.mode'] = 'PSPELL_FAST';
+$config['PSpellShell.aspell'] = $aspellpath;
+$config['PSpellShell.tmp'] = '/tmp';
+
+// Windows PSpellShell settings
+//$config['PSpellShell.aspell'] = '"c:\Program Files\Aspell\bin\aspell.exe"';
+//$config['PSpellShell.tmp'] = 'c:/temp';
