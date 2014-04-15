ccbf63bbaf768784757dd8b7a6e3841eb55ad9cd
Aaron Wells <aaronw@catalyst.net.nz>
2014-04-15 17:26:55 +1200

    Version bump for 1.9.0
    Signed-off-by: Aaron Wells <aaronw@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -15,8 +15,8 @@ $config = new stdClass();
 
 // See https://wiki.mahara.org/index.php/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
-$config->version = 2014032704;
-$config->release = '1.9rc2dev';
+$config->version = 2014032705;
+$config->release = '1.9.0';
 $config->minupgradefrom = 2009022600;
 $config->minupgraderelease = '1.1.0 (release tag 1.1.0_RELEASE)';
 $config->disablelogin = true;
