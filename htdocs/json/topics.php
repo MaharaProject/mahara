4ed1764326d218bddeb8bc2ab64c5dcd3aa264e9
Son Nguyen <son.nguyen@catalyst.net.nz>
2014-04-29 11:55:03 +1200

    Add the group.php library. Bug 1391621
    This lib provides the function group_homepage_url()
    Backporting bug 1313963
    
    Change-Id: I70c1c3b411180758a9bcc363aa3377513c915a19
    Signed-off-by: Son Nguyen <son.nguyen@catalyst.net.nz>

--- htdocs/json/topics.php
+++ htdocs/json/topics.php
@@ -14,6 +14,7 @@ define('JSON', 1);
 
 require(dirname(dirname(__FILE__)) . '/init.php');
 safe_require('interaction', 'forum');
+require_once('group.php');
 
 $limit  = param_integer('limit', 10);
 $offset = param_integer('offset', 0);
@@ -24,7 +25,7 @@ $data = PluginInteractionForum::get_active_topics($limit, $offset, $category);
 $pagination = build_pagination(array(
     'id' => 'topics_pagination',
     'url' => get_config('wwwroot') . 'group/topics.php' . ($category ? ('?category=' . (int) $category) : ''),
-    'jsonscript' => '/json/topics.php',
+    'jsonscript' => 'json/topics.php',
     'datatable' => 'topiclist',
     'count' => $data['count'],
     'limit' => $limit,
