7e705c14e8f9106111d45902ba5ec94a2df386a3
Aaron Wells <aaronw@catalyst.net.nz>
2014-05-13 12:03:19 +1200

    Version bump for 1.6.11testing
    Signed-off-by: Aaron Wells <aaronw@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -30,8 +30,8 @@ defined('INTERNAL') || die();
 $config = new StdClass;
 // See https://wiki.mahara.org/index.php/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
-$config->version = 2012080626;
-$config->release = '1.6.10';
+$config->version = 2012080627;
+$config->release = '1.6.11testing';
 $config->minupgradefrom = 2008040200;
 $config->minupgraderelease = '1.0.0 (release tag 1.0.0_RELEASE)';
 $config->disablelogin = true;
