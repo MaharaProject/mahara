37b61d96b5b5d24f2ea5f0b405e90e730ddc5357
Tobias Zeuch <tobias.zeuch@kit.edu>
2014-07-08 17:09:02 +0200

    New blocktype to display a simple text
    Bug 1339113: A blocktype to display a plain snipped of text on a page that is
    not connected to any artefact and therefore does not cause unintentioned side
    effects when content is added on a view that has been copied.
    
    Change-Id: Ib01448cf08f08ff5738bd1814d9e2027daff006d
    Signed-off-by: Tobias Zeuch <tobias.zeuch@kit.edu>

---
+++ htdocs/blocktype/text/lang/en.utf8/blocktype.text.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage blocktype-text
+ * @author     Catalyst IT Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title']                = 'Text';
+$string['description']          = 'Add text snippets to your page';
+$string['blockcontent']         = 'Block content';
+$string['optionlegend']         = 'Convert "Note" blocks';
+$string['convertdescriptionfeatures'] = 'You can convert all re-usable "Note" blocks that do not use any advanced features into simple "Text" blocks. These blocks only exist on the page on which they were created and cannot be chosen for use on other pages. Advanced features include:
+    <ul>
+        <li>re-use in another block</li>
+        <li>use of a license</li>
+        <li>use of tags</li>
+        <li>attachments</li>
+        <li>feedback on the displayed note-artefact</li>
+    </ul>';
+$string['convertdescription'] = array(
+    0 => 'There is %s note that can be converted. If you select the option to convert this note, please be aware that this may take some time. Once the conversion is finished, you will see a success message on this page.',
+    1 => 'There are %d notes that can be converted. If you select the option to convert these notes, please be aware that this may take some time. Once the conversion is finished, you will see a success message on this page.'
+);
+$string['convertibleokmessage'] = array(
+    0 => 'Successfully converted 1 "Note" block to "Text" block',
+    1 => 'Successfully converted %d "Note" blocks to "Text" blocks'
+);
+$string['checkdescription']     = 'If checked, all "Note" blocks that do not use any advanced features will be converted into simple "Text" blocks';
