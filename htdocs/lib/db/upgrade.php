ff381c44344ff396dedc5474f0e9e320922c049e
Robert Lyon <robertl@catalyst.net.nz>
2014-07-30 21:17:17 +1200

    Fixing upgrade script (Bug #1350254)
    Problem with ensure_record_exists checking nextrun column will break
    things when adding new cron line to cron table.
    
    This is due the the nextrun not being null after it has run.
    
    Change-Id: I1f674cbc842a3e061224bf996cc0267b09018f55
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/db/upgrade.php
+++ htdocs/lib/db/upgrade.php
@@ -3408,7 +3408,6 @@ function xmldb_core_upgrade($oldversion=0) {
 
     if ($oldversion < 2014062000) {
         $data = array('callfunction' => 'auth_clean_expired_password_requests',
-                      'nextrun' => null,
                       'minute' => '5',
                       'hour' => '0',
                       'day' => '*',
