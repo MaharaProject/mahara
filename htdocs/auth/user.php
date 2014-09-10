69041d9e60de31b73ba0c18efc22cfa47598ef63
Robert Lyon <robertl@catalyst.net.nz>
2014-09-11 10:47:05 +1200

    Register confirm allow user to copy collection (Bug #1367939)
    See bug for testing
    
    Change-Id: Iddc02c10d163ad363b5c0acddd324393c8e00dbc
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/auth/user.php
+++ htdocs/auth/user.php
@@ -1219,7 +1219,8 @@ class User {
         require_once(get_config('libroot') . 'view.php');
 
         $views = array();
-        foreach (get_records_select_array('view', 'id IN (' . implode(', ', db_array_to_ph($templateids)) . ')', $templateids, '', 'id, title, description, type') as $result) {
+        $results = get_records_select_array('view', 'id IN (' . implode(', ', db_array_to_ph($templateids)) . ')', $templateids, '', 'id, title, description, type');
+        foreach ($results as $result) {
             $views[$result->id] = $result;
         }
 
@@ -1230,6 +1231,37 @@ class User {
                 'title' => $views[$tid]->title,
                 'description' => $views[$tid]->description,
                 'type' => $views[$tid]->type == 'profile' && $checkviewaccess ? 'portfolio' : $views[$tid]->type,
+            ), $tid, $this->get('id'), $checkviewaccess);
+        }
+        db_commit();
+    }
+
+    /**
+     * Makes a literal copy of a list of collections for this user.
+     *
+     * @param array $templateids A list of collectionids to copy.
+     */
+    public function copy_collections($templateids, $checkviewaccess=true) {
+        if (!$templateids) {
+            // Nothing to do
+            return;
+        }
+        if (!is_array($templateids)) {
+            throw new SystemException('User->copy_collections: templateids must be a list of templates to copy for the user');
+        }
+        require_once(get_config('libroot') . 'collection.php');
+
+        $collections = array();
+        $results = get_records_select_array('collection', 'id IN (' . implode(', ', db_array_to_ph($templateids)) . ')', $templateids, '', 'id, name');
+        foreach ($results as $result) {
+            $collections[$result->id] = $result;
+        }
+
+        db_begin();
+        foreach ($templateids as $tid) {
+            Collection::create_from_template(array(
+                'owner' => $this->get('id'),
+                'title' => $collections[$tid]->name,
             ), $tid, $this->get('id'), $checkviewaccess);
         }
         db_commit();
@@ -1259,12 +1291,7 @@ class User {
             INNER JOIN {collection} c ON cv.collection = c.id
             WHERE v.copynewuser = 1
                 AND v.institution = 'mahara'", array());
-        if ($templatecollectionids) {
-            require_once('collection.php');
-            foreach ($templatecollectionids as $templateid) {
-                Collection::create_from_template(array('owner' => $this->get('id')), $templateid, null, false, true);
-            }
-        }
+        $this->copy_collections($templatecollectionids, false);
     }
 
     /**
@@ -1272,7 +1299,7 @@ class User {
      * All institution views and collections which set to "copy to new institution member"
      * will be copied to this user's profile.
      *
-     * @param $institution: ID of the institution to join
+     * @param string $institution        ID of the institution to join
      */
     public function copy_institution_views_collections_to_new_member($institution) {
         if (empty($institution)) {
@@ -1296,12 +1323,7 @@ class User {
             INNER JOIN {collection} c ON cv.collection = c.id
             WHERE v.copynewuser = 1
                 AND v.institution = ?", array($institution));
-        if ($templatecollectionids) {
-            require_once('collection.php');
-            foreach ($templatecollectionids as $templateid) {
-                Collection::create_from_template(array('owner' => $this->get('id')), $templateid, null, false, true);
-            }
-        }
+        $this->copy_collections($templatecollectionids, false);
     }
 
 }
