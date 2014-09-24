99e9850ecad9bed51b9b93d7e92f706c33f97c39
Robert Lyon <robertl@catalyst.net.nz>
2014-09-25 07:58:37 +1200

    Getting old extension when unzipping a .tar file (Bug #1318995)
    Change-Id: Ib77c0970d6f1e0d3248b58744349b49447d7c413
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/artefact/file/lib.php
+++ htdocs/artefact/file/lib.php
@@ -2614,6 +2614,7 @@ class ArtefactTypeArchive extends ArtefactTypeFile {
 
                 // set the file extension for later use (eg by flowplayer)
                 $this->data['template']->extension = pathinfo($this->data['template']->title, PATHINFO_EXTENSION);
+                $this->data['template']->oldextension = $this->data['template']->extension;
 
                 if (substr($name, -1) == '/') {
                     $this->create_folder($folder);
