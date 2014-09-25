5cad4ce7cc70cf8e99e825565290c9b49d70ab4d
Aaron Wells <aaronw@catalyst.net.nz>
2014-09-25 15:10:18 +1200

    Bumping version to 1.11dev!
    Change-Id: I0aa729107c81714022d2847aa277de726dcc2282

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,8 +16,8 @@ $config = new stdClass();
 // See https://wiki.mahara.org/index.php/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
-$config->version = 2014092300;
-$config->release = '1.10.0dev';
+$config->version = 2014092500;
+$config->release = '1.11.0dev';
 $config->minupgradefrom = 2009022600;
 $config->minupgraderelease = '1.1.0 (release tag 1.1.0_RELEASE)';
 $config->disablelogin = true;
