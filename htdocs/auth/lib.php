b4c1755f02d1be190e4c2b8ab8f83293d0c84b7f
Aaron Wells <aaronw@catalyst.net.nz>
2015-01-26 17:38:09 +1300

    Bug 1427901: Performance improvements for cron
    - Cron doesn't need to run auth_setup()
    - Don't run cron when site is closed for upgrade
    - Get rid of forcelocalupgrade() option because it's no longer needed
    
    Change-Id: I1718b13337c50fadc0573d04f5b3d6b20bc842c2

--- htdocs/auth/lib.php
+++ htdocs/auth/lib.php
@@ -390,27 +390,6 @@ function auth_setup () {
         return;
     }
 
-    // Lock the site until core upgrades are done
-    require(get_config('libroot') . 'version.php');
-    $siteclosed    = $config->version > get_config('version');
-    $disablelogin  = $config->disablelogin;
-
-    if (!$siteclosed && get_config('forcelocalupgrades')) {
-        require(get_config('docroot') . 'local/version.php');
-        $siteclosed = $config->version > get_config('localversion');
-    }
-
-    $cfgsiteclosed = get_config('siteclosed');
-    if ($siteclosed && !$cfgsiteclosed || !$siteclosed && $cfgsiteclosed) {
-        // If the admin closed the site manually, open it automatically
-        // when an upgrade is successful.
-        if ($cfgsiteclosed && get_config('siteclosedbyadmin')) {
-            set_config('siteclosedbyadmin', false);
-        }
-        set_config('siteclosed', $siteclosed);
-        set_config('disablelogin', $disablelogin);
-    }
-
     // Check the time that the session is set to log out. If the user does
     // not have a session, this time will be 0.
     $sessionlogouttime = $USER->get('logout_time');
