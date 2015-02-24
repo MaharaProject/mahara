555f20f2e9fe8406e62117e56f8eeb3ef5478c92
Yuliya Bozhko <yuliya.bozhko@totaralms.com>
2015-02-25 11:44:54 +1300

    Make sure submitted page cannot be deleted via URL (Bug #1425306)
    Change-Id: I4536536d254d9dc85c9eb8405bd6ab61c0ae26e9
    Signed-off-by: Yuliya Bozhko <yuliya.bozhko@totaralms.com>

--- htdocs/view/delete.php
+++ htdocs/view/delete.php
@@ -17,7 +17,7 @@ $viewid = param_integer('id');
 
 $view = new View($viewid, null);
 
-if (!$view || $view->get('owner') == "0" || !$USER->can_edit_view($view)) {
+if (!$view || $view->get('owner') == "0" || !$USER->can_edit_view($view) || $view->is_submitted()) {
     throw new AccessDeniedException(get_string('cantdeleteview', 'view'));
 }
 $groupid = $view->get('group');
