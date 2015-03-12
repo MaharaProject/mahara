45175b13bc40209107d2e308acb46087d1460651
Robert Lyon <robertl@catalyst.net.nz>
2015-03-13 09:03:39 +1300

    Allow plugins to register admin menu items (Bug #1431540)
    To test add to a plugin:
     function admin_menu_items() { }
     function institution_menu_items() { }
     function institution_staff_menu_items() { }
    
    where each has proper markup for a menu item
    
    Change-Id: I1cc0e906f08c90616b9f3111c2c630eb72650a7d
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/web.php
+++ htdocs/lib/web.php
@@ -2372,6 +2372,19 @@ function admin_nav() {
            'weight' => 76,
         );
     }
+
+    // enable plugins to augment the admin menu structure
+    foreach (array('artefact', 'interaction', 'module', 'auth') as $plugintype) {
+        if ($plugins = plugins_installed($plugintype)) {
+            foreach ($plugins as &$plugin) {
+                if (safe_require_plugin($plugintype, $plugin->name)) {
+                    $plugin_menu = call_static_method(generate_class_name($plugintype,$plugin->name), 'admin_menu_items');
+                    $menu = array_merge($menu, $plugin_menu);
+                }
+            }
+        }
+    }
+
     return $menu;
 }
 
@@ -2546,8 +2559,19 @@ function institutional_admin_nav() {
         );
     };
 
-    return $ret;
+    // enable plugins to augment the institution admin menu structure
+    foreach (array('artefact', 'interaction', 'module', 'auth') as $plugintype) {
+        if ($plugins = plugins_installed($plugintype)) {
+            foreach ($plugins as &$plugin) {
+                if (safe_require_plugin($plugintype, $plugin->name)) {
+                    $plugin_menu = call_static_method(generate_class_name($plugintype,$plugin->name), 'institution_menu_items');
+                    $ret = array_merge($ret, $plugin_menu);
+                }
+            }
+        }
+    }
 
+    return $ret;
 }
 
 /**
@@ -2559,7 +2583,7 @@ function institutional_admin_nav() {
  * @return a data structure containing the staff navigation
  */
 function staff_nav() {
-    return array(
+    $menu = array(
         'usersearch' => array(
             'path'   => 'usersearch',
             'url'    => 'admin/users/search.php',
@@ -2582,6 +2606,20 @@ function staff_nav() {
             'accesskey' => 'i',
         ),
     );
+
+    // enable plugins to augment the institution staff menu structure
+    foreach (array('artefact', 'interaction', 'module', 'auth') as $plugintype) {
+        if ($plugins = plugins_installed($plugintype)) {
+            foreach ($plugins as &$plugin) {
+                if (safe_require_plugin($plugintype, $plugin->name)) {
+                    $plugin_menu = call_static_method(generate_class_name($plugintype,$plugin->name), 'institution_staff_menu_items');
+                    $menu = array_merge($menu, $plugin_menu);
+                }
+            }
+        }
+    }
+
+    return $menu;
 }
 
 /**
