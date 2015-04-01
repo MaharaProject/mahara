49718f7fdbd12866da8eeb9f8c5ad5ef5d0673dd
Robert Lyon <robertl@catalyst.net.nz>
2015-04-02 10:42:28 +1300

    Double-check the viewid when setting up watchlist viewing (Bug 1429647)
    A person can alter the viewid passed to the watchlist ajax update and
    so a user can end up watching a view they have no access to
    
    Change-Id: I21d00963ac3d9d53e337bcb0a7162bd2a1da1802
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/js/viewmenu.js
+++ htdocs/js/viewmenu.js
@@ -162,7 +162,9 @@ addLoadEvent(function () {
                 artefactid = null;
             }
             sendjsonrequest(config.wwwroot + 'view/togglewatchlist.json.php', {'view': viewid, 'artefact': artefactid}, 'POST', function(data) {
-                $('toggle_watchlist_link').innerHTML = data.newtext;
+                if (data.newtext) {
+                    $('toggle_watchlist_link').innerHTML = data.newtext;
+                }
             });
         });
     }
