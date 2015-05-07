ce5e28789b0a90e0cee0c33df408980b252936f3
Jono Mingard <jonom@catalyst.net.nz>
2015-05-08 10:19:45 +1200

    Ensure dummy admin email is changed on install (Bug #1378581)
    Email is now changed in all three tables it's stored in during both
    web and cli install
    
    Change-Id: I952e0cbc1d573acdacecad00a4a7eddf2251ede2
    (cherry picked from commit d6cd92425fbb70e4cbd9b6a668af1789997a38f1)

--- htdocs/auth/lib.php
+++ htdocs/auth/lib.php
@@ -1023,6 +1023,7 @@ function requiredfields_submit(Pieform $form, $values) {
                     // we are dealing with the dummy email that is set on install
                     update_record('usr', array('email' => $email), array('id' => $USER->get('id')));
                     update_record('artefact_internal_profile_email', array('email' => $email), array('owner' => $USER->get('id')));
+                    update_record('artefact', array('title' => $email), array('owner' => $USER->get('id'), 'artefacttype' => 'email'));
                 }
             }
             // Check if a validation email has been sent, if not send one
