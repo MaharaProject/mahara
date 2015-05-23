d692dc7dad1f13c43ab40c8114d0b514f358f79d
Son Nguyen <ngson2000@yahoo.com>
2015-05-23 01:44:41 +0000

    Fix pagination in group homepage. Bug 1457246
    Add editing and sortoption parameters when getting the list of group pages,
    shared pages and shared collections
    
    Change-Id: Ie1ca8ca225ac179c55629b25c4402645c959fb48

--- htdocs/blocktype/groupviews/sharedviews.json.php
+++ htdocs/blocktype/groupviews/sharedviews.json.php
@@ -21,9 +21,11 @@ require(dirname(dirname(dirname(__FILE__))) . '/init.php');
 safe_require('blocktype', 'groupviews');
 require_once(get_config('libroot') . 'view.php');
 require_once(get_config('libroot') . 'group.php');
+require_once(get_config('libroot') . 'pieforms/pieform.php');
 
 $offset = param_integer('offset', 0);
 $groupid = param_integer('group');
+$editing = param_boolean('editing', false);
 
 $group_homepage_view = group_get_homepage_view($groupid);
 $bi = group_get_homepage_view_groupview_block($groupid);
@@ -33,27 +35,44 @@ if (!can_view_view($group_homepage_view)) {
 }
 
 $configdata = $bi->get('configdata');
-if (!isset($configdata['showsharedviews'])) {
-    $configdata['showsharedviews'] = 1;
-}
-
 $limit = isset($configdata['count']) ? intval($configdata['count']) : 5;
 $limit = ($limit > 0) ? $limit : 5;
+// Find out what order to sort them by (default is titles)
+if (!isset($configdata['sortsharedviewsby']) || $configdata['sortsharedviewsby'] == PluginBlocktypeGroupViews::SORTBY_TITLE) {
+    $sortsharedviewsby = 'v.title';
+}
+else {
+    $sortsharedviewsby = 'v.mtime DESC';
+}
 
-$sharedviews = (array) View::get_sharedviews_data(
-        $limit,
-        $offset,
-        $groupid,
-        ($configdata['showsharedviews'] == 2 ? false : true)
-);
-foreach ($sharedviews['data'] as &$view) {
-    if (isset($view['template']) && $view['template']) {
-        $view['form'] = pieform(create_view_form($group_homepage_view, null, $view->id));
+// For group members, display a list of views that others have
+// shared to the group
+if (empty($configdata['showsharedviews'])) {
+   $sharedviews = array(
+        'data'   => array(),
+        'count'  => 0,
+        'limit'  => $limit,
+        'offset' => 0
+    );
+}
+else {
+    $sharedviews = (array) View::get_sharedviews_data(
+            $limit,
+            $offset,
+            $groupid,
+            ($configdata['showsharedviews'] == 2 ? false : true),
+            $sortsharedviewsby
+    );
+    foreach ($sharedviews['data'] as &$view) {
+        if (!$editing && isset($view['template']) && $view['template']) {
+            $view['form'] = pieform(create_view_form($group, null, $view->id));
+        }
     }
 }
+
 if (!empty($configdata['showsharedviews']) && isset($sharedviews)) {
     $baseurl = $group_homepage_view->get_url();
-    $baseurl .= (strpos($baseurl, '?') === false ? '?' : '&') . 'group=' . $groupid;
+    $baseurl .= (strpos($baseurl, '?') === false ? '?' : '&') . 'group=' . $groupid . '&editing=' . $editing;
     $pagination = array(
         'baseurl'    => $baseurl,
         'id'         => 'sharedviews_pagination',
