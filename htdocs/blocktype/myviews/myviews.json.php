406c4c8a422cbda82e86069cd6d042b5117a6913
Robert Lyon <robertl@catalyst.net.nz>
2015-05-28 15:01:47 +1200

    Adding paginator to 'My pages' block (Bug #634689)
    As part of the trying to get performance improvements
    
    Change-Id: Ifd4f8799aeff23dbb26210c63200511ec4386860
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

---
+++ htdocs/blocktype/myviews/myviews.json.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage blocktype/groupviews
+ * @author     Catalyst IT Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+/**
+ * returns shared views in a given group id
+ */
+
+define('INTERNAL', 1);
+define('JSON', 1);
+
+require(dirname(dirname(dirname(__FILE__))) . '/init.php');
+
+safe_require('blocktype', 'myviews');
+require_once(get_config('libroot') . 'view.php');
+require_once(get_config('libroot') . 'pieforms/pieform.php');
+
+$offset = param_integer('offset', 0);
+$viewid = param_integer('view');
+$editing = param_boolean('editing', false);
+
+$dashboard = new View($viewid);
+
+if (!can_view_view($dashboard)) {
+    json_reply(true, get_string('accessdenied', 'error'));
+}
+
+$views = View::view_search(null, null, (object) array('owner' => $dashboard->get('owner')), null, 10, $offset, true, null, array('portfolio'));
+$views = (array)$views;
+$baseurl = $dashboard->get_url();
+$baseurl .= (strpos($baseurl, '?') === false ? '?' : '&') . 'view=' . $viewid . '&editing=' . $editing;
+$pagination = array(
+    'baseurl'    => $baseurl,
+    'id'         => 'myviews_pagination',
+    'datatable'  => 'myviewlist',
+    'jsonscript' => 'blocktype/myviews/myviews.json.php',
+    'resultcounttextsingular' => get_string('view', 'view'),
+    'resultcounttextplural'   => get_string('views', 'view'),
+);
+PluginBlocktypeMyViews::render_items($views, 'blocktype:myviews:myviewspaginator.tpl', array(), $pagination);
+
+json_reply(false, array('data' => $views));
