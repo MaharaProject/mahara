55009338a5519252d73a68d417d3b587b9184ff1
Robert Lyon <robertl@catalyst.net.nz>
2015-07-10 13:37:36 +1200

    Version bump for 1.10.5
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,8 +16,8 @@ $config = new stdClass();
 // See https://wiki.mahara.org/index.php/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
-$config->version = 2014092322;
-$config->release = '1.10.5testing';
+$config->version = 2014092323;
+$config->release = '1.10.5';
 $config->series = '1.10';
 $config->minupgradefrom = 2009022600;
 $config->minupgraderelease = '1.1.0 (release tag 1.1.0_RELEASE)';
