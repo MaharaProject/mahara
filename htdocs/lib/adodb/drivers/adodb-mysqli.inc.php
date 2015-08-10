922633a890ccedeb351ee479c2467362bf236938
Robert Lyon <robertl@catalyst.net.nz>
2015-08-10 12:39:36 +1200

    escaping names when showing table / index
    (cherry picked from 08939ca25acddff39b132e4bd823668b98447947)
    
    behatnotneeded
    
    Change-Id: Ic725327bff0e6c399c3ddb33ab3e021f19e4eadf
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/adodb/drivers/adodb-mysqli.inc.php
+++ htdocs/lib/adodb/drivers/adodb-mysqli.inc.php
@@ -345,7 +345,7 @@ class ADODB_mysqli extends ADOConnection {
 		}
 
 		// get index details
-		$rs = $this->Execute(sprintf('SHOW INDEXES FROM %s',$table));
+		$rs = $this->Execute(sprintf('SHOW INDEXES FROM `%s`',$table));
 
 		// restore fetchmode
 		if (isset($savem)) {
@@ -579,10 +579,10 @@ class ADODB_mysqli extends ADOConnection {
 
 		if ($ADODB_FETCH_MODE == ADODB_FETCH_ASSOC || $this->fetchMode == ADODB_FETCH_ASSOC) $associative = true;
 
-		if ( !empty($owner) ) {
-			$table = "$owner.$table";
-		}
-		$a_create_table = $this->getRow(sprintf('SHOW CREATE TABLE %s', $table));
+	    if ( !empty($owner) ) {
+	       $table = "$owner.$table";
+	    }
+	    $a_create_table = $this->getRow(sprintf('SHOW CREATE TABLE `%s`', $table));
 		if ($associative) {
 			$create_sql = isset($a_create_table["Create Table"]) ? $a_create_table["Create Table"] : $a_create_table["Create View"];
 		} else $create_sql = $a_create_table[1];
