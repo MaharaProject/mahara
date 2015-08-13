93ac36d9268953d1815c7b55db7f13751139a534
Pat Kira <pat.kira@catalyst.net.nz>
2015-08-13 13:39:34 +1200

    FIX manage collection pages
    And a small class change on auth/lib.php for Evonne
    
    behatnotneeded
    
    Change-Id: I2cfab3e5797598ed1d11693a79a139fbd2ba5927

---
+++ htdocs/js/manage-collection-pages.js
@@ -0,0 +1,143 @@
+/**
+* Javascript for the manage collection pages
+*
+* @package    mahara
+* @subpackage core
+* @author     Catalyst IT Ltd
+* @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+* @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+*
+*/
+jQuery(function($) {
+    var fixhelper = function(e, div) {
+        var originals = div.children();
+        var helper = div.clone();
+        helper.children().each(function(index) {
+            $(this).width(originals.eq(index).width());
+        });
+        return helper;
+    };
+
+    var updaterows = function(viewid) {
+        var sortorder = $('#collectionviews').sortable('serialize');
+        var collectionid = $('#collectionpages').data('collectionid');
+        $.post(config.wwwroot + "collection/views.json.php", {
+            sesskey: config.sesskey,
+            id: collectionid,
+            direction: sortorder }
+        )
+            .done(function(data) {
+                // update the page with the new table
+                if (data.returnCode == '0') {
+                    $('#collectionviews').replaceWith(data.message.html);
+
+                    if (viewid) {
+                        $('#addviews_view_' + viewid + '_container').remove();
+                        // check if we have just removed the last option leaving
+                        // only the add pages button
+                        if ($('#addviews .checkbox').children().length <= 1) {
+                            // Remove addview button
+                            $('#addviews').remove();
+                            // Disply no page to add message
+                            // And hide bulk select pages buttons
+                            $('.select-pages').addClass('hidden');
+                            $('#nopagetoadd').removeClass('hidden');
+                        }
+                    }
+                    if (data.message.message) {
+
+                        var warningClass = data.message.messagestatus === 'ok' ? 'success' : 'warning';
+
+                        var warnmessage = $('<div id="changestatusline" class="alert alert-dismissible alert-' + warningClass + '" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><p>' + data.message.message + '</p></div>');
+
+                        $('#messages').empty().append(warnmessage);
+                    }
+                    wiresortables();
+                    wireaddrow();
+                }
+            });
+        };
+
+        var wiresortables = function() {
+            $('#collectionviews').sortable({
+                items: '> li',
+                appendTo: document.body,
+                cursor: 'move',
+                helper: fixhelper,
+                opacity: 0.8,
+                placeholder: 'highlight',
+                stop: function(e, ui) {
+                    // Get label within the div using children
+                    // This is for receiving item from all pages list
+                    var labelfor = ui.item.children().attr('for');
+                    if (typeof labelfor !== 'undefined' && labelfor !== false) {
+                        // remove all but the digits
+                        var viewid = ui.item.children().attr('for').replace(/[^\d.]/g,'');
+                        ui.item.replaceWith('<li id="row_' + viewid + '" class="dropped-in-row">' + ui.item.html() + '</li>');
+                        updaterows(viewid);
+                    }
+                    else {
+                        updaterows(false);
+                    }
+                }
+            })
+            .disableSelection()
+            .hover(function() {
+                $(this).css('cursor', 'move');
+            });
+        };
+
+        var wireaddrow = function() {
+            $('#addviews div').draggable({
+                connectToSortable: '#collectionviews',
+                cursor: 'move',
+                revert: 'invalid',
+                helper: 'clone',
+            }).hover(function() {
+                $(this).css('cursor', 'move');
+            });
+        };
+
+        var wiredrop = function() {
+            $('#collectionpages .dropzone-previews').droppable({
+                accept: 'div',
+                activeClass: 'highlight',
+                drop: function (e, ui) {
+                    var labelfor = ui.draggable.children().attr('for');
+                    if (typeof labelfor !== 'undefined' && labelfor !== false) {
+                        // remove all but the digits
+                        var viewid = ui.draggable.children().attr('for').replace(/[^\d.]/g,'');
+                        $('#collectionpages .dropzone-previews').replaceWith('<ol id="collectionviews"><li id="row_' + viewid + '">' + ui.draggable.html() + '</li></ol>');
+                        wiresortables();
+                        updaterows(viewid);
+                    }
+                },
+            });
+        };
+
+        var wireselectall = function() {
+            $("#selectall").click(function(e) {
+                e.preventDefault();
+                $("#addviews :checkbox").prop("checked", true);
+            });
+        };
+
+        var wireselectnone = function() {
+            $("#selectnone").click(function(e) {
+                e.preventDefault();
+                $("#addviews :checkbox").prop("checked", false);
+            });
+        };
+
+        // init
+        if ($('#collectionviews > li').length > 0) {
+            wireaddrow();
+            wiresortables();
+        }
+        else {
+            wireaddrow();
+            wiredrop();
+        }
+        wireselectall();
+        wireselectnone();
+    });
--- htdocs/theme/raw/templates/collection/views.json.tpl
+++ htdocs/theme/raw/templates/collection/views.json.tpl
@@ -1,29 +1,27 @@
 <ol class="list-group" id="collectionviews">
-
     {foreach from=$views.views item=view}
-        <li class="list-group-item" id="row_{$view->view}"> 
+        <li class="list-group-item" id="row_{$view->view}">
             {if $views.count > 1}
                 {if $view->displayorder == $views.min}
-                    
-                    <a href="{$displayurl}&amp;view={$view->view}&amp;direction=down">
-                        <span class="icon icon-long-arrow-down prs"></span>
+                    <a class="btn btn-xs text-default single-arrow-down" href="{$displayurl}&amp;view={$view->view}&amp;direction=down">
+                        <span class="icon icon-lg icon-long-arrow-down prs"></span>
+                        <span class="sr-only">{str tag=moveitemdown}</span>
                     </a>
-                
                 {elseif $view->displayorder == $views.max}
-                
-                    <a href="{$displayurl}&amp;view={$view->view}&amp;direction=up">
-                        <span class="icon icon-long-arrow-up prs"></span>
+                    <a class="btn btn-xs text-default single-arrow-up" href="{$displayurl}&amp;view={$view->view}&amp;direction=up">
+                        <span class="icon icon-lg icon-long-arrow-up prs"></span>
+                        <span class="sr-only">{str tag=moveitemup}</span>
                     </a>
-                
                 {else}
-            
-                    <a href="{$displayurl}&amp;view={$view->view}&amp;direction=up">
-                        <span class="icon icon-long-arrow-up prs"></span>
+                    <a class="btn btn-xs text-default" href="{$displayurl}&amp;view={$view->view}&amp;direction=up">
+                        <span class="icon icon-lg icon-long-arrow-up prs"></span>
+                        <span class="sr-only">{str tag=moveitemup}</span>
                     </a>
-                    <a href="{$displayurl}&amp;view={$view->view}&amp;direction=down">
-                        <span class="icon icon-long-arrow-down "></span>
+                    <a class="btn btn-xs text-default" href="{$displayurl}&amp;view={$view->view}&amp;direction=down">
+                        <span class="icon icon-lg icon-long-arrow-down "></span>
+                        <span class="sr-only">{str tag=moveitemdown}</span>
                     </a>
-                {/if}   
+                {/if}
             {/if}
             <strong>
                 <a href="{$view->fullurl}" class="text-link">
@@ -33,5 +31,4 @@
             {$view->remove|safe}
         </li>
     {/foreach}
-
 </ol>
