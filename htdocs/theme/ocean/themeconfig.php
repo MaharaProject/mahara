7c7f4c3d520e120d2ef1588e933b39a4bda62e72
Toni Gardiner <tonig@catalyst.net.nz>
2015-08-25 15:10:06 +1200

    Created new Ocean theme
      behatnotneeded
    
    Change-Id: Ieeae5a6e915e92d243389438f095a1927d38418d

---
+++ htdocs/theme/ocean/themeconfig.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage core
+ * @author     Catalyst IT Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ */
+
+$theme = new StdClass;
+
+$theme->displayname  = 'Ocean';
+$theme->formrenderer = 'div';
+
+/* Set parent to boolean FALSE  to specify the theme has no parent */
+$theme->parent      = 'raw';
+
+/* This theme includes all css via sass, so we don't need raw's css. */
+$theme->overrideparentcss = true;
+
+/**
+ * There are more themeconfig options available. For an explanation of
+ * all the themeconfig options please look in theme/default/themeconfig.php
+ */
