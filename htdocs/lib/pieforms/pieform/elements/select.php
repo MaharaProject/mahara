e10c81b22df42337d12c0cf7472d74ec44f9bcc9
Jake Blatchford <J.Blatchford@kent.ac.uk>
2015-09-21 10:52:46 +1200

    Bug 1497341: Allowing opt group select fields to preeserve array keys
    When the keys are integers
    
    behatnotneeded
    
    Change-Id: I7904f4225e8cf17e2ecd6b54d1fcdbeb7c2b7c77
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/pieforms/pieform/elements/select.php
+++ htdocs/lib/pieforms/pieform/elements/select.php
@@ -304,7 +304,7 @@ function pieform_element_select_get_options($element) {
     $options = array();
 
     foreach ($element['optgroups'] as $optgroup) {
-        $options = array_merge($options, $optgroup['options']);
+        $options = $options + $optgroup['options'];
     }
 
     return $options;
