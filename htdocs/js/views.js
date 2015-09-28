1e0488c7e23875f6888d39fe82b50fda21f6b98c
Jen Zajac <jen.zajac@catalyst.net.nz>
2015-09-28 17:11:11 +1300

    Edit page - fix drag into narrow col, and block title layout
      behatnotneeded: drag and drop
    
      Bug #1465107
    
    Change-Id: I2928bf03aecd709df08075fbaebed2ef35a0f4a6

--- htdocs/js/views.js
+++ htdocs/js/views.js
@@ -334,9 +334,23 @@
         $('.column .column-content').sortable({
             handle: '.js-heading',
             items: '.js-blockinstance',
-            cursorAt: {left: 5, top: 0},
+            cursorAt: {left: 100, top: 10},
             connectWith: '.js-col-row .column .column-content',
             placeholder: 'block-placeholder',
+            tolerance: "pointer",
+            activate: function(event, ui) {
+                // Fix for dragging blocks to narrow divs:
+                // Wide elements must be centred on narrow divs to make droppable.
+                // This is not always evident to the user.
+                // Instead set a standard small width when starting to sort.
+                // Dynamically setting width on over event doesn't work, as
+                // Sortable seems to cache helper proportions.
+                // Also if height of dragging block is greater than height
+                // row(s) above it then it can't be dropped in that row.
+                // Could use a custom version of Sortable in future?
+                ui.helper.width(200);
+                ui.helper.height('auto');
+            },
             beforeStop: function(event, ui) {
 
                 var whereTo = getBlockinstanceCoordinates(ui.helper);
