aa38a39a2e24800e4eef7b7aa1bb48d1b51ebb17
Robert Lyon <robertl@catalyst.net.nz>
2015-11-04 10:40:30 +1300

    Bug 1512528: The edit access 'Shared with' users fails to paginate
    Normally it should load more options if they exist when one scrolls to
    the bottom of the list, but was not working as the 'more' bits of code
    were left out.
    
    behatnotneeded: can't test a select2 that loads data via ajax yet
    
    Change-Id: Ica89fa229e1c9c2a1e58aa7e2d73882ffb19b65e
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>
    (cherry picked from commit 58d68327c492d8e36d81403d1f474fee0ea1468f)

--- htdocs/module/multirecipientnotification/sendmessage.php
+++ htdocs/module/multirecipientnotification/sendmessage.php
@@ -195,6 +195,9 @@ $form = pieform(array(
             'ajaxurl' => get_config('wwwroot') . 'module/multirecipientnotification/sendmessage.json.php',
             'initfunction' => 'translate_ids_to_names',
             'multiple' => true,
+            'extraparams' => array('escapeMarkup' => 'function (markup) { return markup; }',  // let our custom formatter work
+                                   'templateSelection' => 'function (data) { console.log(data); return data.name; }'
+                                  ),
             'ajaxextraparams' => array(),
             'rules' => array('required' => true),
         ),
