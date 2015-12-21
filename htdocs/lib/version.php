4251b9834fe13256adafbea700f037d65694177d
Robert Lyon <robertl@catalyst.net.nz>
2015-12-22 10:43:57 +1300

    Bug 1528351: Fixing block order drift in database
    Due to a mistake in how blocks were ordered in the system
    
    behatnotneeded - existing tests should be ok
    
    Change-Id: Iac857c85d60e5948b6f95ddccee7a2e8cf43b1b4
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,7 +16,7 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
-$config->version = 2015030422;
+$config->version = 2015030423;
 $config->series = '15.04';
 $config->release = '15.04.6testing';
 $config->minupgradefrom = 2009022600;
