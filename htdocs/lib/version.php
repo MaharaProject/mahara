e46dd1be0449601002f07d728f7ce1457dca550b
Aaron Wells <aaronw@catalyst.net.nz>
2016-03-17 19:14:50 +1300

    Remove "target" attribute from links in user-edited text
    Bug 1558361: TinyMCE will filter them out on the editing
    side, and HTMLPurifier will filter them out on the display
    side.
    
    behatnotneeded: Would require non-trivial new Behat step to
    check whether links have "target" attribute.
    
    Change-Id: If27462b2ca1a382ceeaadb374aade1f795f261bd

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,7 +16,7 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
-$config->version = 2015092915;
+$config->version = 2015092916;
 $config->series = '15.10';
 $config->release = '15.10.2testing';
 $config->minupgradefrom = 2012080604;
