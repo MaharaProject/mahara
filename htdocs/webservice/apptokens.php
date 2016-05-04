ce69a57f4a6719574adae7bd178511c7a3c908e9
Liam Sharpe <liam.sharpe@catalyst.net.nz>
2016-05-05 11:42:24 +1200

    Added a icon to the generate button on webservices - Bug #1545375
    And allow the generate button to have right side when delete button is
    not present
    
    behatnotneeded
    
    Change-Id: Ib1fca4c19b0edcc3e7129601f53169b5a16e0f60

--- htdocs/webservice/apptokens.php
+++ htdocs/webservice/apptokens.php
@@ -167,7 +167,7 @@ if (!empty($dbservices)) {
                                                 'type'  => 'button',
                                                 'usebuttontag' => true,
                                                 'class' => 'btn-default btn-sm',
-                                                'value'   => get_string('gen', 'auth.webservice'),
+                                                'value'   => '<span class="icon icon-refresh"></span> ' . get_string('gen', 'auth.webservice'),
                                                 'elementtitle' => get_string('gen', 'auth.webservice')
                                             ),
                                     ),
@@ -196,7 +196,7 @@ if (!empty($dbservices)) {
                                 ,
                 'type'         => 'html',
                 'key'        => $service->id,
-                'class'        => 'webserviceconfigcontrols btn-group',
+                'class'        => 'webserviceconfigcontrols btn-group' . (empty($service->token) ? ' only-button only-button-top' : ''),
             );
     }
     $pieform = new Pieform($userform);
