032b6ba69ec7fc0db1f6a61ec53402c3c1a1cb49
Aaron Wells <aaronw@catalyst.net.nz>
2016-09-26 15:32:04 +1300

    Bug 1620879: Adding webservices to replicate api/mobile functionality.
    behatnotneeded: can't test in behat
    
    Change-Id: I9b8cfada9122cafe72afea86f573873b9d781bfb
    (cherry picked from commit a617b3c21864aedf1c26f107537da5024a212fb2)

--- htdocs/module/mobileapi/webservice/services.php
+++ htdocs/module/mobileapi/webservice/services.php
@@ -14,7 +14,15 @@ $services = array(
     'Mahara Mobile API' => array(
         'shortname' => 'maharamobile',
         'functions' => [
+            'module_mobileapi_sync',
+            'module_mobileapi_get_blogs',
+            'module_mobileapi_get_folders',
+            'module_mobileapi_get_notifications',
+            'module_mobileapi_get_tags',
+            'module_mobileapi_get_user_profile',
             'module_mobileapi_get_user_profileicon',
+            'module_mobileapi_upload_file',
+            'module_mobileapi_upload_blog_post',
         ],
         'enabled' => 1,
         'restrictedusers' => 0,
@@ -27,10 +35,58 @@ $services = array(
 );
 
 $functions = array(
+    'module_mobileapi_sync' => array(
+        'classname' => 'module_mobileapi_sync',
+        'methodname' => 'sync',
+        'description' => "Retrieve user's own blogs, folders, notifications, tags, and profile information",
+        'type' => 'read',
+    ),
+    'module_mobileapi_get_blogs' => array(
+        'classname' => 'module_mobileapi_sync',
+        'methodname' => 'get_blogs',
+        'description' => "Retrieve user's own blogs",
+        'type' => 'read',
+    ),
+    'module_mobileapi_get_folders' => array(
+        'classname' => 'module_mobileapi_sync',
+        'methodname' => 'get_folders',
+        'description' => "Retrieve user's own folders",
+        'type' => 'read',
+    ),
+    'module_mobileapi_get_notifications' => array(
+        'classname' => 'module_mobileapi_sync',
+        'methodname' => 'get_notifications',
+        'description' => "Retrieve user's own notifications",
+        'type' => 'read',
+    ),
+    'module_mobileapi_get_tags' => array(
+        'classname' => 'module_mobileapi_sync',
+        'methodname' => 'get_tags',
+        'description' => "Retrieve user's own tags",
+        'type' => 'read',
+    ),
+    'module_mobileapi_get_user_profile' => array(
+        'classname' => 'module_mobileapi_sync',
+        'methodname' => 'get_user_profile',
+        'description' => "Retrieve user's own profile information",
+        'type' => 'read',
+    ),
     'module_mobileapi_get_user_profileicon' => array(
         'classname' => 'module_mobileapi_sync',
         'methodname' => 'get_user_profileicon',
         'description' => "Retrieve user's own profile icon",
         'type' => 'read',
+    ),
+    'module_mobileapi_upload_file' => array(
+        'classname' => 'module_mobileapi_upload',
+        'methodname' => 'upload_file',
+        'description' => "Upload a file artefact to user's storage space",
+        'type' => 'write',
+    ),
+    'module_mobileapi_upload_blog_post' => array(
+        'classname' => 'module_mobileapi_upload',
+        'methodname' => 'upload_blog_post',
+        'description' => "Upload a blog post into one of user's blogs",
+        'type' => 'write',
     ),
 );
