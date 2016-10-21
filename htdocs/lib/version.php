b878d38e683017602133e0649dbe86461d659808
Robert Lyon <robertl@catalyst.net.nz>
2016-10-21 13:34:29 +1300

    Version bump for 16.10.1testing
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,8 +16,8 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
-$config->version = 2016090207;
+$config->version = 2016090208;
 $config->series = '16.10';
-$config->release = '16.10.0';
+$config->release = '16.10.1testing';
 $config->minupgradefrom = 2012080604;
 $config->minupgraderelease = '1.6.0 (release tag 1.6.0_RELEASE)';
