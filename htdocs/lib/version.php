a5fec14685428f641abd61cb6a1d8554e1790a16
Cecilia Vela Gurovic <ceciliavg@catalyst.net.nz>
2016-12-13 10:59:21 +1300

    Version bump for 16.10.1
    Signed-off-by: Cecilia Vela Gurovic <ceciliavg@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,8 +16,8 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
-$config->version = 2016090210;
+$config->version = 2016090211;
 $config->series = '16.10';
-$config->release = '16.10.1testing';
+$config->release = '16.10.1';
 $config->minupgradefrom = 2012080604;
 $config->minupgraderelease = '1.6.0 (release tag 1.6.0_RELEASE)';
