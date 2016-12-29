986f34fb824295071fcb929fbae48b74a192093a
Robert Lyon <robertl@catalyst.net.nz>
2016-12-29 21:46:51 +1300

    Version bump for 15.10.8testing
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,9 +16,9 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
-$config->version = 2015092933;
+$config->version = 2015092934;
 $config->series = '15.10';
-$config->release = '15.10.7';
+$config->release = '15.10.8testing';
 $config->minupgradefrom = 2012080604;
 $config->minupgraderelease = '1.6.0 (release tag 1.6.0_RELEASE)';
 $config->disablelogin = true;
