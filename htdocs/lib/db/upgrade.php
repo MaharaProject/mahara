359f354007b26e910d3b1e758b33f21182702d01
Cecilia Vela Gurovic <ceciliavg@catalyst.net.nz>
2017-01-24 11:46:38 +1300

    Bug 1655456: fix shared collection not displaying
    Collection shared to a group was not displaying
    in group page and shared with me page
    after first page of collection was deleted.
    Fixed by resetting the order when deleting a view.
    
    behatnotneeded
    
    Change-Id: I096114ecf50b7a3af6d1393b387073676a984006

--- htdocs/lib/db/upgrade.php
+++ htdocs/lib/db/upgrade.php
@@ -4869,5 +4869,31 @@ function xmldb_core_upgrade($oldversion=0) {
         add_field($table, $field);
     }
 
+    if ($oldversion < 2017012700) {
+        log_debug('Update view positions in collections');
+        $sql = 'SELECT collection, COUNT(*) - 1 AS count, MAX(displayorder) AS maxposition
+                FROM {collection_view}
+                GROUP BY collection
+                HAVING COUNT(*) - 1 != MAX(displayorder)';
+
+        $collections = get_records_sql_array($sql);
+
+        if ($collections) {
+            require_once(get_config('libroot') . 'collection.php');
+            $count = 0;
+            $limit = 1000;
+            $total = count($collections);
+            foreach ($collections as $collectiondata) {
+                $collection = new Collection($collectiondata->collection);
+                $collection->update_display_order();
+                $count++;
+                if (($count % $limit) == 0 || $count == $total) {
+                    log_debug("$count/$total");
+                    set_time_limit(30);
+                }
+            }
+        }
+    }
+
     return $status;
 }
