6d0571751c12e210106831e14c9316c396b32dbd
Robert Lyon <robertl@catalyst.net.nz>
2017-04-13 11:12:02 +1200

    Bug 1680640: Allow multiple submit buttons from one pieform element
    Calling it multisubmit where we pass in an arrays of options/values so
    that it sets up a bunch of 'submit' buttons that all flow thru the
    same [formname]_submit function and be handled there.
    
    This allows the submit buttons to 'live together' on the page and in
    the code.
    
    behatnotneeded
    
    Change-Id: I16864d46ff3ea6bc0cb89e7769ba4caa5c20e53e
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>
    (cherry picked from commit 63316dcc44518df5e6f5ad53ead05d47aee7d594)

---
+++ htdocs/lib/form/elements/multisubmit.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Pieforms: Advanced web forms made easy
+ * Copyright (C) 2006-2008 Catalyst IT Ltd (http://www.catalyst.net.nz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    pieform
+ * @subpackage element
+ * @author     Nigel McNie <nigel@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+/**
+ * Renders a set of submit buttons. Useful if you have multiple choices for a decision that go
+ * to different form update places.
+ *
+ * @param Pieform  $form    The form to render the element for
+ * @param array    $element The element to render
+ * @return string           The HTML for the element
+ */
+function pieform_element_multisubmit(Pieform $form, $element) {
+    if (!isset($element['value']) || !is_array($element['value']) || count($element['value']) != 2) {
+        throw new PieformException('The multisubmit element "' . $element['name']
+            . '" must have a two element array for its value otherwise use "submit" element');
+    }
+    $form->include_plugin('element', 'button');
+    $form->include_plugin('element', 'submit');
+
+    // first try for string indices
+    $plugins = array('button');
+    $elems = '';
+    foreach ($element['options'] as $key => $value) {
+        if (!is_numeric($key) && in_array($key, $plugins)) {
+            $function = 'pieform_element_' . $key;
+        }
+        else {
+            // default to 'submit' element
+            $function = 'pieform_element_submit';
+        }
+        if (function_exists($function)) {
+            $item = $element;
+            $item['class'] = isset($element['class']) ? $element['class'] . ' ' . $key : $key;
+            // A primary choice will give the submit option the btn-primary class
+            if (isset($element['primarychoice']) && $element['primarychoice'] === $value) {
+                $item['class'] .= ' btn-primary';
+            }
+            $item['usebuttontag'] = ($key == 'button') ? true : false;
+            $item['value'] = $element['value'][$key];
+            $item['name'] = $element['name'] . '[' . $key .']';
+            $item['option'] = $element['options'][$key];
+            if (isset($element['confirm']) && isset($element['confirm'][$key])) {
+                $item['confirm'] = $element['confirm'][$key];
+            }
+            else {
+                unset($item['confirm']);
+            }
+            $elems .= $function($form, $item);
+            $elems .= ' ';
+        }
+    }
+
+    if (!empty($elems)) {
+        return $elems;
+    }
+}
+
+function pieform_element_multisubmit_set_attributes($element) {
+    $element['submitelement'] = true;
+    return $element;
+}
+
+function pieform_element_multisubmit_get_value(Pieform $form, $element) {
+    if (!empty($_POST[$element['name']])) {
+        return $element['options'][key($_POST[$element['name']])];
+
+    }
+    else if (is_array($element['value'])) {
+        return $element['value'][0];
+    }
+    else {
+        return $element['value'];
+    }
+}
