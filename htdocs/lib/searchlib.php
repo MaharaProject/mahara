061b1e41a1c9b4bd3b56062b7b9ccd0489f15458
Kristina D.C. Hoeppner <kristina@catalyst.net.nz>
2017-06-01 20:08:37 +1000

    Bug 1694908: Fix URL for user search
    - The URL was incorrect and resulted in an error message.
    - Added missing adminusersearch.js
    
    behatnotneeded
    
    Change-Id: I02c59c2f67bf051803c479ea6b466e547aea2724

--- htdocs/lib/searchlib.php
+++ htdocs/lib/searchlib.php
@@ -692,13 +692,13 @@ function build_admin_export_queue_results($search, $offset, $limit) {
         'exportselect' => array(
             'headhtml' => get_string('requeue', 'export') . '<br><div class="btn-group" role="group"><a class="btn btn-xs btn-default" href="" id="selectallexport">' . get_string('All') . '</a>&nbsp;<a class="btn btn-xs btn-default" href="" id="selectnoneexport">' . get_string('none') . '</a></div>',
             'template' => 'admin/users/searchselectcolumnexport.tpl',
-            'class'    => 'nojs-hidden-table-cell with-checkbox',
+            'class'    => 'nojs-hidden-table-cell with-selectall',
             'accessible' => get_string('bulkselect'),
         ),
         'deleteselect' => array(
             'headhtml' => get_string('delete') . '<br><div class="btn-group" role="group"><a class="btn btn-xs btn-default" href="" id="selectalldelete">' . get_string('All') . '</a>&nbsp;<a class="btn btn-xs btn-default" href="" id="selectnonedelete">' . get_string('none') . '</a></div>',
             'template' => 'admin/users/searchselectcolumnexportdelete.tpl',
-            'class'    => 'nojs-hidden-table-cell with-checkbox',
+            'class'    => 'nojs-hidden-table-cell with-selectall',
             'accessible' => get_string('bulkselect'),
         ),
     );
