69bcdb52be49481c03b26410553169bfc0acbcb5
Cecilia Vela Gurovic <ceciliavg@catalyst.net.nz>
2017-07-05 13:16:07 +1200

    Security Bug 1701978: fix session cookie issues
    1. when a user logs in it clears any obsolete
       usr_session cookies for the user
    2. recording the user-agent of the session
       and if it changes to prompt the user to
       login again
    3. when self adding / editing email address(es)
       send 2 emails
    	- one to the new email address asking user to confirm address
    	- and one to the primary email address to alert user
    	that a new email is being added to their account and
    	if this is bad how to contact their admin about the problem.
    
    behatnotneeded
    Change-Id: Ia44b66cf831abd553b72aa8b1d58d2a2634863b8

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,7 +16,7 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
-$config->version = 2016090228;
+$config->version = 2016090229;
 $config->series = '16.10';
 $config->release = '16.10.6testing';
 $config->minupgradefrom = 2012080604;
