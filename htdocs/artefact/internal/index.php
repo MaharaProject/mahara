424ded281718e23acfb08c4c0cc7772b2bbd9585
Cecilia Vela Gurovic <ceciliavg@catalyst.net.nz>
2017-07-05 13:16:07 +1200

    Security Bug 1701978: fix session cookie issues
    1. when a user logs in it clears any obsolete
       usr_session cookies for the user
    2. recording the user-agent of the session
       and if it changes to prompt the user to
       login again
    3. when self adding / editing email address(es)
       send 2 emails
    	- one to the new email address asking user to confirm address
    	- and one to the primary email address to alert user
    	that a new email is being added to their account and
    	if this is bad how to contact their admin about the problem.
    
    behatnotneeded
    Change-Id: Ia44b66cf831abd553b72aa8b1d58d2a2634863b8

--- htdocs/artefact/internal/index.php
+++ htdocs/artefact/internal/index.php
@@ -260,6 +260,7 @@ function profileform_submit(Pieform $form, $values) {
     $email_errors = array();
 
     $lockedfields = locked_profile_fields();
+    $alertuserofnewemail = array();
 
     foreach ($element_list as $element => $type) {
 
@@ -285,6 +286,8 @@ function profileform_submit(Pieform $form, $values) {
                 $key_url_decline = $key_url . '&decline=1';
 
                 try {
+                    $alertuserofnewemail[] = $email;
+
                     $sitename = get_config('sitename');
                     email_user(
                         (object)array(
@@ -316,6 +319,33 @@ function profileform_submit(Pieform $form, $values) {
                         'expiry'   => db_format_timestamp(time() + 86400),
                     )
                 );
+            }
+
+            // alert user that new email addresses have been added
+            if (!empty($alertuserofnewemail)) {
+                $emails = implode(', ', $alertuserofnewemail);
+                try {
+                    $sitename = get_config('sitename');
+                    email_user(
+                        (object)array(
+                            'id'            => $USER->get('id'),
+                            'username'      => $USER->get('username'),
+                            'firstname'     => $USER->get('firstname'),
+                            'lastname'      => $USER->get('lastname'),
+                            'preferredname' => $USER->get('preferredname'),
+                            'admin'         => $USER->get('admin'),
+                            'staff'         => $USER->get('staff'),
+                            'email'         => $profilefields['email']['default'],
+                        ),
+                        null,
+                        get_string('newemailalert_subject', 'artefact.internal', $sitename),
+                        get_string('newemailalert_body_text', 'artefact.internal', $USER->get('firstname'), $sitename, $emails, $sitename, get_config('wwwroot')),
+                        get_string('newemailalert_body_html', 'artefact.internal', hsc($USER->get('firstname')), hsc($sitename), hsc($emails), hsc($sitename), get_config('wwwroot'))
+                    );
+                }
+                catch (EmailException $e) {
+                    $email_errors[] = $profilefields['email']['default'];
+                }
             }
 
             // remove old addresses
@@ -388,10 +418,7 @@ function profileform_submit(Pieform $form, $values) {
                 $USER->commit();
             }
         }
-        else if ($element == 'maildisabled') {
-            continue;
-        }
-        else if ($element == 'socialprofile') {
+        else if (in_array($element, array('maildisabled', 'socialprofile'))) {
             continue;
         }
         else {
--- htdocs/lib/db/upgrade.php
+++ htdocs/lib/db/upgrade.php
@@ -4615,5 +4615,16 @@ function xmldb_core_upgrade($oldversion=0) {
         }
     }
 
+    if ($oldversion < 2016033135) {
+        log_debug('Add an "useragent" field to usr_session table');
+        $table = new XMLDBTable('usr_session');
+        $field = new XMLDBField('useragent');
+        $field->setType(XMLDB_TYPE_TEXT);
+        $field->setLength('small');
+        if (!field_exists($table, $field)) {
+            add_field($table, $field);
+        }
+    }
+
     return $status;
 }
