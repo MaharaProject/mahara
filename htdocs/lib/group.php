bc985764b8d2f56f4f7c646a8f7539c92c30c46e
Cecilia Vela Gurovic <ceciliavg@catalyst.net.nz>
2017-07-17 08:25:23 +1200

    Bug 1703680: join group pagination error
    In the screen that displays groups we can join
    the functions associated with the 'join me' button
    are given the correct group number (not the ones from
    the first page of the pagination)
    
    behatnotneeded
    
    Change-Id: I1737947acd67258dd49f8c24bb69aacb5402aafc
    (cherry picked from commit aa9ab0cad9b8bac3a47a7d116aaa4be2049b6760)

--- htdocs/lib/group.php
+++ htdocs/lib/group.php
@@ -1134,7 +1134,8 @@ function group_get_join_form($name, $groupid, $returnto='view') {
             ),
             'group' => array(
                 'type' => 'hidden',
-                'value' => $groupid
+                'value' => $groupid,
+                'sesskey' => true,
             ),
             'returnto' => array(
                 'type' => 'hidden',
