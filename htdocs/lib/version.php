3accbc9197525b892ed49da166b6f01fa00b0aa8
Cecilia Vela Gurovic <ceciliavg@catalyst.net.nz>
2017-09-29 13:50:01 +1300

    Bump master version
    Change-Id: I53d11d1763348e1b2617231a7b66d1af89ea053c

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,8 +16,8 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
-$config->version = 2017092600;
-$config->series = '17.10';
-$config->release = '17.10dev';
+$config->version = 2017092900;
+$config->series = '18.04';
+$config->release = '18.04dev';
 $config->minupgradefrom = 2015030409;
 $config->minupgraderelease = '15.04.0 (release tag 15.04.0_RELEASE)';
