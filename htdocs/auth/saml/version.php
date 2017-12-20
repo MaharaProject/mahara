85c0d8dfe5ab47fc88c7c8a000afd91cb45c6195
Robert Lyon <robertl@catalyst.net.nz>
2017-12-20 14:38:31 +1300

    Bug 1739331: Upgrade SSPHP to 1.15.0
    To take advantage of the new things plus the bug fixes
    
    behatnotneeded
    
    Change-Id: Id7033f4c1308ecbfbd38a76e018a1a4ca4327dda
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/auth/saml/version.php
+++ htdocs/auth/saml/version.php
@@ -11,8 +11,8 @@
 defined('INTERNAL') || die();
 
 $config = new StdClass;
-$config->version = 2017102600;
-$config->release = '1.2.2';
+$config->version = 2017122000;
+$config->release = '1.2.3';
 $config->name = 'saml';
 $config->requires_config = 1;
 $config->requires_parent = 0;
