d1927eb9b9ea0a7b513204d3b0917a0d91a296a5
Robert Lyon <robertl@catalyst.net.nz>
2018-03-23 15:45:24 +1300

    Version bump for 18.04rc3testing
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,8 +16,8 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
-$config->version = 2018022504;
+$config->version = 2018022505;
 $config->series = '18.04';
-$config->release = '18.04rc2';
+$config->release = '18.04rc3testing';
 $config->minupgradefrom = 2015030409;
 $config->minupgraderelease = '15.04.0 (release tag 15.04.0_RELEASE)';
