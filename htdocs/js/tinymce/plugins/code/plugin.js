ecc59aa629b8627a29be1095191fa7e9683b5f47
Cecilia Vela Gurovic <ceciliavg@catalyst.net.nz>
2018-08-16 14:32:42 +1200

    Bug 1779041: Upgrade TinyMCE to v4.8.2
    behatnotneeded
    
    Change-Id: I84a279fd731352cd30d90c47831c1438e0fc4cbb

--- htdocs/js/tinymce/plugins/code/plugin.js
+++ htdocs/js/tinymce/plugins/code/plugin.js
@@ -1,60 +1,94 @@
-/**
- * plugin.js
- *
- * Released under LGPL License.
- * Copyright (c) 1999-2015 Ephox Corp. All rights reserved
- *
- * License: http://www.tinymce.com/license
- * Contributing: http://www.tinymce.com/contributing
- */
+(function () {
+var code = (function () {
+  'use strict';
 
-/*global tinymce:true */
+  var global = tinymce.util.Tools.resolve('tinymce.PluginManager');
 
-tinymce.PluginManager.add('code', function(editor) {
-	function showDialog() {
-		var win = editor.windowManager.open({
-			title: "Source code",
-			body: {
-				type: 'textbox',
-				name: 'code',
-				multiline: true,
-				minWidth: editor.getParam("code_dialog_width", 600),
-				minHeight: editor.getParam("code_dialog_height", Math.min(tinymce.DOM.getViewPort().h - 200, 500)),
-				spellcheck: false,
-				style: 'direction: ltr; text-align: left'
-			},
-			onSubmit: function(e) {
-				// We get a lovely "Wrong document" error in IE 11 if we
-				// don't move the focus to the editor before creating an undo
-				// transation since it tries to make a bookmark for the current selection
-				editor.focus();
+  var global$1 = tinymce.util.Tools.resolve('tinymce.dom.DOMUtils');
 
-				editor.undoManager.transact(function() {
-					editor.setContent(e.data.code);
-				});
+  var getMinWidth = function (editor) {
+    return editor.getParam('code_dialog_width', 600);
+  };
+  var getMinHeight = function (editor) {
+    return editor.getParam('code_dialog_height', Math.min(global$1.DOM.getViewPort().h - 200, 500));
+  };
+  var $_9q3l9ma2jkvrikk5 = {
+    getMinWidth: getMinWidth,
+    getMinHeight: getMinHeight
+  };
 
-				editor.selection.setCursorLocation();
-				editor.nodeChanged();
-			}
-		});
+  var setContent = function (editor, html) {
+    editor.focus();
+    editor.undoManager.transact(function () {
+      editor.setContent(html);
+    });
+    editor.selection.setCursorLocation();
+    editor.nodeChanged();
+  };
+  var getContent = function (editor) {
+    return editor.getContent({ source_view: true });
+  };
+  var $_2r48lha4jkvrikk7 = {
+    setContent: setContent,
+    getContent: getContent
+  };
 
-		// Gecko has a major performance issue with textarea
-		// contents so we need to set it when all reflows are done
-		win.find('#code').value(editor.getContent({source_view: true}));
-	}
+  var open = function (editor) {
+    var minWidth = $_9q3l9ma2jkvrikk5.getMinWidth(editor);
+    var minHeight = $_9q3l9ma2jkvrikk5.getMinHeight(editor);
+    var win = editor.windowManager.open({
+      title: 'Source code',
+      body: {
+        type: 'textbox',
+        name: 'code',
+        multiline: true,
+        minWidth: minWidth,
+        minHeight: minHeight,
+        spellcheck: false,
+        style: 'direction: ltr; text-align: left'
+      },
+      onSubmit: function (e) {
+        $_2r48lha4jkvrikk7.setContent(editor, e.data.code);
+      }
+    });
+    win.find('#code').value($_2r48lha4jkvrikk7.getContent(editor));
+  };
+  var $_2zom2ua1jkvrikk4 = { open: open };
 
-	editor.addCommand("mceCodeEditor", showDialog);
+  var register = function (editor) {
+    editor.addCommand('mceCodeEditor', function () {
+      $_2zom2ua1jkvrikk4.open(editor);
+    });
+  };
+  var $_b5q8dza0jkvrikk3 = { register: register };
 
-	editor.addButton('code', {
-		icon: 'code',
-		tooltip: 'Source code',
-		onclick: showDialog
-	});
+  var register$1 = function (editor) {
+    editor.addButton('code', {
+      icon: 'code',
+      tooltip: 'Source code',
+      onclick: function () {
+        $_2zom2ua1jkvrikk4.open(editor);
+      }
+    });
+    editor.addMenuItem('code', {
+      icon: 'code',
+      text: 'Source code',
+      onclick: function () {
+        $_2zom2ua1jkvrikk4.open(editor);
+      }
+    });
+  };
+  var $_eju4h7a5jkvrikk8 = { register: register$1 };
 
-	editor.addMenuItem('code', {
-		icon: 'code',
-		text: 'Source code',
-		context: 'tools',
-		onclick: showDialog
-	});
-});
+  global.add('code', function (editor) {
+    $_b5q8dza0jkvrikk3.register(editor);
+    $_eju4h7a5jkvrikk8.register(editor);
+    return {};
+  });
+  function Plugin () {
+  }
+
+  return Plugin;
+
+}());
+})();
--- htdocs/js/tinymce/plugins/visualchars/plugin.js
+++ htdocs/js/tinymce/plugins/visualchars/plugin.js
@@ -1,116 +1,505 @@
-/**
- * plugin.js
- *
- * Released under LGPL License.
- * Copyright (c) 1999-2015 Ephox Corp. All rights reserved
- *
- * License: http://www.tinymce.com/license
- * Contributing: http://www.tinymce.com/contributing
- */
+(function () {
+var visualchars = (function () {
+  'use strict';
 
-/*global tinymce:true */
+  var Cell = function (initial) {
+    var value = initial;
+    var get = function () {
+      return value;
+    };
+    var set = function (v) {
+      value = v;
+    };
+    var clone = function () {
+      return Cell(get());
+    };
+    return {
+      get: get,
+      set: set,
+      clone: clone
+    };
+  };
 
-tinymce.PluginManager.add('visualchars', function(editor) {
-	var self = this, state;
+  var global = tinymce.util.Tools.resolve('tinymce.PluginManager');
 
-	function toggleVisualChars(addBookmark) {
-		var node, nodeList, i, body = editor.getBody(), nodeValue, selection = editor.selection, div, bookmark;
-		var charMap, visualCharsRegExp;
+  var get = function (toggleState) {
+    var isEnabled = function () {
+      return toggleState.get();
+    };
+    return { isEnabled: isEnabled };
+  };
+  var $_7yj1t6stjkvrio8o = { get: get };
 
-		charMap = {
-			'\u00a0': 'nbsp',
-			'\u00ad': 'shy'
-		};
+  var fireVisualChars = function (editor, state) {
+    return editor.fire('VisualChars', { state: state });
+  };
+  var $_efmcraswjkvrio8r = { fireVisualChars: fireVisualChars };
 
-		function wrapCharWithSpan(value) {
-			return '<span data-mce-bogus="1" class="mce-' + charMap[value] + '">' + value + '</span>';
-		}
+  var charMap = {
+    '\xA0': 'nbsp',
+    '\xAD': 'shy'
+  };
+  var charMapToRegExp = function (charMap, global) {
+    var key, regExp = '';
+    for (key in charMap) {
+      regExp += key;
+    }
+    return new RegExp('[' + regExp + ']', global ? 'g' : '');
+  };
+  var charMapToSelector = function (charMap) {
+    var key, selector = '';
+    for (key in charMap) {
+      if (selector) {
+        selector += ',';
+      }
+      selector += 'span.mce-' + charMap[key];
+    }
+    return selector;
+  };
+  var $_51vunksyjkvrio8z = {
+    charMap: charMap,
+    regExp: charMapToRegExp(charMap),
+    regExpGlobal: charMapToRegExp(charMap, true),
+    selector: charMapToSelector(charMap),
+    charMapToRegExp: charMapToRegExp,
+    charMapToSelector: charMapToSelector
+  };
 
-		function compileCharMapToRegExp() {
-			var key, regExp = '';
+  var constant = function (value) {
+    return function () {
+      return value;
+    };
+  };
 
-			for (key in charMap) {
-				regExp += key;
-			}
 
-			return new RegExp('[' + regExp + ']', 'g');
-		}
 
-		function compileCharMapToCssSelector() {
-			var key, selector = '';
 
-			for (key in charMap) {
-				if (selector) {
-					selector += ',';
-				}
 
-				selector += 'span.mce-' + charMap[key];
-			}
 
-			return selector;
-		}
 
-		state = !state;
-		self.state = state;
-		editor.fire('VisualChars', {state: state});
-		visualCharsRegExp = compileCharMapToRegExp();
+  var never = constant(false);
+  var always = constant(true);
 
-		if (addBookmark) {
-			bookmark = selection.getBookmark();
-		}
+  var never$1 = never;
+  var always$1 = always;
+  var none = function () {
+    return NONE;
+  };
+  var NONE = function () {
+    var eq = function (o) {
+      return o.isNone();
+    };
+    var call$$1 = function (thunk) {
+      return thunk();
+    };
+    var id = function (n) {
+      return n;
+    };
+    var noop$$1 = function () {
+    };
+    var nul = function () {
+      return null;
+    };
+    var undef = function () {
+      return undefined;
+    };
+    var me = {
+      fold: function (n, s) {
+        return n();
+      },
+      is: never$1,
+      isSome: never$1,
+      isNone: always$1,
+      getOr: id,
+      getOrThunk: call$$1,
+      getOrDie: function (msg) {
+        throw new Error(msg || 'error: getOrDie called on none.');
+      },
+      getOrNull: nul,
+      getOrUndefined: undef,
+      or: id,
+      orThunk: call$$1,
+      map: none,
+      ap: none,
+      each: noop$$1,
+      bind: none,
+      flatten: none,
+      exists: never$1,
+      forall: always$1,
+      filter: none,
+      equals: eq,
+      equals_: eq,
+      toArray: function () {
+        return [];
+      },
+      toString: constant('none()')
+    };
+    if (Object.freeze)
+      Object.freeze(me);
+    return me;
+  }();
+  var some = function (a) {
+    var constant_a = function () {
+      return a;
+    };
+    var self = function () {
+      return me;
+    };
+    var map = function (f) {
+      return some(f(a));
+    };
+    var bind = function (f) {
+      return f(a);
+    };
+    var me = {
+      fold: function (n, s) {
+        return s(a);
+      },
+      is: function (v) {
+        return a === v;
+      },
+      isSome: always$1,
+      isNone: never$1,
+      getOr: constant_a,
+      getOrThunk: constant_a,
+      getOrDie: constant_a,
+      getOrNull: constant_a,
+      getOrUndefined: constant_a,
+      or: self,
+      orThunk: self,
+      map: map,
+      ap: function (optfab) {
+        return optfab.fold(none, function (fab) {
+          return some(fab(a));
+        });
+      },
+      each: function (f) {
+        f(a);
+      },
+      bind: bind,
+      flatten: constant_a,
+      exists: bind,
+      forall: bind,
+      filter: function (f) {
+        return f(a) ? me : NONE;
+      },
+      equals: function (o) {
+        return o.is(a);
+      },
+      equals_: function (o, elementEq) {
+        return o.fold(never$1, function (b) {
+          return elementEq(a, b);
+        });
+      },
+      toArray: function () {
+        return [a];
+      },
+      toString: function () {
+        return 'some(' + a + ')';
+      }
+    };
+    return me;
+  };
+  var from = function (value) {
+    return value === null || value === undefined ? NONE : some(value);
+  };
+  var Option = {
+    some: some,
+    none: none,
+    from: from
+  };
 
-		if (state) {
-			nodeList = [];
-			tinymce.walk(body, function(n) {
-				if (n.nodeType == 3 && n.nodeValue && visualCharsRegExp.test(n.nodeValue)) {
-					nodeList.push(n);
-				}
-			}, 'childNodes');
+  var typeOf = function (x) {
+    if (x === null)
+      return 'null';
+    var t = typeof x;
+    if (t === 'object' && Array.prototype.isPrototypeOf(x))
+      return 'array';
+    if (t === 'object' && String.prototype.isPrototypeOf(x))
+      return 'string';
+    return t;
+  };
+  var isType = function (type) {
+    return function (value) {
+      return typeOf(value) === type;
+    };
+  };
 
-			for (i = 0; i < nodeList.length; i++) {
-				nodeValue = nodeList[i].nodeValue;
-				nodeValue = nodeValue.replace(visualCharsRegExp, wrapCharWithSpan);
 
-				div = editor.dom.create('div', null, nodeValue);
-				while ((node = div.lastChild)) {
-					editor.dom.insertAfter(node, nodeList[i]);
-				}
 
-				editor.dom.remove(nodeList[i]);
-			}
-		} else {
-			nodeList = editor.dom.select(compileCharMapToCssSelector(), body);
 
-			for (i = nodeList.length - 1; i >= 0; i--) {
-				editor.dom.remove(nodeList[i], 1);
-			}
-		}
 
-		selection.moveToBookmark(bookmark);
-	}
 
-	function toggleActiveState() {
-		var self = this;
+  var isFunction = isType('function');
 
-		editor.on('VisualChars', function(e) {
-			self.active(e.state);
-		});
-	}
+  var map = function (xs, f) {
+    var len = xs.length;
+    var r = new Array(len);
+    for (var i = 0; i < len; i++) {
+      var x = xs[i];
+      r[i] = f(x, i, xs);
+    }
+    return r;
+  };
+  var each = function (xs, f) {
+    for (var i = 0, len = xs.length; i < len; i++) {
+      var x = xs[i];
+      f(x, i, xs);
+    }
+  };
 
-	editor.addCommand('mceVisualChars', toggleVisualChars);
 
-	editor.addButton('visualchars', {
-		title: 'Show invisible characters',
-		cmd: 'mceVisualChars',
-		onPostRender: toggleActiveState
-	});
 
-	editor.addMenuItem('visualchars', {
-		text: 'Show invisible characters',
-		cmd: 'mceVisualChars',
-		onPostRender: toggleActiveState,
-		selectable: true,
-		context: 'view',
-		prependToContext: true
-	});
-});
+
+
+
+
+
+
+
+
+
+  var slice = Array.prototype.slice;
+
+
+
+
+
+
+
+  var from$1 = isFunction(Array.from) ? Array.from : function (x) {
+    return slice.call(x);
+  };
+
+  var fromHtml = function (html, scope) {
+    var doc = scope || document;
+    var div = doc.createElement('div');
+    div.innerHTML = html;
+    if (!div.hasChildNodes() || div.childNodes.length > 1) {
+      console.error('HTML does not have a single root node', html);
+      throw 'HTML must have a single root node';
+    }
+    return fromDom(div.childNodes[0]);
+  };
+  var fromTag = function (tag, scope) {
+    var doc = scope || document;
+    var node = doc.createElement(tag);
+    return fromDom(node);
+  };
+  var fromText = function (text, scope) {
+    var doc = scope || document;
+    var node = doc.createTextNode(text);
+    return fromDom(node);
+  };
+  var fromDom = function (node) {
+    if (node === null || node === undefined)
+      throw new Error('Node cannot be null or undefined');
+    return { dom: constant(node) };
+  };
+  var fromPoint = function (docElm, x, y) {
+    var doc = docElm.dom();
+    return Option.from(doc.elementFromPoint(x, y)).map(fromDom);
+  };
+  var Element$$1 = {
+    fromHtml: fromHtml,
+    fromTag: fromTag,
+    fromText: fromText,
+    fromDom: fromDom,
+    fromPoint: fromPoint
+  };
+
+  var $_c6fz83t7jkvrioa6 = {
+    ATTRIBUTE: Node.ATTRIBUTE_NODE,
+    CDATA_SECTION: Node.CDATA_SECTION_NODE,
+    COMMENT: Node.COMMENT_NODE,
+    DOCUMENT: Node.DOCUMENT_NODE,
+    DOCUMENT_TYPE: Node.DOCUMENT_TYPE_NODE,
+    DOCUMENT_FRAGMENT: Node.DOCUMENT_FRAGMENT_NODE,
+    ELEMENT: Node.ELEMENT_NODE,
+    TEXT: Node.TEXT_NODE,
+    PROCESSING_INSTRUCTION: Node.PROCESSING_INSTRUCTION_NODE,
+    ENTITY_REFERENCE: Node.ENTITY_REFERENCE_NODE,
+    ENTITY: Node.ENTITY_NODE,
+    NOTATION: Node.NOTATION_NODE
+  };
+
+  var name = function (element) {
+    var r = element.dom().nodeName;
+    return r.toLowerCase();
+  };
+  var type = function (element) {
+    return element.dom().nodeType;
+  };
+  var value = function (element) {
+    return element.dom().nodeValue;
+  };
+  var isType$1 = function (t) {
+    return function (element) {
+      return type(element) === t;
+    };
+  };
+  var isComment = function (element) {
+    return type(element) === $_c6fz83t7jkvrioa6.COMMENT || name(element) === '#comment';
+  };
+  var isElement = isType$1($_c6fz83t7jkvrioa6.ELEMENT);
+  var isText = isType$1($_c6fz83t7jkvrioa6.TEXT);
+  var isDocument = isType$1($_c6fz83t7jkvrioa6.DOCUMENT);
+  var $_705mcrt6jkvrioa4 = {
+    name: name,
+    type: type,
+    value: value,
+    isElement: isElement,
+    isText: isText,
+    isDocument: isDocument,
+    isComment: isComment
+  };
+
+  var wrapCharWithSpan = function (value) {
+    return '<span data-mce-bogus="1" class="mce-' + $_51vunksyjkvrio8z.charMap[value] + '">' + value + '</span>';
+  };
+  var $_fl06i2t8jkvrioa7 = { wrapCharWithSpan: wrapCharWithSpan };
+
+  var isMatch = function (n) {
+    return $_705mcrt6jkvrioa4.isText(n) && $_705mcrt6jkvrioa4.value(n) !== undefined && $_51vunksyjkvrio8z.regExp.test($_705mcrt6jkvrioa4.value(n));
+  };
+  var filterDescendants = function (scope, predicate) {
+    var result = [];
+    var dom = scope.dom();
+    var children = map(dom.childNodes, Element$$1.fromDom);
+    each(children, function (x) {
+      if (predicate(x)) {
+        result = result.concat([x]);
+      }
+      result = result.concat(filterDescendants(x, predicate));
+    });
+    return result;
+  };
+  var findParentElm = function (elm, rootElm) {
+    while (elm.parentNode) {
+      if (elm.parentNode === rootElm) {
+        return elm;
+      }
+      elm = elm.parentNode;
+    }
+  };
+  var replaceWithSpans = function (html) {
+    return html.replace($_51vunksyjkvrio8z.regExpGlobal, $_fl06i2t8jkvrioa7.wrapCharWithSpan);
+  };
+  var $_2kxcaqszjkvrio91 = {
+    isMatch: isMatch,
+    filterDescendants: filterDescendants,
+    findParentElm: findParentElm,
+    replaceWithSpans: replaceWithSpans
+  };
+
+  var show = function (editor, rootElm) {
+    var node, div;
+    var nodeList = $_2kxcaqszjkvrio91.filterDescendants(Element$$1.fromDom(rootElm), $_2kxcaqszjkvrio91.isMatch);
+    each(nodeList, function (n) {
+      var withSpans = $_2kxcaqszjkvrio91.replaceWithSpans($_705mcrt6jkvrioa4.value(n));
+      div = editor.dom.create('div', null, withSpans);
+      while (node = div.lastChild) {
+        editor.dom.insertAfter(node, n.dom());
+      }
+      editor.dom.remove(n.dom());
+    });
+  };
+  var hide = function (editor, body) {
+    var nodeList = editor.dom.select($_51vunksyjkvrio8z.selector, body);
+    each(nodeList, function (node) {
+      editor.dom.remove(node, 1);
+    });
+  };
+  var toggle = function (editor) {
+    var body = editor.getBody();
+    var bookmark = editor.selection.getBookmark();
+    var parentNode = $_2kxcaqszjkvrio91.findParentElm(editor.selection.getNode(), body);
+    parentNode = parentNode !== undefined ? parentNode : body;
+    hide(editor, parentNode);
+    show(editor, parentNode);
+    editor.selection.moveToBookmark(bookmark);
+  };
+  var $_q7h6tsxjkvrio8s = {
+    show: show,
+    hide: hide,
+    toggle: toggle
+  };
+
+  var toggleVisualChars = function (editor, toggleState) {
+    var body = editor.getBody();
+    var selection = editor.selection;
+    var bookmark;
+    toggleState.set(!toggleState.get());
+    $_efmcraswjkvrio8r.fireVisualChars(editor, toggleState.get());
+    bookmark = selection.getBookmark();
+    if (toggleState.get() === true) {
+      $_q7h6tsxjkvrio8s.show(editor, body);
+    } else {
+      $_q7h6tsxjkvrio8s.hide(editor, body);
+    }
+    selection.moveToBookmark(bookmark);
+  };
+  var $_ae7crrsvjkvrio8q = { toggleVisualChars: toggleVisualChars };
+
+  var register = function (editor, toggleState) {
+    editor.addCommand('mceVisualChars', function () {
+      $_ae7crrsvjkvrio8q.toggleVisualChars(editor, toggleState);
+    });
+  };
+  var $_364dl1sujkvrio8p = { register: register };
+
+  var global$1 = tinymce.util.Tools.resolve('tinymce.util.Delay');
+
+  var setup = function (editor, toggleState) {
+    var debouncedToggle = global$1.debounce(function () {
+      $_q7h6tsxjkvrio8s.toggle(editor);
+    }, 300);
+    if (editor.settings.forced_root_block !== false) {
+      editor.on('keydown', function (e) {
+        if (toggleState.get() === true) {
+          e.keyCode === 13 ? $_q7h6tsxjkvrio8s.toggle(editor) : debouncedToggle();
+        }
+      });
+    }
+  };
+  var $_6ajaedt9jkvrioab = { setup: setup };
+
+  var toggleActiveState = function (editor) {
+    return function (e) {
+      var ctrl = e.control;
+      editor.on('VisualChars', function (e) {
+        ctrl.active(e.state);
+      });
+    };
+  };
+  var register$1 = function (editor) {
+    editor.addButton('visualchars', {
+      active: false,
+      title: 'Show invisible characters',
+      cmd: 'mceVisualChars',
+      onPostRender: toggleActiveState(editor)
+    });
+    editor.addMenuItem('visualchars', {
+      text: 'Show invisible characters',
+      cmd: 'mceVisualChars',
+      onPostRender: toggleActiveState(editor),
+      selectable: true,
+      context: 'view',
+      prependToContext: true
+    });
+  };
+
+  global.add('visualchars', function (editor) {
+    var toggleState = Cell(false);
+    $_364dl1sujkvrio8p.register(editor, toggleState);
+    register$1(editor);
+    $_6ajaedt9jkvrioab.setup(editor, toggleState);
+    return $_7yj1t6stjkvrio8o.get(toggleState);
+  });
+  function Plugin () {
+  }
+
+  return Plugin;
+
+}());
+})();
