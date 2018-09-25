c4d2b5fe6e365bdf9cd72efc0009ac7506f80d36
Robert Lyon <robertl@catalyst.net.nz>
2018-09-26 08:59:21 +1200

    Bug 1794370: Use correct owner for watchlist message
    When sending watchlist notification for block changes in a view
    
    behatnotneeded
    
    Change-Id: Ieeb89c6662acabd8438486702e10433ea6feab70
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>
    (cherry picked from commit 0866ac938ff6d973c14b8238ab8109f6a87440f0)

--- htdocs/lib/activity.php
+++ htdocs/lib/activity.php
@@ -1228,8 +1228,8 @@ class ActivityTypeWatchlist extends ActivityType {
 
     protected $view;
 
-    private $ownerinfo;
-    private $viewinfo;
+    protected $ownerinfo;
+    protected $viewinfo;
 
     /**
      * @param array $data Parameters:
@@ -1311,7 +1311,6 @@ class ActivityTypeWatchlist extends ActivityType {
  */
 class ActivityTypeWatchlistnotification extends ActivityTypeWatchlist{
     protected $view;
-    protected $viewinfo;
     protected $blocktitles = array();
     protected $usr;
 
@@ -1328,7 +1327,6 @@ class ActivityTypeWatchlistnotification extends ActivityTypeWatchlist{
         $this->usr = $data->usr;
         $this->unsubscribelink = get_config('wwwroot') . 'view/unsubscribe.php?a=watchlist&t=';
         $this->unsubscribetype = 'watchlist';
-        $this->viewinfo = new View($this->view);
     }
 
     /**
@@ -1340,7 +1338,7 @@ class ActivityTypeWatchlistnotification extends ActivityTypeWatchlist{
      */
     public function get_message($user) {
         $message = get_string_from_language($user->lang, 'newwatchlistmessageview1', 'activity',
-                                        $this->viewinfo->get('title'), display_name($this->usr, $user));
+                                        $this->viewinfo->get('title'), $this->ownerinfo);
 
         try {
             foreach ($this->blocktitles as $blocktitle) {
