7c6f7a2c1d6ea4ed6e1364f055f44d268dd21ab1
Liam <liam.sharpe@catalyst.net.nz>
2018-12-12 15:34:24 +1300

    Bug 1765276: Fixed reports graph and missing line in php file that gave an error on the styleguide
    behatnotneeded
    
    Change-Id: Ifc7774702e5ffade77a878262f120dab698a87bf

--- htdocs/lib/mahara.php
+++ htdocs/lib/mahara.php
@@ -4052,13 +4052,13 @@ function tags_sideblock() {
             'data'   => $tags,
             'template' => 'sideblocks/tags.tpl',
             'visible' => $USER->is_logged_in() &&
-                         (defined('MENUITEM') &&
-                          in_array(MENUITEM, array('profile',
-                                                   'create/files',
-                                                   'share/sharedbyme',
-                                                   'create/views')
-                                  )
-                         ),
+                        (defined('MENUITEM') &&
+                         in_array(MENUITEM, array('profile',
+                                                  'create/files',
+                                                  'share/sharedbyme',
+                                                  'create/views')
+                                )
+                        ),
         );
         return $sideblock;
     }
