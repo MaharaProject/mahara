bc1fb0f28b6f10a0205d634fc57c861ee06d94c0
Rebecca Blundell <rebeccablundell@catalyst.net.nz>
2019-04-29 11:03:08 +1200

    Bug 1825617: Add subheading to title of "Connected apps" page
    Under the arrow bar nav it should read:
    Sub-heading | Arrow-bar title
    
    behatnotneeded
    
    Change-Id: Iab104cdcffe92f57aff25eab5165d02be0419584

--- htdocs/blocktype/openbadgedisplayer/badgrtoken.php
+++ htdocs/blocktype/openbadgedisplayer/badgrtoken.php
@@ -23,6 +23,7 @@ define('APPS', 1);
 require('./../../init.php');
 safe_require('blocktype', 'openbadgedisplayer');
 define('TITLE', get_string('connectedapps'));
+define('SUBSECTIONHEADING', get_string('badgrtokentitle', 'blocktype.openbadgedisplayer'));
 
 // Users shouldn't be able to access this page if openbadgedisplayer blocktype is not active.
 if (!is_plugin_active('openbadgedisplayer','blocktype')) {
