f3fab509a7ed9ed720cbd09dd367e4f061a3803f
Liam <liam.sharpe@catalyst.net.nz>
2019-07-11 13:45:24 +1200

    Bug #1806524: edit page has duplicated "addblock" in html tags
    behatnotneeded
    
    Change-Id: Ica5567f61330a85b94585b886568b796478c738d
    (cherry picked from commit 3e87aac48617ddc6adfcaebdaa747f53579a4bbb)

--- htdocs/js/views.js
+++ htdocs/js/views.js
@@ -493,7 +493,7 @@
         var position = $(this).val().split('-'),
             element = workspace.find('.js-col-row').eq(parseInt(position[0], 10) - 1).find('.column').eq(parseInt(position[1], 10) - 1),
             options = [get_string('blockordertopcell')],
-            selectbox = $('#addblock_position');
+            selectbox = $('#newblock_position');
 
         element.find('.column-content .blockinstance .blockinstance-header').each(function() {
             options.push(get_string('blockorderafter', 'view', $(this).html()));
