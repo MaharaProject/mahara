be12c634a9d32324d5d49d5f87c05c1086dc5ccb
Cecilia Vela Gurovic <ceciliavg@catalyst.net.nz>
2019-10-10 09:51:01 +1300

    Add release version
    Change-Id: Idd9760d192fce6a44ea8a9a55e71c53bde272958

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -18,6 +18,6 @@ $config = new stdClass();
 
 $config->version = 2019093000;
 $config->series = '19.10';
-$config->release = '19.10dev';
+$config->release = '19.10rc1';
 $config->minupgradefrom = 2017031605;
 $config->minupgraderelease = '17.04.0 (release tag 17.04.0_RELEASE)';
