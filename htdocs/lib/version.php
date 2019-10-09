4e5a72cf7dd920a7fb69c04bf32460930b2c3bb1
Cecilia Vela Gurovic <ceciliavg@catalyst.net.nz>
2019-10-10 10:06:02 +1300

    Version bump for 19.10rc1
    Signed-off-by: Cecilia Vela Gurovic <ceciliavg@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,7 +16,7 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
-$config->version = 2019093000;
+$config->version = 2019093001;
 $config->series = '19.10';
 $config->release = '19.10rc1';
 $config->minupgradefrom = 2017031605;
