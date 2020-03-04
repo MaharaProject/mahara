818899530d4eb33b62899b8cde5554f5bce1fa73
Robert Lyon <robertl@catalyst.net.nz>
2020-03-04 14:17:04 +1300

    Version bump for 19.10.3testing
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,8 +16,8 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
-$config->version = 2019093013;
+$config->version = 2019093014;
 $config->series = '19.10';
-$config->release = '19.10.2';
+$config->release = '19.10.3testing';
 $config->minupgradefrom = 2017031605;
 $config->minupgraderelease = '17.04.0 (release tag 17.04.0_RELEASE)';
