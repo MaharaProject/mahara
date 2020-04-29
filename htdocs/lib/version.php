faa589b3d0c112ac07d6a8ead45ae286ad60e4a6
Lisa Seeto <lisaseeto@catalyst.net.nz>
2020-04-30 10:15:43 +1200

    Version bump for 19.10.4testing
    Signed-off-by: Lisa Seeto <lisaseeto@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,8 +16,8 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
-$config->version = 2019093015;
+$config->version = 2019093016;
 $config->series = '19.10';
-$config->release = '19.10.3';
+$config->release = '19.10.4testing';
 $config->minupgradefrom = 2017031605;
 $config->minupgraderelease = '17.04.0 (release tag 17.04.0_RELEASE)';
