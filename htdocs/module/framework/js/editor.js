7d8f013b1fb78d56009602c317d526562d27a714
Cecilia Vela Gurovic <ceciliavg@catalyst.net.nz>
2020-07-09 18:09:01 +1200

    Bug 1886883: SE editor - add an empty element form
    when the parent standard of the last element is deleted
    
    Test1
    - create a SE framework
    - have more than one standard, only one of them has standard elements
    - delete that standard element
    - the form will show an error 'Value must have at least 1 items'
    - click on Save
    expected result: the form will not save and show an error
    actual result: the form will save and the framework won't have any standard elements on the DB, showing a success message at the top 'Your framework has been submitted'
    
    Test2
    - the previous test leaves the framework in a state with no standard elements, and after loading and saving many times, I can see the default empty element is displayed in the form but it's never saved to DB. It only gets saved when editing the name or changing any field
    
    behatnotneeded
    
    Change-Id: I08b7bb01aa9866a92c267bd30be8f70cd4b6676b

--- htdocs/module/framework/js/editor.js
+++ htdocs/module/framework/js/editor.js
@@ -58,7 +58,10 @@ jQuery(function($) {
     JSONEditor.plugins.select2.enable = true;
 
     var parent_array = [''];
+
+    // standard ids from the Standards form
     var standard_array = [];
+    // standard names from the Standards form
     var standard_names = [];
 
     // Counts to increment standard and standardelement ids
@@ -1021,6 +1024,8 @@ jQuery(function($) {
     * @param standardid id to set
     */
     function update_standard_in_standard_element(index, standardid) {
+        // in case standardid is a string
+        standardid = +standardid;
         // Set the standardid hidden field
         var sid_field = editor.getEditor("root.standardelements." + index + "." + "standardid");
         if (sid_field && standard_array.length > 0) {
@@ -1029,7 +1034,7 @@ jQuery(function($) {
         // Select corresponding element in the dropdown
         var sidoptions_field = editor.getEditor("root.standardelements." + index + "." + "standardoptions");
         set_standard_array_field($(sidoptions_field.container));
-        sidoptions_field.input.selectedIndex = standardid - 1;
+        sidoptions_field.input.selectedIndex = $.inArray(+standardid, standard_array);
     }
 
     function update_standard_shortname_handler() {
@@ -1052,6 +1057,9 @@ jQuery(function($) {
     function update_delete_standard_button_handlers() {
         $('[data-schemaid="standard"] > h3 > div > button.json-editor-btn-delete').off('click');
         $('[data-schemaid="standard"] > h3 > div > button.json-editor-btn-delete').on('click', function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+
             var standardid = 0,
                 last_standardid = 0,
                 standard_array_temp = standard_array;
@@ -1075,6 +1083,11 @@ jQuery(function($) {
             // if the standard is the last one (or the one that has focus) change the standar element count
             if ((last_standardid == standardid)) {
                 update_eid_number();
+            }
+            // if the standard has the last element as child
+            // we need to add an empty standard element to the form (there should be at least one)
+            if (editor.getEditor("root.standardelements").rows.length < 1) {
+                $("#add_standardelement")[0].click();
             }
 
         });
