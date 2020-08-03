579fa7fd8c3f9e8e364ca1aa134ff73e132221d0
Robert Lyon <robertl@catalyst.net.nz>
2020-08-04 10:20:18 +1200

    Bug 1891575: Allow LTI webservice access to all collection pages
    Change-Id: I786096483317010e8a62467db420fd2dacf16450
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/mahara.php
+++ htdocs/lib/mahara.php
@@ -3055,8 +3055,16 @@ function can_view_view($view, $user_id=null) {
     }
 
     // Grant temporary access if LTI signed request was made
-    if ($view->get('id') == $SESSION->get('lti.canviewview')) {
-        return true;
+    if ($ltiview = $SESSION->get('lti.canviewview')) {
+        if ($view->get('id') == $ltiview) {
+            // this page is allowed directly
+            return true;
+        }
+        else if (record_exists_select('collection_view', 'collection = (SELECT collection FROM {collection_view} WHERE view = ?) AND view = ?',
+            array($ltiview, $view->get('id')))) {
+            // this page is part of a collection that is allowed
+            return true;
+        }
     }
 
     // Overriding start/stop dates are set by the owner to deny access
