80340e7e73e8e30229b7e09e6e4513b9acff7990
Lisa Seeto <lisaseeto@catalyst.net.nz>
2020-08-27 15:11:31 +1200

    Bug 1893159: Dashboard widgets are not receiving customisable theme styling
    - add in styling for widgets
    - add upgrade step to show warning to resave custom theme
    
    Change-Id: Ie6b46989200e4a19300ac228229dfe14bf0e8565
    Signed-off-by: Lisa Seeto <lisaseeto@catalyst.net.nz>

--- htdocs/lib/db/upgrade.php
+++ htdocs/lib/db/upgrade.php
@@ -1700,5 +1700,17 @@ function xmldb_core_upgrade($oldversion=0) {
         }
     }
 
+    // set customethemeupdate to true for Bug 1893159s
+    if ($oldversion < 2020013011) {
+        $custom_themes = get_records_sql_array("SELECT name FROM {institution} WHERE theme = ?", array('custom'));
+        if ($custom_themes) {
+            // set_config_institution requires the Institution class.
+            require_once(get_config('docroot') . 'lib/institution.php');
+            foreach ($custom_themes as $inst) {
+                set_config_institution($inst->name, 'customthemeupdate', true);
+            }
+        }
+    }
+
     return $status;
 }
