5af6e2edd72980f9f1b396a104b8beb0a78b219f
Lisa Seeto <lisaseeto@catalyst.net.nz>
2020-12-04 15:41:48 +1300

    Bug 1906383: Use of Image Gallery block means Copy page unavailable
     - change the way that the js script for slideshow is called
       eliminating the need for bootstrap to be included a second time
       (error was because bootstrap was included without popper.js first,
        but it turns out bootstrap didn't have to be included at all)
    
    Change-Id: I3cb530d892105027c9ace08dded0a3717a571834
    Signed-off-by: Lisa Seeto <lisaseeto@catalyst.net.nz>

--- htdocs/lib/view.php
+++ htdocs/lib/view.php
@@ -2690,7 +2690,9 @@ class View {
                 );
                 foreach($instancejs as $jsfile) {
                   if (is_array($jsfile) && isset($jsfile['file'])) {
-                    $javascriptfiles[] = $this->add_blocktype_path($blockinstance, $jsfile['file']);
+                    if (!empty($jsfile['file'])) {
+                      $javascriptfiles[] = $this->add_blocktype_path($blockinstance, $jsfile['file']);
+                    }
                     if (isset($jsfile['initjs'])) {
                       $initjavascripts[] = $jsfile['initjs'];
                     }
@@ -2700,8 +2702,8 @@ class View {
                       }
                     }
                   }
-                  else if (is_string($jsfile)) {
-                    $javascriptfiles[] = $this->add_blocktype_path($blockinstance, $jsfile);;
+                  else if (is_string($jsfile) && !empty($jstring)) {
+                    $javascriptfiles[] = $this->add_blocktype_path($blockinstance, $jsfile);
                   }
                 }
                 // Check to see if we need to include the block Ajax file.
