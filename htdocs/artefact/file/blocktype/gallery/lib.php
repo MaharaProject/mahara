45d435d13847311eed1a75803ad94f1ed726e308
Lisa Seeto <lisaseeto@catalyst.net.nz>
2020-12-04 15:41:48 +1300

    Bug 1906383: Use of Image Gallery block means Copy page unavailable
     - change the way that the js script for slideshow is called
       eliminating the need for bootstrap to be included a second time
       (error was because bootstrap was included without popper.js first,
        but it turns out bootstrap didn't have to be included at all)
    
    Change-Id: I3cb530d892105027c9ace08dded0a3717a571834
    Signed-off-by: Lisa Seeto <lisaseeto@catalyst.net.nz>
    (cherry picked from commit a5bbfc50a62fa967efed7aa0de8b2b6749081208)

--- htdocs/artefact/file/blocktype/gallery/lib.php
+++ htdocs/artefact/file/blocktype/gallery/lib.php
@@ -72,9 +72,11 @@ class PluginBlocktypeGallery extends MaharaCoreBlocktype {
                 'initjs' => " $('.js-masonry.thumbnails').masonry({ itemSelector: '.thumb' });"
             ),
             array(
-                'file' => get_config('wwwroot') . 'lib/bootstrap/assets/javascripts/bootstrap.min.js',
-                'initjs' => "$('#slideshow{$blockid}').on('slid.bs.carousel', function () {
-                    $(window).trigger('colresize');
+                'file' => '',
+                'initjs' => "$(function() {
+                    $('#slideshow{$blockid}').on('slid.bs.carousel', function () {
+                        $(window).trigger('colresize');
+                    });
                 });"
             )
         );
