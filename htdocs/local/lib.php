aab8210da4168ff194a8fca85e01c901976c4a72
Doris Tam <doristam@catalyst.net.nz>
2021-01-19 10:34:12 +1300

    Resolve "WR 349177: 3.7 Name and status display on a portfolio page (customisation)"

--- htdocs/local/lib.php
+++ htdocs/local/lib.php
@@ -7,3 +7,28 @@ function local_expected_account_preferences() {
         'apcstatusdateend' => null,
     );
 }
+
+/**
+ * PCNZ Customisation 3.7 (WR 349177) to display apcstatusdate in display author
+ * below the display name
+ */
+function local_display_author($viewinstance) {
+    $view = null;
+    $apcinfo = '';
+    if (!empty($viewinstance->get('owner'))) {
+        $userobj = new User();
+        $userobj->find_by_id($viewinstance->get('owner'));
+        $view = $userobj->get_profile_view();
+
+        // Hide author if profile isn't visible to account holder
+        if (!$view || !can_view_view($view)) {
+            return null;
+        }
+        $apcinfo = get_account_preference($viewinstance->get('owner'), 'apcstatusactive') ? get_string('apcperiod', 'view', date('j F Y', strtotime(get_account_preference($viewinstance->get('owner'), 'apcstatusdate')))) : '';
+    }
+
+    $ownername = hsc($viewinstance->formatted_owner());
+    $ownerlink = hsc($viewinstance->owner_link());
+
+    return get_string('viewauthor', 'view', $ownerlink, $ownername) . '<div></div>' . $apcinfo;
+}
