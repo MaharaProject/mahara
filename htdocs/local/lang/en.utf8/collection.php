a0be5334ece2c6050ecbdf8f168a7fa1bb7a677f
Kristina Hoeppner <kristina@catalyst.net.nz>
2021-03-01 09:07:03 +1300

    WR 349189: Custom lang string
    Change-Id: I361a3345885102a7c4455256b9848b56a7c33d87

--- htdocs/local/lang/en.utf8/collection.php
+++ htdocs/local/lang/en.utf8/collection.php
@@ -5,3 +5,4 @@ $string['overallcompletion'] = 'Overall completion of individual pages';
 $string['needssignedoff'] = 'Needs to be completed';
 $string['signedoff'] = 'Completed';
 $string['verifiednotavailable'] = 'You cannot verify portfolio "%s" by %s';
+$string['ownerhasremovedaccess'] = '%s has unlinked you from their MyRecert portfolio "%s" as a verifier. The Pharmacy Council thanks you for being a verifier.';
