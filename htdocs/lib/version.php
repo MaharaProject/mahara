8708977a03295d3ae22eeb6b6d38be6d1a88a526
Doris Tam <doristam@catalyst.net.nz>
2021-05-04 13:57:37 +1200

    Version bump for 20.04.4testing
    Signed-off-by: Doris Tam <doristam@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,8 +16,8 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
-$config->version = 2020013016;
+$config->version = 2020013017;
 $config->series = '20.04';
-$config->release = '20.04.3';
+$config->release = '20.04.4testing';
 $config->minupgradefrom = 2017031605;
 $config->minupgraderelease = '18.04.0 (release tag 18.04.0_RELEASE)';
