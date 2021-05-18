1166d222bc1ddf1b48f4ccf3cc0a0dec7728adc2
Robert Lyon <robertl@catalyst.net.nz>
2021-05-18 12:04:06 +1200

    Bug 1928611: Hide android/ios version of tinymce when modal closed
    Change-Id: Ica0da1561756bae5572d86be39d58699a37fd503
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>
    (cherry picked from commit adc54bc23eabc1e83950ccf31452e57b9a9a0565)

--- htdocs/js/views.js
+++ htdocs/js/views.js
@@ -600,8 +600,8 @@
      * Rewrites a configure button to be AJAX
      */
     function rewriteConfigureButton(button) {
-        button.off('click');
-        button.on('click', function(e) {
+        button.off('click touchstart');
+        button.on('click touchstart', function(e) {
             e.stopPropagation();
             e.preventDefault();
 
@@ -622,10 +622,8 @@
      * in that case.
      */
     function rewriteDeleteButton(button, pblockinstanceId) {
-        button.off('click');
-
-        button.on('click', function(e) {
-
+        button.off('click touchstart');
+        button.on('click touchstart', function(e) {
             e.stopPropagation();
             e.preventDefault();
 
