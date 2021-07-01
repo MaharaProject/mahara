d0542ca032ff6e4697c8000c4c0ba6d0c0c7e23c
Robert Lyon <robertl@catalyst.net.nz>
2021-07-01 14:23:30 +1200

    Bug 1932450: Pick SE framework when in multiple institutions
    Patch: Allows a person in multiple institutions that have SE turned on
    to be able to choose institution specific SE frameworks from any of
    those institutions
    
    Change-Id: I8f865e85dec824b15f484bf5a37b7bfdf4e8a0fb
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>
    (cherry picked from commit c4b49a9af9b6c4ff75f4fca2c7f7699b16db1a66)

--- htdocs/lib/collection.php
+++ htdocs/lib/collection.php
@@ -860,7 +860,7 @@ class Collection {
      * - The institution has 'SmartEvidence' turned on
      * - There are frameworks available for the institutions
      *
-     * @return object $institution or false
+     * @return array of $institution objects or false
      */
      public function get_framework_institution() {
         require_once('institution.php');
@@ -875,7 +875,7 @@ class Collection {
         if ($this->institution) {
             $institution = $this->institution;
             $institution = new Institution($institution);
-            $allowsmartevidence = ($institution->allowinstitutionsmartevidence) ? $institution : false;
+            $allowsmartevidence = ($institution->allowinstitutionsmartevidence) ? array($institution) : false;
         }
         else {
             $user = new User();
@@ -885,14 +885,13 @@ class Collection {
                 foreach ($institutionids as $institution) {
                     $institution = new Institution($institution);
                     if ($institution->allowinstitutionsmartevidence == true) {
-                        $allowsmartevidence = $institution;
-                        break;
+                        $allowsmartevidence[] = $institution;
                     }
                 }
             }
             else {
                 $institution = new Institution('mahara');
-                $allowsmartevidence = ($institution->allowinstitutionsmartevidence) ? $institution : false;
+                $allowsmartevidence = ($institution->allowinstitutionsmartevidence) ? array($institution) : false;
             }
         }
         return $allowsmartevidence;
@@ -904,12 +903,12 @@ class Collection {
      * @return array Available frameworks
      */
     public function get_available_frameworks() {
-        $institution = $this->get_framework_institution();
-        if (!$institution) {
+        $institutions = $this->get_framework_institution();
+        if (!$institutions) {
             return array();
         }
 
-        if ($frameworks = Framework::get_frameworks($institution->name, true)) {
+        if ($frameworks = Framework::get_frameworks($institutions, true)) {
             // Inactive frameworks are only allowed if they were added to
             // collection when they were active.
             foreach ($frameworks as $key => $framework) {
