4fb5b8e728707e950afaf64c0c59a3c79803159e
Robert Lyon <robertl@catalyst.net.nz>
2021-09-23 14:22:30 +1200

    Security bug 1944633: Select2 dealing with bad characters
    If we have something like <script>alert(document.domain)</script>
    being put into a select2 field then selected, eg tags for a page, then
    we need to escape the input so that the code isn't executed.
    
    Change-Id: I64b8dbd3d6071e27584d8c5199b2eb35c803c9de
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>
    (cherry picked from commit 8f8fd43ed08e6c8ef614668ce84c269605ba3ca6)
    (cherry picked from commit 756e4ccc7f56be3cf786e84506952987883696f9)

--- htdocs/view/access.json.php
+++ htdocs/view/access.json.php
@@ -155,7 +155,7 @@ switch ($type) {
                 $info = array(
                     'id' => $value->id,
                     'url' => group_homepage_url($value),
-                    'name' => $value->name,
+                    'name' => htmlspecialchars($value->name),
                     'grouptype' => $value->grouptype
                 );
                 $data['data'][$key] = $info;
