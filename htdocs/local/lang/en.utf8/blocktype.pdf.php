fed010a0bb1bf7c172ed94bacb6b843f07264b73
Kristina Hoeppner <kristina@catalyst.net.nz>
2021-10-15 08:39:50 +1300

    WR 368100: Mention 1 PDF only in the block alert notification

---
+++ htdocs/local/lang/en.utf8/blocktype.pdf.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage artefact/file/blocktype-pdf
+ * @author     Son Nguyen, Catalyst IT Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['pdfwarning'] = 'You can add only one PDF in this block.<br /> Note: Displaying large PDF can slow down your browser.';
