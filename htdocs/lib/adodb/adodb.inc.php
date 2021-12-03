6944edaf93a9858f4307a1b91ed91f912d4571ab
Dianne Tennent <dianne.tennent@catalyst.net.nz>
2021-12-03 15:07:57 +1300

    Bug 1950695: Class referenced with incorrect case
    Related to this change: https://reviews.mahara.org/12245
    
    Change-Id: Iab8c221429963d818e8db50eb739388350cb843f

--- htdocs/lib/adodb/adodb.inc.php
+++ htdocs/lib/adodb/adodb.inc.php
@@ -1132,7 +1132,7 @@ if (!defined('_ADODB_LAYER')) {
 			$this->_transOK = false;
 			$this->RollbackTrans();
 			if ($this->debug) {
-				ADOCOnnection::outp("Smart Rollback occurred");
+				ADOConnection::outp("Smart Rollback occurred");
 			}
 		}
 
