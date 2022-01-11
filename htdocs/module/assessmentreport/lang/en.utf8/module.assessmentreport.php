7caa8d794bac012c8c731ce4b596f96ebcf85b7f
Dianne Tennent <dianne.tennent@catalyst.net.nz>
2022-01-11 14:39:56 +1300

    Bug 1953095: 'Submitted host' info for LTI submission
    Update event handler in the submit function in
    view.php to include 'externalhost'
    Adjust strings to be more generic to work with
    both LTI1.1 and 1.3
    
    Change-Id: I80ab755447e8441dc4a30dc3af871eee47575c40
    (cherry picked from commit 119c101fda0d281536bf459793dd2842e9da6947)

--- htdocs/module/assessmentreport/lang/en.utf8/module.assessmentreport.php
+++ htdocs/module/assessmentreport/lang/en.utf8/module.assessmentreport.php
@@ -2,11 +2,11 @@
 
 defined('INTERNAL') || die();
 
-$string["message"] = 'This is to confirm that you submitted the %s "%s" successfully to the group "%s" at %s.
+$string["message1"] = 'This is to confirm that you submitted the %s "%s" successfully to \'%s\' at %s.
 
-If you have any questions about this submission, please contact the group administrator.';
-$string["messageexternal"] = 'This is to confirm that you submitted the %s "%s" successfully to the external host "%s" at %s.
+If you have any questions about this submission, please contact your instructor.';
+$string["messageexternal1"] = 'This is to confirm that you submitted the %s "%s" successfully to the external host "%s" at %s.
 
-If you have any questions about this submission, please contact the group administrator.';
-$string["subject"] = 'Your submission of your %s was successful';
+If you have any questions about this submission, please contact your instructor.';
+$string["subject"] = 'Submission of your %s was successful';
 $string["page"] = "page";
