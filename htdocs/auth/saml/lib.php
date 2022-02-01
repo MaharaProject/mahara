b9233a56852f409bd682d9f4844c7a86674b9dfe
Robert Lyon <robertl@catalyst.net.nz>
2022-02-01 13:50:47 +1300

    Bug 1959634: Update SimpleSamlPhp to version 1.19.5
    To make it better compatible with PHP 8.1
    
    Change-Id: Ic12e110f88b20aa118a5f6617ec48bcabf5ef5b9
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>
    (cherry picked from commit 986299dfb779d98e4b87bf24b553b74a7e017601)

--- htdocs/auth/saml/lib.php
+++ htdocs/auth/saml/lib.php
@@ -838,7 +838,7 @@ class PluginAuthSaml extends PluginAuth {
 
     public static function install_auth_default() {
         // Set library version to download
-        set_config_plugin('auth', 'saml', 'version', '1.19.1');
+        set_config_plugin('auth', 'saml', 'version', '1.19.5');
     }
 
     private static function delete_old_certificates() {
