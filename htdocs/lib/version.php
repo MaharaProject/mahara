bde5454334585684d15b7f1ebb97b6d8c55d21be
Robert Lyon <robertl@catalyst.net.nz>
2022-03-24 11:32:59 +1300

    Bug 1966138: Better seting of email profile field
    Normally when we use set_profile_field() to set an email it will make
    the existing primary email be renamed to the new one.
    But if you already have a secondary email with the same value then
    things go wrong as it tries to set your existing primary to the new
    value rather than just make the secondary one the primary one
    
    Change-Id: Ia82c24c7f8e18eaac8c5c7cfd648c6a4b50ec3c0
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,7 +16,7 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on dev branches, increment the version by one. On main, use the date.
 
-$config->version = 2022032202;
+$config->version = 2022032203;
 $config->series = '22.04';
 $config->release = '22.04rc2testing';
 $config->minupgradefrom = 2020013006;
