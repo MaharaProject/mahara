4d1c3fc4d3a7b38205970e8496bec7f035eeb433
Gold <gold@catalyst.net.nz>
2022-04-12 13:23:46 +1200

    Bug 1967811: Set submittableto = 1 in a MySQL friendly manner.
    This SQL is also compatible with how MySQL does things.
    
    Change-Id: I9630fb137eae0791ffac2d5e9e3ea04e3bed29ef
    Signed-off-by: Gold <gold@catalyst.net.nz>
    (cherry picked from commit 2505ba8b8cf5c3d606c1263570d5e947ade19875)

--- htdocs/lib/db/upgrade.php
+++ htdocs/lib/db/upgrade.php
@@ -2122,10 +2122,15 @@ function xmldb_core_upgrade($oldversion=0) {
     if ($oldversion < 2020092120) {
         log_debug("Make sure groups associated with LTI assessment have 'submittableto' set to true");
         execute_sql("
-            UPDATE {group} SET submittableto = 1 WHERE id IN (
-                SELECT g1.id FROM {lti_assessment} l
-                JOIN {group} g1 ON g1.id = l.group
-                WHERE g1.submittableto = 0
+            UPDATE {group}
+            SET submittableto = 1
+            WHERE id IN (
+                SELECT foo.id FROM (
+                    SELECT g1.id
+                    FROM {lti_assessment} l
+                    JOIN {group} g1 ON g1.id = l.group
+                    WHERE g1.submittableto = 0
+                ) AS foo
             )");
     }
 
