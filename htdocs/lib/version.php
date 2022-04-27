46808bee1dd5d5c925701d72df235529823eb997
Doris Tam <doristam@catalyst.net.nz>
2022-04-27 13:00:44 +1200

    Version bump for 21.10.2
    Signed-off-by: Doris Tam <doristam@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,8 +16,8 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on dev branches, increment the version by one. On main, use the date.
 
-$config->version = 2021080215;
+$config->version = 2021080216;
 $config->series = '21.10';
-$config->release = '21.10.2testing';
+$config->release = '21.10.2';
 $config->minupgradefrom = 2017031605;
 $config->minupgraderelease = '18.10.0 (release tag 18.10.0_RELEASE)';
