17cd2e1904e2298b740f0fdb3ada356d8387a889
Robert Lyon <robertl@catalyst.net.nz>
2022-05-05 11:35:32 +1200

    Bug 1971639: Need to check lowercase on both ends
    In case older sites have entered username or email address into the
    system as camelcase we need to check our value vs supplied value both
    as lowercase
    
    Change-Id: Ib605db75f16d62f53f843b966aebc41814381971
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>
    (cherry picked from commit 2fb4d305cc81febab404131f55020c10619cedef)

--- htdocs/webservice/functions/mahara_view_external.php
+++ htdocs/webservice/functions/mahara_view_external.php
@@ -96,7 +96,8 @@ class mahara_view_external extends external_api {
         }
         else if (isset($user['username'])) {
             $username = strtolower($user['username']);
-            $dbuser = get_record('usr', 'username', $username);
+            $sql = 'SELECT * FROM {usr} WHERE LOWER(username) = ?';
+            $dbuser = get_record_sql($sql, array($username));
             if (empty($dbuser)) {
                 throw new WebserviceInvalidParameterException(get_string('invalidusername', 'auth.webservice', $user['username']));
             }
@@ -104,7 +105,8 @@ class mahara_view_external extends external_api {
         }
         else if (isset($user['email'])) {
             $email = strtolower($user['email']);
-            $dbuser = get_record('usr', 'email', $email, null, null, null, null, '*', 0);
+            $sql = 'SELECT * FROM {usr} WHERE LOWER(email) = ?';
+            $dbuser = get_record_sql($sql, array($email), 0);
             if (empty($dbuser)) {
                 throw new WebserviceInvalidParameterException(get_string('invalidusername', 'auth.webservice', $user['email']));
             }
