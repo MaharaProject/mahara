9f46df6be02f986f1fbf7f7ca236752ae4c1bd63
Robert Lyon <robertl@catalyst.net.nz>
2022-05-16 11:51:39 +1200

    Bug 1973249: Show error if archiving failed
    - Shows need to retrigger archiving for fail on the page if admin/institution
    admin
    - Shows need to retrigger archiving for fail on the Admin current
    submissions list (at admin/groups/archives.php?current=1)
    
    - Also now shows a message on page for the owner when they have
    submitted the page
    
    Change-Id: I2d79f17eefadb4cb6fed602858bc4dcc547411c8
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lang/en.utf8/view.php
+++ htdocs/lang/en.utf8/view.php
@@ -111,6 +111,7 @@ $string['collectionsubmittedtogroupgrade'] = 'This collection was submitted to t
 $string['collectionsubmittedtohost'] = 'This collection has been submitted for assessment.';
 $string['collectionsubmittedtohoston'] = 'This collection was submitted on %s.';
 $string['submittedpendingrelease'] = 'Pending release after archiving.';
+$string['submittedpendingreleasefailed'] = 'Release failed to archive.<br>Go to <a href="%s">Export queue</a> to re-queue.';
 $string['nobodycanseethisview2'] = 'Only you can see this page.';
 $string['noviews2'] = 'No portfolios';
 $string['youhavenoviews2'] = 'You don\'t have any portfolios.';
