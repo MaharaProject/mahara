542749933f799c1f87580c60821ce717f22641d7
Fergus Whyte <fergusw@catalyst.net.nz>
2022-05-20 09:46:01 +1200

    /local/ Add beacon plugin check.
    Change-Id: Id65d413e20a54cc4d33ceba736c1a0160efa9db6

--- htdocs/local/upgrade.php
+++ htdocs/local/upgrade.php
@@ -7,4 +7,9 @@
 defined('INTERNAL') || die();
 
 function xmldb_local_upgrade($oldversion=0) {
+    if ($oldversion < 2022052000) {
+        if ($data = check_upgrades('module.beacon')) {
+            upgrade_plugin($data);
+        }
+    }
 }
