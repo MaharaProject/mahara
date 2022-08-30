7674665057fab715f406e77a07cfb7e06f9dcde0
Robert Lyon <robertl@catalyst.net.nz>
2022-08-30 17:02:17 +1200

    Security bug 1979575: Add the -dSAFER flag to ghostscript calls
    To avoid potential remote shell execution in Ubuntu 18.04 servers
    The -dSAFER restricts what file operations the job can perform
    
    Change-Id: I3fcdc54e2febd05a460d85196e3ebd08ac36716a
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/export/pdf/lib.php
+++ htdocs/export/pdf/lib.php
@@ -303,7 +303,7 @@ class PluginExportPdf extends PluginExportHtml {
                     exec('pdfunite ' . implode(' ', $collection) . ' ' . $pdfdirectory . '/' . $collectionid . '_' . $collectionname . '.pdf', $output);
                 }
                 else {
-                    exec('gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=' .  $pdfdirectory . '/' . $collectionid . '_' . $collectionname . '.pdf -dBATCH ' . implode(' ', $collection), $output);
+                    exec('gs -dSAFER -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=' .  $pdfdirectory . '/' . $collectionid . '_' . $collectionname . '.pdf -dBATCH ' . implode(' ', $collection), $output);
                 }
                 // remove the page pdfs that are now in collections
                 foreach ($collection as $c) {
