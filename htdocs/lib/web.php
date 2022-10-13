3611d15a8d3704508e858059ccd347a06ba6a2ed
Nathan Nguyen <nathannguyen@catalyst-au.net>
2022-10-13 14:22:36 +1100

    Bug#1992702 add style as allowed attribute for iframe
    HTML purifier remove style attribute from iframe.
    Some embed contents (such as those generated from Canva) require 'style' so that they can be displayed properly
    
    Change-Id: Ie66616d8a17177f342389165954e13015d1dd26b

--- htdocs/lib/web.php
+++ htdocs/lib/web.php
@@ -384,7 +384,7 @@ tinyMCE.init({
         + ",figure[class]"
         + ",figcaption[class]"
         + ",ul[id|type|compact]"
-        + ",iframe[src|width|height|name|scrolling|frameborder|allowfullscreen|webkitallowfullscreen|mozallowfullscreen|longdesc|marginheight|marginwidth|align|title|class|type]"
+        + ",iframe[src|width|height|name|scrolling|frameborder|allowfullscreen|webkitallowfullscreen|mozallowfullscreen|longdesc|marginheight|marginwidth|align|title|class|type|style]"
         + ",a[id|class|title|href|name|target]"
         + ",button[id|class|title]"
     ,urlconverter_callback : "custom_urlconvert",
@@ -4007,6 +4007,7 @@ function clean_html($text, $xhtml=false) {
                 'longdesc' => 'URI',
                 'marginheight' => 'Pixels',
                 'marginwidth' => 'Pixels',
+                'style' => 'Text',
             )
         );
         // allow the tags used with image map to be rendered
