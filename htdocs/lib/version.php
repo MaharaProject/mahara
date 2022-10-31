7e2c4bd1280468d0c35e032113ce7d900ad7e559
Robert Lyon <robertl@catalyst.net.nz>
2022-11-01 12:43:03 +1300

    Version bump for 22.10.1testing
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,8 +16,8 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on dev branches, increment the version by one. On main, use the date.
 
-$config->version = 2022090902;
+$config->version = 2022090904;
 $config->series = '22.10';
-$config->release = '22.10rc2testing';
+$config->release = '22.10.1testing';
 $config->minupgradefrom = 2020013006;
 $config->minupgraderelease = '20.04.0 (release tag 20.04.0_RELEASE)';
