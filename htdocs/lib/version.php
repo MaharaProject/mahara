c5584e61f3e8f2fcef6c701f237192ff786dfd2c
Gold <gold@catalyst.net.nz>
2022-11-01 14:59:53 +1300

    Version bump for 22.04.4testing
    Signed-off-by: Gold <gold@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -16,8 +16,8 @@ $config = new stdClass();
 // See https://wiki.mahara.org/wiki/Developer_Area/Version_Numbering_Policy
 // For upgrades on dev branches, increment the version by one. On main, use the date.
 
-$config->version = 2022032212;
+$config->version = 2022032214;
 $config->series = '22.04';
-$config->release = '22.04.3testing';
+$config->release = '22.04.4testing';
 $config->minupgradefrom = 2020013006;
 $config->minupgraderelease = '20.04.0 (release tag 20.04.0_RELEASE)';
