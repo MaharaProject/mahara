1e5bdee1f934a8342d6bdbbee724678d4a1998fb
Robert Lyon <robertl@catalyst.net.nz>
2023-03-30 10:20:43 +1300

    Bug 2013246: Adding a way to disable copy links when via <a> tags
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>
    Change-Id: I9b0b516b3b85aa324a481fd7a0596216adf0fa1c

--- htdocs/js/mahara.js
+++ htdocs/js/mahara.js
@@ -1306,6 +1306,32 @@ $(function() {
     });
 });
 
+function disable_copy_links() {
+    // Make copy view links 'processing' on click
+    // to avoid double clicking
+    $('a[href*="view/copy.php"]').on('click', function(event) {
+        event.preventDefault();
+        let info = $(this);
+        let infoicon = '';
+        if ($(this).parent().hasClass('dropdown-item')) {
+            $(this).parent().addClass('disabled');
+            infoicon = $(this).find('span.icon');
+        }
+        else {
+            $(this).addClass('disabled');
+            infoicon = $(this).find('span.icon').css('padding-right', '0px').css('padding-left', '7px');
+            $(this).css('color', 'black');
+        }
+        info.text('');
+        if (infoicon) {
+            infoicon.removeClass().addClass('icon icon-lg left icon-spinner icon-pulse');
+            info.append(infoicon[0]);
+        }
+        info.append(document.createTextNode(get_string('processing') + ' ...'));
+        window.location.href = $(this).prop('href');
+    });
+};
+
 /**
  * Offset html anchors for fixed header
  */
@@ -1333,6 +1359,10 @@ jQuery(function($) {
             $('a' + this.hash).attr('tabindex', 0).focus();
         }
     });
+});
+
+jQuery(window).on('blocksloaded', function() {
+    disable_copy_links();
 });
 
 /**
